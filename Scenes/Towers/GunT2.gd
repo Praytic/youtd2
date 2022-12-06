f5fdd279d4233b0e8e06a2b1807c2eb136cb61d5
Praytic <chernogorovvm@gmail.com>
2022-12-05 22:22:13 -0800

    Add RotatingTower parent and inherit all towers from it

---
+++ Scenes/Towers/GunT2.gd
@@ -0,0 +1,1 @@
+extends "res://Scenes/Towers/RotatingTower.gd"
---
+++ Scenes/Towers/RotatingTower.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var initialRotation
+
+func _ready():
+	initialRotation = get_node("Gun").rotation
+
+func _physics_process(delta):
+	var gun = get_node("Gun")
+	var angle = gun.get_angle_to(get_global_mouse_position()) + initialRotation
+	gun.rotate(angle)
