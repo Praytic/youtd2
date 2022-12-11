e866d3445fcd414cb36c982edc8811ea0333e102
Praytic <chernogorovvm@gmail.com>
2022-12-10 22:21:51 -0800

    Add _get_property_list for AreaOfEffect

--- Scenes/Towers/AreaOfEffect.gd
+++ Scenes/Towers/AreaOfEffect.gd
@@ -1,11 +1,31 @@
 tool
 extends Node2D
 
-export var radius: float
-export var resolution: float
+class_name AreaOfEffect
+
+var radius: float
+var resolution: float
 
 func _init(radius: float, position: Vector2):
 	var aoe_sprite = Sprite.new()
 	aoe_sprite.texture = load("res://Resources/PulsingDot.tres")
 	self.radius = radius
 	self.resolution = resolution
+
+func _get_property_list():
+	var properties = []
+	properties.append({
+		name = "Debug",
+		type = TYPE_NIL,
+		usage = PROPERTY_USAGE_CATEGORY | PROPERTY_USAGE_SCRIPT_VARIABLE
+	})
+
+	# Example of adding a property to the script category
+	properties.append({
+		name = "Logging_Enabled",
+		type = TYPE_BOOL
+	})
+	return properties
+
+func _ready():
+	pass
