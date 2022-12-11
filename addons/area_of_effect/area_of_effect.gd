4959d87f35d38059c1f1615a9522a1ccab633c7a
Praytic <chernogorovvm@gmail.com>
2022-12-10 21:13:32 -0800

    Remove addon in favor of script class

--- addons/area_of_effect/area_of_effect.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends EditorPlugin
-
-
-func _enter_tree():
-	add_custom_type("AreaOfEffect", "Node2D", preload("core.gd"), preload("button_icon.png"))
-
-func _exit_tree():
-	remove_custom_type("AreaOfEffect")
