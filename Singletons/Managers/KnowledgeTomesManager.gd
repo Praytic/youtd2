650763a164b43447925d681b3f20285048b3c315
Praytic <chernogorovvm@gmail.com>
2022-12-11 21:38:56 -0800

    Separate global logic to managers

---
+++ Singletons/Managers/KnowledgeTomesManager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+signal knowledge_tomes_change(value)
+
+var knowledge_tomes: int setget set_knowledge_tomes
+var income: int = Properties.globals["ini_knowledge_tomes_income"]
+var max_kt: int = Properties.globals["max_knowledge_tomes"]
+
+func _ready():
+	set_knowledge_tomes(Properties.globals["ini_knowledge_tomes"])
+	
+func set_knowledge_tomes(value):
+	if (value >= max_kt):
+		knowledge_tomes = max_kt
+	elif (value < 0):
+		knowledge_tomes = 0
+	else:
+		knowledge_tomes = value
+	emit_signal("knowledge_tomes_change", value)
+	
+func add_knowledge_tomes(value = income):
+	set_knowledge_tomes(knowledge_tomes + value)
