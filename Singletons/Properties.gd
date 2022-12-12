8365b71deb0bbdeb8084581824c41a27369c2f4d
Praytic <chernogorovvm@gmail.com>
2022-12-11 16:22:31 -0800

    Add global properties

---
+++ Singletons/Properties.gd
@@ -0,0 +1,49 @@
+extends Node
+
+var towers = { 
+	"GunT1": {
+		"id": 1,
+		"name": "Tiny Shrub",
+		"family_id": 1,
+		"author": "gex",
+		"rarity": "common",
+		"element": "nature",
+		"attack_type": "physical",
+		"attack_range": 800,
+		"attack_cd": 0.9,
+		"damage_l": 26,
+		"damage_r": 26,
+		"cost": 30,
+		"description": "Basic nature tower with a slightly increased chance to critical strike."
+	},
+	"GunT2": {
+		"id": 439,
+		"name": "Shrub",
+		"family_id": 1,
+		"author": "gex",
+		"rarity": "common",
+		"element": "nature",
+		"attack_type": "physical",
+		"attack_range": 840,
+		"attack_cd": 0.9,
+		"damage_l": 113,
+		"damage_r": 113,
+		"cost": 30,
+		"description": "Common nature tower with an increased critical strike chance and damage."
+	},
+	"MissleT1": {
+		"id": 439,
+		"name": "Small Cactus",
+		"family_id": 41,
+		"author": "Lapsus",
+		"rarity": "common",
+		"element": "nature",
+		"attack_type": "essence",
+		"attack_range": 820,
+		"attack_cd": 2.5,
+		"damage_l": 58,
+		"damage_r": 58,
+		"cost": 30,
+		"description": "A tiny desert plant with a high AoE. Slightly more efficient against mass creeps and humans."
+	}
+}
