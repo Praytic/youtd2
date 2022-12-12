63c319e5c51fb3c1394254bb5516d6360c54ab32
Praytic <chernogorovvm@gmail.com>
2022-12-11 16:22:31 -0800

    Add global properties

--- Scenes/Towers/Tower.gd
+++ Scenes/Towers/Tower.gd
@@ -6,12 +6,22 @@ signal build_complete
 signal build_init
 
 var aoe: AreaOfEffect
+onready var _properties = Properties.towers[_internal_name]
+onready var attack_range: float = properties["attack_range"]
+onready var attack_cd: float = properties["attack_cd"]
+onready var id: float = properties["id"]
+onready var name: float = properties["name"]
+onready var family_id: float = properties["family_id"]
+onready var author: float = properties["author"]
+onready var rarity: float = properties["rarity"]
+onready var attack_cd: float = properties["attack_cd"]
+onready var attack_cd: float = properties["attack_cd"]
+var _internal_name = get_script().resource_path.get_file().get_basename()
 
-export(float) var radius
 export(int) var size = 32
 
 func _ready():
-	aoe = AreaOfEffect.new(radius)
+	aoe = AreaOfEffect.new(attack_range)
 	aoe.position = Vector2(size, size) / 2
 	connect("build_complete", self, "_on_build_complete")
 	connect("build_init", self, "_on_build_init")
