4890d5efd6373f8485e547557dcc1163141820d7
Kvel2D <kvelyognaut@gmail.com>
2022-12-14 18:39:03 +0100

    fix position of tower build
    was using position that cursor should be drawn at
    change to using position in world

--- Singletons/Managers/CameraManager.gd
+++ Singletons/Managers/CameraManager.gd
@@ -3,9 +3,7 @@ extends Node
 # Returns the position of the mouse clamped to 
 # the underlying tile on the map
 func get_tile_pos_on_cam(cam: Camera2D, map: TileMap) -> Vector2:
-	var world_pos = map.get_local_mouse_position()
-	var map_pos = map.world_to_map(world_pos)
-	var clamped_world_pos = map.map_to_world(map_pos)
+	var clamped_world_pos = get_mouse_pos_on_map_clamped(cam, map)
 
 	var camera_center_pos = cam.get_camera_screen_center()
 	var screen_size = get_viewport().get_visible_rect().size
@@ -13,3 +11,10 @@ func get_tile_pos_on_cam(cam: Camera2D, map: TileMap) -> Vector2:
 	var clamped_mouse_pos = clamped_world_pos - camera_pos
 	
 	return clamped_mouse_pos
+
+func get_mouse_pos_on_map_clamped(cam: Camera2D, map: TileMap) -> Vector2:
+	var world_pos = map.get_local_mouse_position()
+	var map_pos = map.world_to_map(world_pos)
+	var clamped_world_pos = map.map_to_world(map_pos)
+
+	return clamped_world_pos
