7380f46f9bf0fbfd7a567877f450a34c8f86a9bf
Kvel2D <kvelyognaut@gmail.com>
2022-12-15 08:26:53 +0100

    Fix warnings and errors

--- Scenes/Towers/RotatingTower.gd
+++ Scenes/Towers/RotatingTower.gd
@@ -7,7 +7,7 @@ var initialRotation
 func _ready():
 	initialRotation = get_node("Gun").rotation
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	var gun = get_node("Gun")
 	var angle = gun.get_angle_to(get_global_mouse_position()) + initialRotation
 	gun.rotate(angle)
