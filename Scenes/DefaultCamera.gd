59bc911da28ef8b3431cd3b46cc847ad6cc08306
Praytic <chernogorovvm@gmail.com>
2022-12-22 10:42:09 -0800

    Add camera hint

--- Scenes/DefaultCamera.gd
+++ Scenes/DefaultCamera.gd
@@ -1,22 +1,36 @@
 extends Camera2D
 
+signal camera_moved(direction)
+signal camera_zoomed(factor)
+
 export(float) var cam_move_speed = 500.0
 export(float) var maximum_zoom_in = 0.15
 export(float) var minimum_zoom_out = 10
 export(float) var zoom_sensitivity = 1.0
 var move_direction: Vector2
-	
+
+
 func _ready():
 	pass
+
 
 func _physics_process(delta):
 	if (move_direction != Vector2.ZERO):
 		var diagonal_modif = 1
 		if move_direction.abs() == Vector2.ONE:
 			diagonal_modif *= sqrt(2.0)/2.0
-		position = get_camera_position() + move_direction * delta * cam_move_speed * diagonal_modif
+		var shift_vector: Vector2 = move_direction * delta * cam_move_speed * diagonal_modif
+		position = get_camera_position() + shift_vector
+		
+		emit_signal("camera_moved", shift_vector)
 
-func _unhandled_input(_event):
+
+func _unhandled_input(event):
+	_move(event)
+	_zoom(event)
+
+
+func _move(_event):
 	var new_move_direction = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		new_move_direction.x = 1.0
@@ -27,8 +41,8 @@ func _unhandled_input(_event):
 	if Input.is_action_pressed("ui_left"):
 		new_move_direction.x = -1.0
 	
-	_zoom(_event)
 	self.move_direction = new_move_direction
+
 
 func _zoom(event):
 	var factor
@@ -48,3 +62,5 @@ func _zoom(event):
 	elif (new_zoom > minimum_zoom_out):
 		new_zoom = minimum_zoom_out
 	zoom = Vector2(new_zoom, new_zoom)
+	
+	emit_signal("camera_zoomed", factor)
