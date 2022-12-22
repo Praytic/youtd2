cbf1033f55c138be07e1fb28183bb283d1538c56
Praytic <chernogorovvm@gmail.com>
2022-12-22 10:42:09 -0800

    Add camera hint

--- Scenes/HUD.gd
+++ Scenes/HUD.gd
@@ -5,6 +5,14 @@ signal start_wave(wave_index)
 signal stop_wave()
 
 
+onready var camera: Camera2D = get_node(@"/root/GameScene/DefaultCamera")
+
+
+func _ready():
+	camera.connect("camera_moved", self, "_on_camera_moved")
+	camera.connect("camera_zoomed", self, "_on_camera_zoomed")
+
+
 func _on_StartWaveButton_pressed():
 	var wave_index: int = $VBoxContainer/HBoxContainer/WaveEdit.value
 	emit_signal("start_wave", wave_index)
@@ -13,3 +21,9 @@ func _on_StartWaveButton_pressed():
 func _on_StopWaveButton_pressed():
 	emit_signal("stop_wave")
 
+
+func _on_camera_moved(_vector):
+	$Hints.hide()
+
+func _on_camera_zoomed(_factor):
+	$Hints.hide()
