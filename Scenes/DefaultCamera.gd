d807f7378ac294d22263538583767ddef37d86b3
Kvel2D <kvelyognaut@gmail.com>
2023-01-01 18:41:56 +0100

    Implement zooming via mousewheel
    remove "factor" arg from camera_zoomed() signal because there's no
    \ factor for mousewheel and this arg is unused

--- Scenes/DefaultCamera.gd
+++ Scenes/DefaultCamera.gd
@@ -1,12 +1,13 @@
 extends Camera2D
 
 signal camera_moved(direction)
-signal camera_zoomed(factor)
+signal camera_zoomed()
 
 export(float) var cam_move_speed = 500.0
 export(float) var maximum_zoom_in = 0.15
 export(float) var minimum_zoom_out = 10
 export(float) var zoom_sensitivity = 1.0
+export(float) var mousewheel_zoom_speed = 0.4
 var move_direction: Vector2
 
 
@@ -45,22 +46,30 @@ func _move(_event):
 
 
 func _zoom(event):
-	var factor
+	var new_zoom = zoom.x
+
 	if event is InputEventMagnifyGesture:
-		factor = event.factor
+		var zoom_amount = -(event.factor - 1.0) * zoom_sensitivity
+		new_zoom = zoom.y + zoom_amount
 	elif event is InputEventMouseButton:
+#		Make zoom change slower as the camera gets more zoomed in
+		var slow_down_multiplier = zoom.x / minimum_zoom_out
+		var zoom_change = mousewheel_zoom_speed * slow_down_multiplier
+		
 		match event.get_button_index():
-			BUTTON_WHEEL_DOWN, BUTTON_WHEEL_UP:
-				factor = event.factor
-	if not factor: 
+			BUTTON_WHEEL_DOWN:
+				new_zoom += zoom_change
+			BUTTON_WHEEL_UP:
+				new_zoom += -zoom_change
+			_:
+				return
+	else:
 		return
-	
-	var zoom_amount = -(factor - 1.0) * zoom_sensitivity
-	var new_zoom = zoom.y + zoom_amount
+
 	if (new_zoom < maximum_zoom_in):
 		new_zoom = maximum_zoom_in
 	elif (new_zoom > minimum_zoom_out):
 		new_zoom = minimum_zoom_out
 	zoom = Vector2(new_zoom, new_zoom)
 	
-	emit_signal("camera_zoomed", factor)
+	emit_signal("camera_zoomed")
