2f015b960e5ae74cad8ef2029a6030bb0ede9347
Kvel2D <kvelyognaut@gmail.com>
2023-01-01 21:26:31 +0100

    Add simple tower tooltip
    shown when tower buttons are hovered
    displays info from Properties

--- Scenes/HUD.gd
+++ Scenes/HUD.gd
@@ -11,6 +11,8 @@ onready var camera: Camera2D = get_node(@"/root/GameScene/DefaultCamera")
 func _ready():
 	camera.connect("camera_moved", self, "_on_camera_moved")
 	camera.connect("camera_zoomed", self, "_on_camera_zoomed")
+	
+	$TowerTooltip.hide()
 
 
 func _on_StartWaveButton_pressed():
@@ -27,3 +29,32 @@ func _on_camera_moved(_vector):
 
 func _on_camera_zoomed(_factor):
 	$Hints.hide()
+
+
+func _on_MissleT1_mouse_entered():
+	_on_TowerButton_mouse_entered($BuildBar/MissleT1)
+
+
+func _on_MissleT1_mouse_exited():
+	_on_GenericButton_mouse_exited($BuildBar/MissleT1)
+
+
+func _on_GunT1_mouse_entered():
+	_on_TowerButton_mouse_entered($BuildBar/GunT1)
+
+
+func _on_GunT1_mouse_exited():
+	_on_GenericButton_mouse_exited($BuildBar/GunT1)
+
+
+# TODO: connect button signals directly to the general entered/exited slots,
+# without using specific slots for each button.
+func _on_TowerButton_mouse_entered(tower_button):
+	var tower_id = tower_button.tower_id
+	$TowerTooltip.set_tower_id(tower_id)
+	$TowerTooltip.show()
+
+
+func _on_GenericButton_mouse_exited(tower_button):
+	var tower_id = tower_button.tower_id
+	$TowerTooltip.hide()
