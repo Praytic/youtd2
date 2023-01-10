3376a4c4e95023509fffc3f9be9c54cea1b7b12c
Praytic <chernogorovvm@gmail.com>
2023-01-09 18:49:01 -0800

    Create animated sprite of mob with diff angles

--- Scenes/Mob.gd
+++ Scenes/Mob.gd
@@ -5,24 +5,51 @@ class_name Mob
 export var health: int = 10
 export var mob_move_speed: int = 200
 
-# Called when the node enters the scene tree for the first time.
+
+var last_position
+
+
+onready var _sprite = $KinematicBody2D/Sprite
+
+
 func _ready():
-	pass
+	last_position = global_position
 
 
 func _process(delta):
 	offset += delta * mob_move_speed
-
+	var move_direction = last_position - global_position
+	
+	
+	if move_direction != Vector2.ZERO:
+		var angle = (int(rad2deg(move_direction.angle_to(Vector2.ONE))) + 360) % 360
+		print(angle)
+		if angle >= 315 and angle <= 360 or angle >= 0 and angle < 45:
+			print("Moving west")
+			_sprite.play("run_w")
+		elif angle >= 45 and angle < 135:
+			print("Moving south")
+			_sprite.play("run_s")
+		elif angle >= 135 and angle < 225:
+			print("Moving east")
+			_sprite.play("run_e")
+		elif angle >= 225 and angle < 315:
+			print("Moving north")
+			_sprite.play("run_n")
+	else:
+		_sprite.play("stand")
+	
+	
 #	Delete mob once it has reached the end of the path
 	var reached_end_of_path: bool = (unit_offset >= 1.0)
 
 	if reached_end_of_path:
 		queue_free()
-
+	last_position = global_position
 
 func apply_damage(damage):
 	health -= damage
 	
 	if health < 0:
 		queue_free()
-	
+
