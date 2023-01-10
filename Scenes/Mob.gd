c7281060fa8038561d6c07c43eff33e0b6a72ad7
Kvel2D <kvelyognaut@gmail.com>
2023-01-10 12:37:21 +0100

    Implement drawing order for mobs
    put mobs into YSort
    can't use PathFollow2D so move mobs manually along the path

--- Scenes/Mob.gd
+++ Scenes/Mob.gd
@@ -1,15 +1,16 @@
-extends PathFollow2D
+extends KinematicBody2D
 
 class_name Mob
 
 export var health: int = 10
-export var mob_move_speed: int = 200
+export var mob_move_speed: int = 500
 
 
 var last_position
+var path_curve: Curve2D
+var current_path_index: int = 0
 
-
-onready var _sprite = $KinematicBody2D/Sprite
+onready var _sprite = $Sprite
 
 
 func _ready():
@@ -17,10 +18,22 @@ func _ready():
 
 
 func _process(delta):
-	offset += delta * mob_move_speed
 	var move_direction = last_position - global_position
 	
+	var path_point: Vector2 = path_curve.get_point_position(current_path_index)
+	position = position.move_toward(path_point, mob_move_speed * delta)
 	
+	var reached_path_point: bool = (position == path_point)
+	
+	if reached_path_point:
+		current_path_index += 1
+		
+#		Delete mob once it has reached the end of the path
+		var reached_end_of_path: bool = (current_path_index >= path_curve.get_point_count())
+
+		if reached_end_of_path:
+			queue_free()
+
 	if move_direction != Vector2.ZERO:
 		var angle = (int(rad2deg(move_direction.angle_to(Vector2.ONE))) + 360) % 360
 		print(angle)
@@ -38,13 +51,7 @@ func _process(delta):
 			_sprite.play("run_n")
 	else:
 		_sprite.play("stand")
-	
-	
-#	Delete mob once it has reached the end of the path
-	var reached_end_of_path: bool = (unit_offset >= 1.0)
 
-	if reached_end_of_path:
-		queue_free()
 	last_position = global_position
 
 func apply_damage(damage):
@@ -53,3 +60,7 @@ func apply_damage(damage):
 	if health < 0:
 		queue_free()
 
+
+func set_path(path: Path2D):
+	path_curve = path.curve
+	position = path_curve.get_point_position(0)
