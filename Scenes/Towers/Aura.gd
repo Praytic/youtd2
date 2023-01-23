76b6cb7f4645445bb3fcf8c7f2145738c596aa57
Kvel2D <kvelyognaut@gmail.com>
2023-01-23 14:58:52 +0100

    Implement value ranges
    Need to figure out whether values are always float or always int
    Currently its mixed and it's not good

--- Scenes/Towers/Aura.gd
+++ Scenes/Towers/Aura.gd
@@ -9,7 +9,10 @@ signal expired(aura)
 
 
 var type: String
-var value: float
+var value_is_range: bool
+var value_fixed: float
+var value_min: float
+var value_max: float
 var duration: float
 var period: float
 
@@ -18,9 +21,18 @@ var is_expired: bool = false
 
 func _init(aura_info):
 	type = aura_info["type"]
-	value = aura_info["value"]
 	duration = aura_info["duration"]
 	period = aura_info["period"]
+
+	value_is_range = aura_info.has("value_range")
+
+	if value_is_range:
+		var value_range: Array = aura_info["value_range"] as Array
+
+		value_min = min(value_range[0], value_range[1])
+		value_max = max(value_range[0], value_range[1])
+	else:
+		value_fixed = aura_info["value_fixed"]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -29,8 +41,6 @@ func _ready():
 #	This means that for periodic aura's, their first tick happens
 #	when aura is created.
 	apply()
-
-	var aura_is_instant = duration == 0
 	
 	if duration > 0:
 		var duration_timer: Timer = Timer.new()
@@ -65,3 +75,14 @@ func expire():
 	is_expired = true
 	emit_signal("expired", self)
 	queue_free()
+
+
+# NOTE: not sure what to do here about float vs int
+# It seems like values need to be float sometimes (when?)
+# But want to do randomization as ints.
+func get_value() -> float:
+	if value_is_range:
+		var out = Utils.randi_range(int(value_min), int(value_max))
+		return out
+	else:
+		return value_fixed
