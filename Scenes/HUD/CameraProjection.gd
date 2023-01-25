9e9a0d8c7260863a52d8bdc6a799939d8b3b02a4
Praytic <chernogorovvm@gmail.com>
2023-01-25 13:28:40 -0800

    Complete minimap projections

--- Scenes/HUD/CameraProjection.gd
+++ Scenes/HUD/CameraProjection.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var _camera_projection_size: Vector2 setget set_size
+
 func _draw():
-	draw_circle(global_position, 100.0, Color.white)
-	print("Circle: %s" % position)
+	var camera_projection_rect = Rect2(Vector2.ZERO, _camera_projection_size)
+	draw_rect(camera_projection_rect, Color.white, false)
+
+func set_size(value: Vector2):
+	_camera_projection_size = value
--- Scenes/HUD/Minimap.gd
+++ Scenes/HUD/Minimap.gd
@@ -4,7 +4,9 @@ onready var default_camera = get_node("%Camera")
 onready var minimap_camera = $ViewportContainer/Viewport/MinimapCamera
 onready var minimap_texture = $ViewportContainer/Viewport/MinimapTexture
 onready var camera_projection = $ViewportContainer/Viewport/CameraProjection
+onready var mobs_projection = $ViewportContainer/Viewport/MobsProjection
 onready var map = get_node("%Map")
+onready var mobs = map.get_node("MobYSort")
 onready var minimap_scale: float
 
 func _ready():
@@ -13,7 +15,7 @@ func _ready():
 	var minimap_size = minimap_texture.get_rect().size
 	minimap_scale = minimap_size.x / map_size.x / 2
 	_update_view_rect()
-	
+
 
 func _on_Camera_camera_moved(shift_vector):
 	minimap_camera.position = minimap_camera.get_camera_position() + \
@@ -38,6 +40,20 @@ func _update_view_rect():
 	var minimap_view_pos = -minimap_ctrans.get_origin() / minimap_ctrans.get_scale()
 	var minimap_view_center = minimap_view_pos + minimap_view_size / 2
 	
-	print("Camera: %s %s" % [minimap_view_pos, minimap_view_size])
-	camera_projection.position = minimap_view_center
+	camera_projection.position = projection_pos
+	camera_projection.set_size(projection_size)
 	camera_projection.update()
+
+
+func _on_MobYSort_child_entered_tree(mob):
+	mob.connect("moved", self, "_on_Mob_moved", [mob])
+
+
+func _on_MobYSort_child_exiting_tree(mob):
+	mob.disconnect("moved", self, "_on_Mob_moved")
+	mobs_projection.pos_dict.erase(mob)
+
+
+func _on_Mob_moved(delta, mob):
+	mobs_projection.pos_dict[mob] = mob.position * minimap_scale
+	mobs_projection.update()
