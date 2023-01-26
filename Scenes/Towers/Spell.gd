9c6308b01c8eeb41dc4b35c857448bd9c87eec08
Kvel2D <kvelyognaut@gmail.com>
2023-01-26 14:48:58 +0100

    Add AuraType.INCREASE_SPELL_CAST_RANGE

--- Scenes/Towers/Spell.gd
+++ Scenes/Towers/Spell.gd
@@ -10,7 +10,8 @@ class_name Spell
 var spell_info: Dictionary
 var aura_info_container: AuraInfoContainer
 var spell_parameter_mod_map: Dictionary = {
-	Properties.AuraType.DECREASE_SPELL_CAST_CD: 0.0
+	Properties.AuraType.DECREASE_SPELL_CAST_CD: 0.0,
+	Properties.AuraType.INCREASE_SPELL_CAST_RANGE: 0.0,
 }
 
 
@@ -48,7 +49,12 @@ func apply_aura(aura: Aura):
 		else:
 			spell_parameter_mod_map[aura.type] = aura.get_value()
 
-		$CastTimer.wait_time = get_modded_spell_parameter(Properties.SpellParameter.CAST_CD, Properties.AuraType.DECREASE_SPELL_CAST_CD)
+	match aura.type:
+		Properties.AuraType.DECREASE_SPELL_CAST_CD:
+			$CastTimer.wait_time = get_modded_spell_parameter(Properties.SpellParameter.CAST_CD, Properties.AuraType.DECREASE_SPELL_CAST_CD)
+		Properties.AuraType.INCREASE_SPELL_CAST_RANGE:			
+			var cast_range: float = get_modded_spell_parameter(Properties.SpellParameter.CAST_RANGE, Properties.AuraType.INCREASE_SPELL_CAST_RANGE)
+			Utils.circle_shape_set_radius($CastArea/CollisionShape2D, cast_range)
 
 	aura_info_container.apply_aura(aura)
 
@@ -57,9 +63,9 @@ func get_spell_parameter(parameter: int):
 	return spell_info[parameter]
 
 
-func get_modded_spell_parameter(parameter: int, mod_aura_type: int) -> float:
+func get_modded_spell_parameter(spell_parameter: int, mod_aura_type: int) -> float:
 	$CastTimer.wait_time
-	var default_value: float = spell_info[Properties.SpellParameter.CAST_CD]
+	var default_value: float = spell_info[spell_parameter]
 	var modifier: float = spell_parameter_mod_map[mod_aura_type]
 	var modded = default_value * (1.0 + modifier)
 
