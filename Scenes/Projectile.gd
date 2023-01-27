5b11d9280b7199ebc4b2ff238db6750947acd878
Kvel2D <kvelyognaut@gmail.com>
2023-01-27 11:10:24 +0100

    Add splash damage
    Move damage dealing to tower
    Add Projectile.reached_mob() signal

--- Scenes/Projectile.gd
+++ Scenes/Projectile.gd
@@ -4,16 +4,18 @@ extends KinematicBody2D
 # Projectile moves towards the target and disappears when it
 # reaches the target.
 
+signal reached_mob(mob)
+
+onready var object_container = get_tree().get_root().get_node("GameScene").get_node("Map").get_node("MobYSort")
+
 var target_mob: Mob = null
 export var speed: int = 100
 export var contact_distance: int = 30
-var damage: Array
 
 
-func init(target_mob_arg: Mob, tower_position: Vector2, damage_arg: Array):
+func init(target_mob_arg: Mob, tower_position: Vector2):
 	target_mob = target_mob_arg
 	position = tower_position
-	damage = damage_arg
 
 
 func have_target() -> bool:
@@ -28,15 +30,11 @@ func _process(delta):
 #	Move towards mob
 	var target_pos = target_mob.position
 	var pos_diff = target_pos - position
-	
+	var move_vector = speed * pos_diff.normalized() * delta
+	position += move_vector
+
 	var reached_mob = pos_diff.length() < contact_distance
 
 	if reached_mob:
-		target_mob.apply_damage(damage)
-
+		emit_signal("reached_mob", target_mob)
 		queue_free()
-		return
-	
-	var move_vector = speed * pos_diff.normalized() * delta
-	
-	position += move_vector
