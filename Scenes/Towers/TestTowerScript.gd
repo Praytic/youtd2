e35501518942b391f7b6c5cadf10c4854d40bf0d
Kvel2D <kvelyognaut@gmail.com>
2023-01-27 22:33:21 +0100

    Begin implementing Buff

--- Scenes/Towers/TestTowerScript.gd
+++ Scenes/Towers/TestTowerScript.gd
@@ -8,12 +8,22 @@ var parameters: Dictionary = {
 	Properties.ScriptParameter.ON_ATTACK_CHANCE_LEVEL_ADD: 0.0,
 }
 
+var test_buff: Buff
+
+
+func _init():
+	test_buff = Buff.new(5.0)
+
+
 func _ready():
 	pass
 
 
 func on_attack(tower: Tower, event: Event):
 	print("on_attack!")
+
+	var target: Mob = event.target
+	test_buff.apply(tower, target, 1.0)
 
 
 func on_damage(tower: Tower, event: Event):
