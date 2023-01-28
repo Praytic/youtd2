c77da7112576e80d958b36ec1dab6c734a96ff9b
Kvel2D <kvelyognaut@gmail.com>
2023-01-28 09:01:47 +0100

    Make tower script instances global
    This way, towers of same type share one script instance
    This is necessary to implement correct buff behavior

--- Scenes/Towers/TestTowerScript.gd
+++ Scenes/Towers/TestTowerScript.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-var parameters: Dictionary = {
+const parameters: Dictionary = {
 	Properties.ScriptParameter.ON_DAMAGE_CHANCE: 1.0,
 	Properties.ScriptParameter.ON_DAMAGE_CHANCE_LEVEL_ADD: 0.0,
 	Properties.ScriptParameter.ON_ATTACK_CHANCE: 1.0,
