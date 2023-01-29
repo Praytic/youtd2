022c112ffc6ea29e1b77bb38e3a353473986c5e7
Kvel2D <kvelyognaut@gmail.com>
2023-01-29 16:03:29 +0100

    Remove unused value_modifier arg

--- Scenes/Buffs/Slow.gd
+++ Scenes/Buffs/Slow.gd
@@ -2,7 +2,7 @@ class_name Slow
 extends Buff
 
 
-func _init(tower_arg: Tower, time: float, time_level_add: float, value_modifier: float, power_level: int).(tower_arg, time, time_level_add, value_modifier, power_level):
+func _init(tower_arg: Tower, time: float, time_level_add: float, power_level: int).(tower_arg, time, time_level_add, power_level):
 	var slow_modifier: Modifier = Modifier.new()
 	slow_modifier.add_modification(Modification.Type.MOD_MOVE_SPEED, -100.0, 0.01)
 	.set_modifier(slow_modifier)
