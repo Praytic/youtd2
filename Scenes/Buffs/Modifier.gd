60c5e34a9106701f1daa16193bb8d60bb7fea817
Kvel2D <kvelyognaut@gmail.com>
2023-01-29 15:44:37 +0100

    Move modifier level type impl to Buff.gd
    Because Modifier can be used outside Buff directly on Unit, so it
    \ doesn't make sense for ModifierLevelType code to be in Modifier

--- Scenes/Buffs/Modifier.gd
+++ Scenes/Buffs/Modifier.gd
@@ -2,23 +2,11 @@ class_name Modifier
 extends Node
 
 # Modifier stores a list of modifications. Can apply and
-# undo apply of such modifications on a unit. LevelType
-# determines whether modifier scales with tower level or
-# buff level. By default modification scales with tower
-# level.
-
-
-enum LevelType {
-	TOWER,
-	BUFF
-}
+# undo apply of such modifications on a unit.
 
 
 var _modification_list: Array = [] setget , get_modification_list
 var level: int = 0
-var _tower_level: int
-var _buff_level: int
-var level_type: int = LevelType.TOWER
 
 
 func add_modification(modification_type: int, value_base: float, level_add: float):
@@ -28,16 +16,3 @@ func add_modification(modification_type: int, value_base: float, level_add: floa
 
 func get_modification_list() -> Array:
 	return _modification_list.duplicate(true)
-
-
-func set_levels(tower_level: int, buff_level: int):
-	_tower_level = tower_level
-	_buff_level = buff_level
-
-
-func get_level() -> int:
-	match level_type:
-		LevelType.TOWER: return _tower_level
-		LevelType.BUFF: return _buff_level
-
-	return 0
