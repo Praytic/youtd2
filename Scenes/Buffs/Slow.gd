43100b71f4a97158dd8c029cc8624015d8198560
Kvel2D <kvelyognaut@gmail.com>
2023-01-29 10:03:58 +0100

    Add level_add to buff time and modifier
    Add ModifierLevelType to select between buff power level and tower level
    Remove value_modifier arg
    Add level args to Modifier apply() and remove()
    Add level_add arg to Modification ctor

--- Scenes/Buffs/Slow.gd
+++ Scenes/Buffs/Slow.gd
@@ -4,7 +4,7 @@ extends Buff
 class_name Slow
 
 
-func _init(tower_arg: Tower, time: float, value_modifier: float, power_level: int).(tower_arg, time, value_modifier, power_level):
+func _init(tower_arg: Tower, time: float, time_level_add: float, value_modifier: float, power_level: int).(tower_arg, time, time_level_add, value_modifier, power_level):
 	var slow_modifier: Modifier = Modifier.new()
-	slow_modifier.add_modification(Modification.Type.MOD_MOVE_SPEED, 0, -100.0)
+	slow_modifier.add_modification(Modification.Type.MOD_MOVE_SPEED, -100.0, 0.01)
 	.set_modifier(slow_modifier)
