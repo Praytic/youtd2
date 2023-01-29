c7dcdffa9e81d3ad891a6b78e32f5b8f45300ddd
Kvel2D <kvelyognaut@gmail.com>
2023-01-29 13:07:35 +0100

    Remove set_buff_modifier()
    Implement different level types by adding Modifier.level_type var
    \ which can be modified directly

--- Scenes/Buffs/Modifier.gd
+++ Scenes/Buffs/Modifier.gd
@@ -2,11 +2,23 @@ class_name Modifier
 extends Node
 
 # Modifier stores a list of modifications. Can apply and
-# undo apply of such modifications on a unit.
+# undo apply of such modifications on a unit. LevelType
+# determines whether modifier scales with tower level or
+# buff level. By default modification scales with tower
+# level.
+
+
+enum LevelType {
+	TOWER,
+	BUFF
+}
 
 
 var _modification_list: Array = [] setget , get_modification_list
 var level: int = 0
+var _tower_level: int
+var _buff_level: int
+var level_type: int = LevelType.TOWER
 
 
 func add_modification(modification_type: int, value_base: float, level_add: float):
@@ -14,5 +26,18 @@ func add_modification(modification_type: int, value_base: float, level_add: floa
 	_modification_list.append(modification)
 
 
-func get_modification_list()-> Array:
+func get_modification_list() -> Array:
 	return _modification_list.duplicate(true)
+
+
+func set_levels(tower_level: int, buff_level: int):
+	_tower_level = tower_level
+	_buff_level = buff_level
+
+
+func get_level() -> int:
+	match level_type:
+		LevelType.TOWER: return _tower_level
+		LevelType.BUFF: return _buff_level
+
+	return 0
