af9c0c60dbc3994239f629b6eaf2cd42bc8aaf8e
Kvel2D <kvelyognaut@gmail.com>
2023-01-30 21:38:16 +0100

    Fix slow debuff

--- Scenes/Buffs/Slow.gd
+++ Scenes/Buffs/Slow.gd
@@ -4,5 +4,5 @@ extends Buff
 
 func _init(tower_arg: Tower, time: float, time_level_add: float, power_level: int, friendly: bool).(tower_arg, time, time_level_add, power_level, friendly):
 	var slow_modifier: Modifier = Modifier.new()
-	slow_modifier.add_modification(Modification.Type.MOD_MOVE_SPEED, -100.0, 0.01)
+	slow_modifier.add_modification(Modification.Type.MOD_MOVE_SPEED_ABSOLUTE, -100.0, 0.01)
 	.set_modifier(slow_modifier)
