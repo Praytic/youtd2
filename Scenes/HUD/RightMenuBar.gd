d8dd164f8baf0794ac893336a36276df20c2b1e3
Kvel2D <kvelyognaut@gmail.com>
2023-01-30 14:47:32 +0100

    Add button which can build any tower

--- Scenes/HUD/RightMenuBar.gd
+++ Scenes/HUD/RightMenuBar.gd
@@ -1,5 +1,10 @@
 extends Control
 
+
+onready var builder_control = get_tree().current_scene.get_node(@"%BuilderControl")
+onready var tower_option_button: OptionButton = $VBoxContainer/TowerOptionButton
+
+
 func _ready():
 	self.hide()
 
@@ -11,3 +16,8 @@ func _on_Button_pressed():
 func _unhandled_input(event):
 	if event.is_action_released("ui_cancel") or event.is_action_released("ui_accept"):
 		hide()
+
+
+func _on_BuildSelectedTowerButton_pressed():
+	var selected_tower_id: int = tower_option_button.get_selected_id()
+	builder_control.on_build_button_pressed(selected_tower_id)
