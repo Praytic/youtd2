aeb797373af2196ee5933989a690fc3d052dbee3
Kvel2D <kvelyognaut@gmail.com>
2023-01-31 20:05:33 +0100

    Get rid of tower id duplication
    In Properties.tower_id_map
    Messy implementation but better than nothing

--- Scenes/HUD/TowersOptionButton.gd
+++ Scenes/HUD/TowersOptionButton.gd
@@ -6,8 +6,10 @@ const SETTINGS_PATH: String = "user://settings.cfg"
 
 
 func _ready():
-	for tower_name in Properties.tower_id_map.keys():
-		var tower_id: int = Properties.tower_id_map[tower_name]
+	var tower_name_list: Array = TowerManager.get_tower_name_list()
+
+	for tower_name in tower_name_list:
+		var tower_id: int = TowerManager.get_tower_id(tower_name)
 		add_item(tower_name, tower_id)
 	
 	var saved_tower_id: int = _get_saved_tower_id()
