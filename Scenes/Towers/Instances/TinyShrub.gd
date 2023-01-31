b9b8da3a0ac4ea63d544533dec266dbf473eb390
Kvel2D <kvelyognaut@gmail.com>
2023-01-31 20:04:11 +0100

    Define towers from "Shrub" family

--- Scenes/Towers/Instances/TinyShrub.gd
+++ Scenes/Towers/Instances/TinyShrub.gd
@@ -11,32 +11,18 @@ func _get_properties() -> Dictionary:
 		"element": "nature",
 		"attack_type": "physical",
 		"base_stats": {
-			Tower.Stat.ATTACK_RANGE: 600.0,
-			Tower.Stat.ATTACK_CD: 1.0,
-			Tower.Stat.ATTACK_DAMAGE_MIN: 10,
-			Tower.Stat.ATTACK_DAMAGE_MAX: 20,
-		},
-		"trigger_parameters": {
-			Tower.TriggerParameter.ON_DAMAGE_CHANCE: 1.0,
-			Tower.TriggerParameter.ON_DAMAGE_CHANCE_LEVEL_ADD: 0.0,
-			Tower.TriggerParameter.ON_ATTACK_CHANCE: 1.0,
-			Tower.TriggerParameter.ON_ATTACK_CHANCE_LEVEL_ADD: 0.0,
-		},
-		"splash": {
-			320: 0.5,
+			Tower.Stat.ATTACK_RANGE: 800.0,
+			Tower.Stat.ATTACK_CD: 0.9,
+			Tower.Stat.ATTACK_DAMAGE_MIN: 26,
+			Tower.Stat.ATTACK_DAMAGE_MAX: 26,
 		},
 		"cost": 30,
 		"description": "Basic nature tower with a slightly increased chance to critical strike.",
 	}
 
+
 func _get_specials_modifier() -> Modifier:
 	var specials_modifier: Modifier = Modifier.new()
-	specials_modifier.add_modification(Modification.Type.MOD_ATTACK_CRIT_CHANCE, 0.2, 0.0035)
+	specials_modifier.add_modification(Modification.Type.MOD_ATTACK_CRIT_CHANCE, 0.02, 0.0035)
 
 	return specials_modifier
-
-
-func _on_attack(event: Event):
-	var slow: Buff = Slow.new(self, 5.0, 0.0, get_level(), false)
-	var target: Unit = event.target
-	target.apply_buff(slow)
