015fffc7ca9b3a1e2b1471b69199e3ffc2875ade
Kvel2D <kvelyognaut@gmail.com>
2023-01-31 21:18:01 +0100

    Reorder tower properties
    According to order on website
    General properties first, optional next
    Add comments about properties
    Remove old property vars
    Fix splash not using property value

--- Scenes/Towers/Instances/Shrubfield.gd
+++ Scenes/Towers/Instances/Shrubfield.gd
@@ -3,8 +3,8 @@ extends Tower
 
 func _get_base_properties() -> Dictionary:
 	return {
-		Tower.Property.ID: 511,
 		Tower.Property.NAME: "Shrubfield",
+		Tower.Property.ID: 511,
 		Tower.Property.FAMILY_ID: 1,
 		Tower.Property.AUTHOR: "gex",
 		Tower.Property.RARITY: "common",
