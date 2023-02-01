86896ea49b81779cb07cfa9d9e96491da084050d
Kvel2D <kvelyognaut@gmail.com>
2023-02-01 21:04:29 +0100

    Move general tower properties to csv file

--- Scenes/Towers/Instances/GreaterShrub.gd
+++ Scenes/Towers/Instances/GreaterShrub.gd
@@ -2,21 +2,7 @@ extends Tower
 
 
 func _get_base_properties() -> Dictionary:
-	return {
-		Tower.Property.NAME: "Greater Shrub",
-		Tower.Property.ID: 459,
-		Tower.Property.FAMILY_ID: 1,
-		Tower.Property.AUTHOR: "gex",
-		Tower.Property.RARITY: "common",
-		Tower.Property.ELEMENT: "nature",
-		Tower.Property.ATTACK_TYPE: "physical",
-		Tower.Property.ATTACK_RANGE: 880.0,
-		Tower.Property.ATTACK_CD: 0.9,
-		Tower.Property.ATTACK_DAMAGE_MIN: 299,
-		Tower.Property.ATTACK_DAMAGE_MAX: 299,
-		Tower.Property.COST: 400,
-		Tower.Property.DESCRIPTION: "Common nature tower with an increased critical strike chance and damage.",
-	}
+	return {}
 
 
 func _get_specials_modifier() -> Modifier:
