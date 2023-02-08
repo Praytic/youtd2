7d33d6caf5e376d2f03d229124b70a0c2ef151a2
Kvel2D <kvelyognaut@gmail.com>
2023-02-08 20:24:29 +0100

    Add scripts for Cactus towers
    (family id = 41)
    Fix class name for BloomingCactus in .csv

---
+++ Scenes/Towers/Instances/AverageCactus.gd
@@ -0,0 +1,12 @@
+extends Tower
+
+
+func _init_tower():
+	var splash_attack_buff = SplashAttack.new({320: 0.5})
+	splash_attack_buff.apply_to_unit_permanent(self, self, 0, true)
+
+
+func _get_specials_modifier() -> Modifier:
+	var specials_modifier: Modifier = SmallCactus.get_cactus_special(0.19, 0.012)
+
+	return specials_modifier
--- Scenes/Towers/Instances/SmallCactus.gd
+++ Scenes/Towers/Instances/SmallCactus.gd
@@ -1,13 +1,22 @@
+class_name SmallCactus
 extends Tower
 
 
-func _get_base_properties() -> Dictionary:
-	return {}
+func _init_tower():
+	var splash_attack_buff = SplashAttack.new({320: 0.5})
+	splash_attack_buff.apply_to_unit_permanent(self, self, 0, true)
 
 
 func _get_specials_modifier() -> Modifier:
-	var specials_modifier: Modifier = Modifier.new()
-	specials_modifier.add_modification(Modification.Type.MOD_DMG_TO_MASS, 0.15, 0.01)
-	specials_modifier.add_modification(Modification.Type.MOD_DMG_TO_HUMANOID, 0.15, 0.01)
+	var specials_modifier: Modifier = get_cactus_special(0.15, 0.01)
 
 	return specials_modifier
+
+
+static func get_cactus_special(value: float, value_add: float) -> Modifier:
+	var cactus_special: Modifier = Modifier.new()
+	cactus_special.add_modification(Modification.Type.MOD_DMG_TO_MASS, 0.15, 0.01)
+	cactus_special.add_modification(Modification.Type.MOD_DMG_TO_HUMANOID, 0.15, 0.01)
+
+	return cactus_special
+
