64ca0ab54395d7c636922126904f03be41e7cfcc
Kvel2D <kvelyognaut@gmail.com>
2023-02-09 19:40:34 +0100

    Move FrostRoot effect into tower script

--- Scenes/Towers/Instances/FrostRoot1.gd
+++ Scenes/Towers/Instances/FrostRoot1.gd
@@ -2,18 +2,29 @@ extends Tower
 
 
 const _tier_stats_map: Dictionary = {
-	1: {value = 25, value_add = 1},
-	2: {value = 125, value_add = 5},
-	3: {value = 375, value_add = 15},
-	4: {value = 750, value_add = 30},
-	5: {value = 1500, value_add = 60},
-	6: {value = 2500, value_add = 100},
+	1: {damage = 25, damage_add = 1},
+	2: {damage = 125, damage_add = 5},
+	3: {damage = 375, damage_add = 15},
+	4: {damage = 750, damage_add = 30},
+	5: {damage = 1500, damage_add = 60},
+	6: {damage = 2500, damage_add = 100},
 }
 
-
 func _ready():
-	var tier: int = get_tier()
-	var stats = _tier_stats_map[tier]
+	var frozen_thorn_buff: Buff = Buff.new()
+	frozen_thorn_buff.set_id("frozen_thorn")
+	frozen_thorn_buff.add_event_handler_with_chance(Buff.EventType.DAMAGE, self, "_on_damage", 0.15, 0.0)
 
-	var frozen_thorn_buff = FrozenThorn.new(stats.value, stats.value_add)
 	frozen_thorn_buff.apply_to_unit_permanent(self, self, 0, false)
+
+
+func _on_damage(event: Event):
+	if event.is_main_target && !event.target.is_immune():
+		Utils.sfx_at_unit("Abilities\\Spells\\Undead\\FrostArmor\\FrostArmorDamage.mdl", event.target)
+
+		var tier: int = get_tier()
+		var stats = _tier_stats_map[tier]
+
+		var damage: float = stats.damage + stats.damage_add * get_level()
+
+		do_spell_damage(event.target, damage, calc_spell_crit_no_bonus(), false)
