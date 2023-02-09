ccf4cba9fad137762ff714e5b8e87aad037cf8c7
Kvel2D <kvelyognaut@gmail.com>
2023-02-09 19:58:45 +0100

    Remove usage of script path for buff id's
    Defining a buff is mandatory through ctor parameter
    Having two ways to define buff id's is confusing and error-prone

--- Scenes/Buffs/Slow.gd
+++ Scenes/Buffs/Slow.gd
@@ -2,7 +2,7 @@ class_name Slow
 extends Buff
 
 
-func _init():
+func _init().("slow"):
 	var slow_modifier: Modifier = Modifier.new()
 	slow_modifier.add_modification(Modification.Type.MOD_MOVE_SPEED_ABSOLUTE, -100.0, 0.01)
 	.set_modifier(slow_modifier)
