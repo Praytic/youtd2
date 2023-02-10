2db8f3cb73f414f2a58a3ba2b2a234ba3adff77e
Kvel2D <kvelyognaut@gmail.com>
2023-02-10 11:02:51 +0100

    Make some Event members private
    To indicate that they are set once on creation and don't change after
    Event.damage remains public because it's allowed to change it

--- Scenes/Towers/Event.gd
+++ Scenes/Towers/Event.gd
@@ -1,17 +1,38 @@
 class_name Event
 extends Node
 
-var buff
+var _buff
+# NOTE: damage may be modified in event handlers to change
+# the final effect of the event
 var damage: float
 # target is of type Unit, can't use typing because of cyclic dependency...
-var target
+var _target
 # This flag is to prevent infinite recursion from
 # damage/damaged events. For example, if a tower does splash
 # damage to mobs around the mob it attacks, then the target
 # of the attack will be the main target, while mobs hit by
 # splash will not.
-var is_main_target: bool = false
+var _is_main_target: bool = false
+
+
+func _init(target, damage_arg, is_main_target_arg: bool):
+	_target = target
+	damage = damage_arg
+	_is_main_target = is_main_target_arg
+
 
 
 func _ready():
 	pass
+
+
+func get_buff():
+	return _buff
+
+
+func get_target():
+	return _target
+
+
+func is_main_target() -> bool:
+	return _is_main_target
