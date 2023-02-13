5b65b74eb3a5ea5710778901900decb1ea544ca1
Kvel2D <kvelyognaut@gmail.com>
2023-02-13 21:57:50 +0100

    Add floating text

---
+++ Scenes/FloatingText.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+
+onready var tween: Tween = $Tween
+onready var label: Label = $Label
+
+var text: String = "placeholder"
+var duration: float = 1.0
+
+
+func _ready():
+	label.text = text
+
+# 	Text pops out at the start
+	tween.interpolate_property(self, "scale",
+		Vector2(0, 0),
+		Vector2(1.0, 1.0),
+		0.3 * duration, Tween.TRANS_QUART, Tween.EASE_OUT)
+
+# 	Text fades to transparent after popping out
+	tween.interpolate_property(self, "modulate",
+		Color.white,
+		Color(1, 1, 1, 0),
+		0.3 * duration, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.7 * duration)
+
+# 	Text shrinks while fading
+	tween.interpolate_property(self, "scale",
+		Vector2(1.0, 1.0),
+		Vector2(0.4, 0.4),
+		1.0 * duration, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.6 * duration)
+
+# 	Text bounces
+	tween.interpolate_property(self, "position",
+		Vector2(0, 0),
+		Vector2(50, -50),
+		1.0 * duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+
+	tween.interpolate_callback(self, 1.0 * duration, "queue_free")
+
+	tween.start()
--- Scenes/Towers/Instances/ObeliskOfFortuity1.gd
+++ Scenes/Towers/Instances/ObeliskOfFortuity1.gd
@@ -1,3 +1,25 @@
 extends Tower
 
-# TODO: script
+
+const _tier_stats_map: Dictionary = {
+	1: {value = 0.15, value_add = 0.01},
+	2: {value = 0.17, value_add = 0.011},
+	3: {value = 0.19, value_add = 0.012},
+	4: {value = 0.21, value_add = 0.013},
+	5: {value = 0.23, value_add = 0.014},
+	6: {value = 0.25, value_add = 0.016},
+}
+
+
+func _ready():
+#	NOTE: splash values are the same for all tiers
+	var warming_up = Buff.new("warming_up")
+	warming_up.add_event_handler(Buff.EventType.DAMAGE, self, "on_damage")
+	warming_up.apply_to_unit_permanent(self, self, 0, true)
+
+
+func on_damage(event: Event):
+	if true:
+	# if calc_bad_chance(0.3 - get_level() * 0.006):
+		event.damage = 0
+		Utils.display_floating_text_x("Miss", self, Color.red, 0.05, 0.0, 2.0)
