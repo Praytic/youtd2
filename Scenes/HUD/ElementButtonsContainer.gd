aba776bd3eb3d5a284aa51c22bc34557ed423add
Kvel2D <kvelyognaut@gmail.com>
2023-02-16 21:34:23 +0100

    Fix errors from button pressed signals
    Button's pressed() is already defined and with 0 arguments,
    \ so emitting it with argument "element" causes problems
    Connect to button pressed() signals in script and pass "element"
    \ var as bind parameter

--- Scenes/HUD/ElementButtonsContainer.gd
+++ Scenes/HUD/ElementButtonsContainer.gd
@@ -10,10 +10,6 @@ func _on_BuildingMenuButton_pressed():
 	show()
 
 
-func _on_Button_pressed(element):
-	hide()
-
-
 func _unhandled_input(event):
 	if event.is_action_released("ui_cancel"):
 		hide()
