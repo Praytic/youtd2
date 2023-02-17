5c4d94cfe109077df54894b03224553dbde0f4d7
Kvel2D <kvelyognaut@gmail.com>
2023-02-17 20:45:22 +0100

    Add RuinedSunPedestal

---
+++ Scenes/Towers/Instances/RuinedSunPedestal1.gd
@@ -0,0 +1,30 @@
+extends Tower
+
+
+# TODO: implement visual
+
+
+const _stats_map: Dictionary = {
+	1: {bounce_decrease = 0.50, undead_damage = 0.20, undead_damage_add = 0.020},
+	2: {bounce_decrease = 0.48, undead_damage = 0.21, undead_damage_add = 0.021},
+	3: {bounce_decrease = 0.46, undead_damage = 0.22, undead_damage_add = 0.022},
+	4: {bounce_decrease = 0.44, undead_damage = 0.23, undead_damage_add = 0.023},
+	5: {bounce_decrease = 0.42, undead_damage = 0.24, undead_damage_add = 0.024},
+	6: {bounce_decrease = 0.40, undead_damage = 0.25, undead_damage_add = 0.025},
+	7: {bounce_decrease = 0.38, undead_damage = 0.26, undead_damage_add = 0.026},
+}
+
+
+func _ready():
+	var bounce_attack_buff = BounceAttack.new(3, 0.5)
+	bounce_attack_buff.apply_to_unit_permanent(self, self, 0, true)
+
+
+func _get_specials_modifier() -> Modifier:
+	var tier: int = get_tier()
+	var stats = _stats_map[tier]
+
+	var specials_modifier: Modifier = Modifier.new()
+	specials_modifier.add_modification(Modification.Type.MOD_DMG_TO_UNDEAD, stats.undead_damage, stats.undead_damage_add)
+
+	return specials_modifier
