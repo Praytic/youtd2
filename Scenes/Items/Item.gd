a17c96da75ec0ad58c74bb130997ac38b0ed38a5
Kvel2D <kvelyognaut@gmail.com>
2023-02-19 11:24:08 +0100

    Get item properties directly from Properties
    Since they are immutable no point in storing them in instances

--- Scenes/Items/Item.gd
+++ Scenes/Items/Item.gd
@@ -16,53 +16,44 @@ enum CsvProperty {
 const cell_size = 32
 
 
-var _id: int setget ,get_id
-var _name: String setget ,get_name
-var _author: String setget ,get_author
-# enum Constants.Rarity
-var _rarity: int setget ,get_rarity
-var _cost: int setget ,get_cost
-var _description: String setget ,get_description
-var _required_wave_level: int setget ,get_required_wave_level
-
-
 #########################
 ### Code starts here  ###
 #########################
 
 
 func _ready():
-	var props: Dictionary = Properties.get_item_properties_by_filename(filename)
-	_id = props[CsvProperty.ID]
-	_name = props[CsvProperty.NAME]
-	_author = props[CsvProperty.AUTHOR]
-	_rarity = Constants.Rarity.get(props[CsvProperty.RARITY].to_upper())
-	_cost = props[CsvProperty.COST].to_int()
-	_description = props[CsvProperty.DESCRIPTION]
-	_required_wave_level = props[CsvProperty.REQUIRED_WAVE_LEVEL]
+	pass
 
 
 #########################
 ### Setters / Getters ###
 #########################
 
-func get_id():
-	return _id
+func get_id() -> int:
+	return get_property(CsvProperty.ID).to_int()
 
-func get_name():
-	return _name
+func get_name() -> String:
+	return get_property(CsvProperty.NAME)
 
-func get_author():
-	return _author
+func get_author() -> String:
+	return get_property(CsvProperty.AUTHOR)
 
-func get_rarity():
-	return _rarity
+func get_rarity() -> int:
+	var rarity_string: String = get_property(CsvProperty.DESCRIPTION)
+	var rarity: int = Constants.Rarity.get(rarity_string.to_upper())
 
-func get_cost():
-	return _cost
+	return rarity
 
-func get_description():
-	return _description
+func get_cost() -> int:
+	return get_property(CsvProperty.RARITY).to_int()
 
-func get_required_wave_level():
-	return _required_wave_level
+func get_description() -> String:
+	return get_property(CsvProperty.DESCRIPTION)
+
+func get_required_wave_level() -> int:
+	return get_property(CsvProperty.REQUIRED_WAVE_LEVEL).to_int()
+
+func get_property(property: int) -> String:
+	var properties: Dictionary = Properties.get_item_properties_by_filename(filename)
+
+	return properties[property]
