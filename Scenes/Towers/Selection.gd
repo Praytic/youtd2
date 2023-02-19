c34bb83f37b5ca2ccc6b789bcda42412cfbd79b7
Kvel2D <kvelyognaut@gmail.com>
2023-02-19 10:00:33 +0100

    Remove unused sdsd var

--- Scenes/Towers/Selection.gd
+++ Scenes/Towers/Selection.gd
@@ -3,5 +3,4 @@ extends Node2D
 
 func _draw():
 	draw_set_transform_matrix(Transform2D.scaled(Vector2(1, 0.5)))
-	var sdsd = get_parent()
 	draw_arc(Vector2.ZERO, get_parent().cell_size, deg2rad(0), deg2rad(360), 100, Color.white, 1.5, true)
