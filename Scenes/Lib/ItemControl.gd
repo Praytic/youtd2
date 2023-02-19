597447f7c0b3067df6a593ba7f33a8bf8aa5a814
Kvel2D <kvelyognaut@gmail.com>
2023-02-19 11:26:48 +0100

    Fix item scene path

--- Scenes/Lib/ItemControl.gd
+++ Scenes/Lib/ItemControl.gd
@@ -23,7 +23,7 @@ func _on_Mob_death(event):
 		var scene_name_list: Array = Properties.get_item_scene_name_list()
 		var item_idx = random.randi_range(0, scene_name_list.size() - 1)
 		var item_scene_name = scene_name_list[item_idx]
-		var item_scene_path: String = "res://Scenes/Items/%s.tscn" % [item_scene_name]
+		var item_scene_path: String = "res://Scenes/Items/Instances/%s.tscn" % [item_scene_name]
 		var item_scene = load(item_scene_path)
 		var item_instance = item_scene.instance()
 		item_instance.position = event.get_target().position
