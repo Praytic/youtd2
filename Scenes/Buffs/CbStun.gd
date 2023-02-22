15aa7d29278d5d2c46cce4e55aefc5293c1121e7
Kvel2D <kvelyognaut@gmail.com>
2023-02-22 19:35:02 +0100

    Do stun visual using Effect system

--- Scenes/Buffs/CbStun.gd
+++ Scenes/Buffs/CbStun.gd
@@ -1,20 +1,22 @@
 class_name CbStun
 extends Buff
 
+var stun_effect: int
 
 func _init().("cb_stun"):
 	add_event_handler(Buff.EventType.CREATE, self, "_on_create")
-	add_event_handler(Buff.EventType.EXPIRE, self, "_on_expire")
+	add_event_handler(Buff.EventType.CLEANUP, self, "_on_cleanup")
 
 
 func _on_create(_event: Event):
-	get_buffed_unit().movement_enabled = false
+	var target = get_buffed_unit()
 
-#	NOTE: adding stun visual to buff instead of mob for
-#	automatic removal when buff expires
-	var stun_visual = load("res://Scenes/StunVisual.tscn").instance()
-	add_child(stun_visual)
+	target.movement_enabled = false 
+
+	stun_effect = Effect.create_animated("res://Scenes/StunVisual.tscn", target.position.x, target.position.y, 0, 0)
 
 
-func _on_expire(_event: Event):
+func _on_cleanup(_event: Event):
 	get_buffed_unit().movement_enabled = true
+	
+	Effect.destroy_effect(stun_effect)
