3e9622d0b3d317175942f2f57cebf1159659c6a7
Kvel2D <kvelyognaut@gmail.com>
2023-02-24 20:21:08 +0100

    Add SmallPocketRocket tower

---
+++ Scenes/Towers/Instances/SmallPocketRocket.gd
@@ -0,0 +1,21 @@
+extends Tower
+
+# TODO: visual
+
+func _get_tier_stats() -> Dictionary:
+	return {
+		1: {dmg_to_mass = 0.20},
+		2: {dmg_to_mass = 0.20},
+		3: {dmg_to_mass = 0.20},
+		4: {dmg_to_mass = 0.20},
+		5: {dmg_to_mass = 0.20},
+	}
+
+
+func _ready():
+	var specials_modifier: Modifier = Modifier.new()
+	specials_modifier.add_modification(Modification.Type.MOD_DMG_TO_MASS, _stats.dmg_to_mass, 0.02)
+	add_modifier(specials_modifier)
+
+	var splash_attack_buff = SplashAttack.new({125: 0.55})
+	splash_attack_buff.apply_to_unit_permanent(self, self, 0, true)
