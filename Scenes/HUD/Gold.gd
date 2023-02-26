8d9a8028bd0839634bd459a11492f2af23d5b560
Kvel2D <kvelyognaut@gmail.com>
2023-02-26 22:16:58 +0100

    Fix resource counts not being updated
    label_text was loaded only once in ResourceStatusPanel._ready()
    After that, changing label_text only changed the variable
    Remove variable, change label text directly
    Initialize text values for resources labels
    Rename export var "label_text" to "default_label_text"

--- Scenes/HUD/Gold.gd
+++ Scenes/HUD/Gold.gd
@@ -1,8 +1,11 @@
 tool
 extends "res://Scenes/HUD/ResourceStatusPanel.gd"
 
-func _init():
+func _ready():
 	GoldManager.connect("gold_change", self, "_on_gold_change")
-	
+
+	var initial_value: int = int(GoldManager.gold)
+	_on_gold_change(initial_value)
+
 func _on_gold_change(new_value):
-	self.label_text = str(int(new_value))
+	set_label_text(str(int(new_value)))
