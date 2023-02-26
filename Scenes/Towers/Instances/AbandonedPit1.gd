559430b6e8cfb7801f927075102233fb9837126a
Kvel2D <kvelyognaut@gmail.com>
2023-02-26 22:17:15 +0100

    Add AbandonedPit tower

---
+++ Scenes/Towers/Instances/AbandonedPit1.gd
@@ -0,0 +1,18 @@
+extends Tower
+
+# TODO: visual
+
+func _get_tier_stats() -> Dictionary:
+	return {
+		1: {bounty_level_add = 0.005},
+		2: {bounty_level_add = 0.006},
+		3: {bounty_level_add = 0.007},
+		4: {bounty_level_add = 0.008},
+		5: {bounty_level_add = 0.009},
+	}
+
+
+func _ready():
+	var specials_modifier: Modifier = Modifier.new()
+	specials_modifier.add_modification(Modification.Type.MOD_BOUNTY_RECEIVED, 0.20, _stats.bounty_level_add)
+	add_modifier(specials_modifier)
