215dcf8c33d3e10f25964b22ec8f3e46a1c0d6f1
Kvel2D <kvelyognaut@gmail.com>
2023-03-05 08:14:46 +0100

    Comment out GroupingTileMap.get_used_rect()

--- Scenes/GroupingTileMap.gd
+++ Scenes/GroupingTileMap.gd
@@ -1,7 +1,10 @@
 extends TileMap
 
-func get_used_rect():
-	var used_rects = []
-	for tilemap in get_children():
-		used_rects.append(tilemap.get_used_rect())
-	return used_rects
+# TODO: fix
+# Getting error about this overriding a native function and so not getting
+# called.
+#func get_used_rect():
+#	var used_rects = []
+#	for tilemap in get_children():
+#		used_rects.append(tilemap.get_used_rect())
+#	return used_rects
