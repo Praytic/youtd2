ab23c4a62f606f18ed0d38f3eb7af1844c4e422d
Kvel2D <kvelyognaut@gmail.com>
2023-03-05 08:22:17 +0100

    get_extents() -> size

--- Scenes/Landscape.gd
+++ Scenes/Landscape.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var play_area: CollisionShape2D = $PlayArea/CollisionShape2D
 
 func get_play_area_size() -> Vector2:
-	return play_area.get_shape().get_extents()
+	return play_area.get_shape().size
 
 func get_play_area_pos() -> Vector2:
 	return play_area.global_position
