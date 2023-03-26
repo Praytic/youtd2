68b66a8d771231e673aa1166bfb8b4b8533ca337
Kvel2D <kvelyognaut@gmail.com>
2023-03-26 18:48:09 +0200

    Remove TODO's from item scripts

--- Scenes/Items/Instances/Item77.gd
+++ Scenes/Items/Instances/Item77.gd
@@ -2,9 +2,6 @@
 extends Item
 
 
-# TODO: visual
-
-
 func _item_init():
 	_modifier.add_modification(Modification.Type.MOD_ATTACKSPEED, -0.16, 0.0)
 	_modifier.add_modification(Modification.Type.MOD_EXP_RECEIVED, -0.16, 0.0)
