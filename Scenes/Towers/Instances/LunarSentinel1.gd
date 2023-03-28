f53c4e0de6b6e86b00e4869a3e5e4cfc564b6322
Kvel2D <kvelyognaut@gmail.com>
2023-03-28 18:52:39 +0200

    Rename Autocast.range
    Conflicted with native function range()

--- Scenes/Towers/Instances/LunarSentinel1.gd
+++ Scenes/Towers/Instances/LunarSentinel1.gd
@@ -22,7 +22,7 @@ func tower_init():
 	autocast.caster_art = ""
 	autocast.num_buffs_before_idle = 0
 	autocast.autocast_type = Autocast.Type.AC_TYPE_OFFENSIVE_UNIT
-	autocast.range = 1200
+	autocast.cast_range = 1200
 	autocast.target_self = false
 	autocast.target_art = "Abilities/Spells/Items/AIil/AIilTarget.mdl"
 	autocast.cooldown = 2
