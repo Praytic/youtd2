43d981574a91e02b3f417f01285d2b52c1cec2c2
Kvel2D <kvelyognaut@gmail.com>
2023-04-03 21:57:37 +0200

    Fix another body_entered() issue
    Emit signal outside of body_entered() slot

--- Scenes/Buffs/BuffRangeArea.gd
+++ Scenes/Buffs/BuffRangeArea.gd
@@ -9,6 +9,8 @@ signal unit_came_in_range(handler_object, handler_function, unit)
 var _target_type: TargetType
 var _handler_object: Node
 var _handler_function: String
+var _emit_signal_in_process: bool
+var _unit: Unit = null
 
 
 func _init():
@@ -20,6 +22,12 @@ func init(radius: float, target_type: TargetType, handler_object: Node, handler_
 	_target_type = target_type
 	_handler_object = handler_object
 	_handler_function = handler_function
+
+
+func _process(_delta: float):
+	if _emit_signal_in_process:
+		_emit_signal_in_process = false
+		unit_came_in_range.emit(_handler_object, _handler_function, _unit)
 
 
 func _on_body_entered(body: Node):
@@ -34,4 +42,5 @@ func _on_body_entered(body: Node):
 	var target_match: bool = _target_type.match(unit)
 
 	if target_match:
-		unit_came_in_range.emit(_handler_object, _handler_function, unit)
+		_emit_signal_in_process = true
+		_unit = unit
