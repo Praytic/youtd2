9012f430028f27f48f08194aaa8cc27e92e45c10
Praytic <chernogorovvm@gmail.com>
2023-04-05 11:12:04 -0700

    Increase buildable area from 2 to 3 tiles wide

--- Scenes/Landscape.gd
+++ Scenes/Landscape.gd
@@ -5,6 +5,7 @@ const BUILDABLE_AREA_LAYER: int = 3
 @onready var play_area: Area2D = $PlayArea
 @onready var play_area_shape: CollisionShape2D = $PlayArea/CollisionShape2D
 @onready var _tilemap: TileMap = $TileMap
+@onready var _buildable_area: TileMap = $BuildableArea
 @onready var camera: Camera2D = %Map/Camera2D
 
 
@@ -31,24 +32,24 @@ func get_play_area_pos() -> Vector2:
 # Returns cursor position if the area is not buildable
 # Returns tilemap position if the area is buildable
 func get_current_buildable_pos() -> Vector2:
-	var world_pos: Vector2 = _tilemap.get_local_mouse_position()
-	var map_pos: Vector2 = _tilemap.local_to_map(world_pos)
-	var cell_at_mouse: int = _tilemap.get_cell_source_id(BUILDABLE_AREA_LAYER, map_pos)
+	var world_pos: Vector2 = _buildable_area.get_local_mouse_position()
+	var map_pos: Vector2 = _buildable_area.local_to_map(world_pos)
+	var cell_at_mouse: int = _buildable_area.get_cell_source_id(0, map_pos)
 	var mouse_is_on_buildable_cell: bool = cell_at_mouse != -1
 
 	if mouse_is_on_buildable_cell:
-		var clamped_world_pos: Vector2 = _tilemap.map_to_local(map_pos)
-
-		return clamped_world_pos
+		var clamped_world_pos: Vector2 = _buildable_area.map_to_local(map_pos)
+		var clamped_global_pos = _buildable_area.to_global(clamped_world_pos)
+		return clamped_global_pos
 	else:
 		var mouse_pos: Vector2 = get_global_mouse_position()
 
 		return mouse_pos
 
 func can_build_at_mouse_pos() -> bool:
-	var world_pos: Vector2 = _tilemap.get_local_mouse_position()
-	var map_pos: Vector2 = _tilemap.local_to_map(world_pos)
-	var buildable_area_cell_exists_at_pos: bool = _tilemap.get_cell_source_id(BUILDABLE_AREA_LAYER, map_pos) != -1
+	var world_pos: Vector2 = _buildable_area.get_local_mouse_position()
+	var map_pos: Vector2 = _buildable_area.local_to_map(world_pos)
+	var buildable_area_cell_exists_at_pos: bool = _buildable_area.get_cell_source_id(0, map_pos) != -1
 
 	return buildable_area_cell_exists_at_pos
 
