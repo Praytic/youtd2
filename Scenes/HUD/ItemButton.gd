13c3c626016605b33e5bbefe42d3ba6daf7c3c0b
Kvel2D <kvelyognaut@gmail.com>
2023-04-08 18:38:25 +0200

    Move _get_item_button_icon() to Item
    So it can be used for item icons while dragging and also in
    \ tower inventory
    Make some Item f-ns static so that they can be used with just
    \ item id, without needing an item scene

--- Scenes/HUD/ItemButton.gd
+++ Scenes/HUD/ItemButton.gd
@@ -13,14 +13,12 @@ var tier_icon
 @onready var _icon_size: String : set = set_icon_size
 @onready var _tier_icons_s = preload("res://Assets/Towers/tier_icons_s.png")
 @onready var _tier_icons_m = preload("res://Assets/Towers/tier_icons_m.png")
-@onready var _item_icons_s = preload("res://Assets/Items/item_icons_s.png")
-@onready var _item_icons_m = preload("res://Assets/Items/item_icons_m.png")
 @onready var _item_button_fallback_icon = preload("res://Assets/icon.png")
 
 
 func _ready():
 	set_theme_type_variation("TowerButton")
-	icon = _get_item_button_icon("M")
+	icon = Item.get_icon(_item.get_id(), "M")
 
 
 # NOTE: turn off drawing tier icon so that item's icon gets drawn
@@ -31,7 +29,7 @@ func _ready():
 func set_icon_size(icon_size: String):
 	_icon_size = icon_size
 	tier_icon = _get_item_button_tier_icon(icon_size)
-	icon = _get_item_button_icon(icon_size)
+	icon = Item.get_icon(_item.get_id(), icon_size)
 
 
 func set_item(item: Item):
@@ -56,26 +54,3 @@ func _get_item_button_tier_icon(icon_size_letter: String) -> Texture2D:
 	
 	icon_out.set_region(Rect2(6 * icon_size, item_rarity * icon_size, icon_size, icon_size))
 	return icon_out
-
-func _get_item_button_icon(icon_size_letter: String) -> Texture2D:
-	var icon_atlas_num: int = _item.get_icon_atlas_num()
-	var icon_atlas_family: int = _item.get_icon_atlas_family()
-	if icon_atlas_num == -1 or icon_atlas_family == -1:
-		return _item_button_fallback_icon
-
-	var item_icon = AtlasTexture.new()
-	var icon_size: int
-	if icon_size_letter == "S":
-		item_icon.set_atlas(_item_icons_s)
-		icon_size = ICON_SIZE_S
-	elif icon_size_letter == "M":
-		item_icon.set_atlas(_item_icons_m)
-		icon_size = ICON_SIZE_M
-	else:
-		return _item_button_fallback_icon
-	
-	var region: Rect2 = Rect2(icon_atlas_num * icon_size, icon_atlas_family * icon_size, icon_size, icon_size)
-	item_icon.set_region(region)
-
-	return item_icon
-
