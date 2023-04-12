19f493165fad383029b717a0e4f6fb52fd489bef
Kvel2D <kvelyognaut@gmail.com>
2023-04-12 21:51:17 +0200

    Add EventBus
    Forgot to include in prev commit

---
+++ Singletons/EventBus.gd
@@ -0,0 +1,26 @@
+extends Node
+
+# Class for global events. Emitters should can use this to
+# emit global events. Subscribes can connect to global
+# signals.
+
+signal item_button_mouse_entered(item_id: int)
+signal item_button_mouse_exited()
+signal tower_button_mouse_entered(tower_id: int)
+signal tower_button_mouse_exited()
+
+
+func emit_item_button_mouse_entered(item_id: int):
+	item_button_mouse_entered.emit(item_id)
+
+
+func emit_item_button_mouse_exited():
+	item_button_mouse_exited.emit()
+
+
+func emit_tower_button_mouse_entered(tower_id: int):
+	tower_button_mouse_entered.emit(tower_id)
+
+
+func emit_tower_button_mouse_exited():
+	tower_button_mouse_exited.emit()
