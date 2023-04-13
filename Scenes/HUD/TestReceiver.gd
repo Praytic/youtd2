bb8cd3c3769fad2c87022260bb0339c65add38dd
Kvel2D <kvelyognaut@gmail.com>
2023-04-13 14:39:28 +0200

    Add missing TestReceiver file

---
+++ Scenes/HUD/TestReceiver.gd
@@ -0,0 +1,23 @@
+extends Control
+
+signal test_signal()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+func _on_right_menu_bar_test_signal():
+	pass # Replace with function body.
+
+
+func _on_right_menu_bar_element_changed(element):
+	pass # Replace with function body.
