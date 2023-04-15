411f44b768ea787dcb993fc1820abd2fa3d26eee
Kvel2D <kvelyognaut@gmail.com>
2023-04-15 17:03:06 +0200

    Render all actions in blender script
    Hardcoding action name is annoying, rendering all is okay
    \ because they go into separate folders anyway

--- Assets/render_8_direction_sprites.py
+++ Assets/render_8_direction_sprites.py
@@ -66,81 +66,78 @@ def render8directions_selected_objects(path):
             
             # dynamically set the last frame to render based on action
             scn.frame_end = int(bpy.context.active_object.animation_data.action.frame_range[1])
+ 
+            # create folder for animation
+            action_folder = os.path.join(path, a.name)
+            if not os.path.exists(action_folder):
+                os.makedirs(action_folder)
             
-            # set which actions you want to render.  Make sure to use the exact name of the action!
-            if a.name == "run":
-                
-                # create folder for animation
-                action_folder = os.path.join(path, a.name)
-                if not os.path.exists(action_folder):
-                    os.makedirs(action_folder)
-                
-                # loop through all 8 directions NOTE: these
-                # direcitons are based on convention for
-                # isometric world, where "North" is in
-                # "up-and-right" direction.
-                for angle in range(0, 360, 45):
-                    if angle == 0:
-                        cardinalDirection = "S"
-                    if angle == 45:
-                        cardinalDirection = "SE"
-                    if angle == 90:
-                        cardinalDirection = "E"
-                    if angle == 135:
-                        cardinalDirection = "NE"
-                    if angle == 180:
-                        cardinalDirection = "N"
-                    if angle == 225:
-                        cardinalDirection = "NW"
-                    if angle == 270:
-                        cardinalDirection = "W"
-                    if angle == 315:
-                        cardinalDirection = "SW"
-                        
-                    # set which angles we want to render.
-                    if (
-                        angle == 0
-                        or angle == 45
-                        or angle == 90
-                        or angle == 135
-                        or angle == 180
-                        or angle == 225
-                        or angle == 270
-                        or angle == 315
-                        ):
-                        
-                        # create folder for specific angle
-                        animation_folder = os.path.join(action_folder, cardinalDirection)
-                        if not os.path.exists(animation_folder):
-                            os.makedirs(animation_folder)
-                        
-                        # rotate the model for the new angle
-                        bpy.context.active_object.rotation_euler[2] = math.radians(angle)
-                        
-                        # the below is for the use case where the root needed to be translated.
+            # loop through all 8 directions NOTE: these
+            # direcitons are based on convention for
+            # isometric world, where "North" is in
+            # "up-and-right" direction.
+            for angle in range(0, 360, 45):
+                if angle == 0:
+                    cardinalDirection = "S"
+                if angle == 45:
+                    cardinalDirection = "SE"
+                if angle == 90:
+                    cardinalDirection = "E"
+                if angle == 135:
+                    cardinalDirection = "NE"
+                if angle == 180:
+                    cardinalDirection = "N"
+                if angle == 225:
+                    cardinalDirection = "NW"
+                if angle == 270:
+                    cardinalDirection = "W"
+                if angle == 315:
+                    cardinalDirection = "SW"
+                    
+                # set which angles we want to render.
+                if (
+                    angle == 0
+                    or angle == 45
+                    or angle == 90
+                    or angle == 135
+                    or angle == 180
+                    or angle == 225
+                    or angle == 270
+                    or angle == 315
+                    ):
+                    
+                    # create folder for specific angle
+                    animation_folder = os.path.join(action_folder, cardinalDirection)
+                    if not os.path.exists(animation_folder):
+                        os.makedirs(animation_folder)
+                    
+                    # rotate the model for the new angle
+                    bpy.context.active_object.rotation_euler[2] = math.radians(angle)
+                    
+                    # the below is for the use case where the root needed to be translated.
 #                        obRoot.rotation_euler[2] = math.radians(angle)
-                        
-                        
-                        # loop through and render frames.  Can set how "often" it renders.
-                        # Every frame is likely not needed.  Currently set to 2 (every other).
-                        for i in range(s.frame_start,s.frame_end,1):
-                            s.frame_current = i
+                    
+                    
+                    # loop through and render frames.  Can set how "often" it renders.
+                    # Every frame is likely not needed.  Currently set to 2 (every other).
+                    for i in range(s.frame_start,s.frame_end,1):
+                        s.frame_current = i
 
-                            s.render.filepath = (
-                                                animation_folder
-                                                + "\\"
-                                                + str(a.name)
-                                                + "_"
-                                                + str(cardinalDirection)
-                                                + "_"
-                                                + str(s.frame_current).zfill(3)
-                                                )
-                            bpy.ops.render.render( #{'dict': "override"},
-                                                  #'INVOKE_DEFAULT',  
-                                                  False,            # undo support
-                                                  animation=False, 
-                                                  write_still=True
-                                                 ) 
+                        s.render.filepath = (
+                                            animation_folder
+                                            + "\\"
+                                            + str(a.name)
+                                            + "_"
+                                            + str(cardinalDirection)
+                                            + "_"
+                                            + str(s.frame_current).zfill(3)
+                                            )
+                        bpy.ops.render.render( #{'dict': "override"},
+                                              #'INVOKE_DEFAULT',  
+                                              False,            # undo support
+                                              animation=False, 
+                                              write_still=True
+                                             ) 
                                         
                 # restore original rotation
                 bpy.context.active_object.rotation_euler[2] = math.radians(0)
