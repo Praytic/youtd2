15f520d99b8150234f444a1ab1d7865667287535
Kvel2D <kvelyognaut@gmail.com>
2023-04-27 19:34:00 +0200

    Optimize Aura
    Replace Aura2D with timed get_units_in_range()

--- Scenes/Buffs/Aura.gd
+++ Scenes/Buffs/Aura.gd
@@ -16,13 +16,12 @@ var _aura_effect_is_friendly: bool = false
 var _aura_effect: BuffType = null
 
 var _caster: Unit = null
+var _target_list: Array = []
 
 
 func _ready():
 # 	NOTE: supress "variable never used" warning 
 	_aura_effect_is_friendly = _aura_effect_is_friendly
-
-	Utils.circle_polygon_set_radius($Area2D/CollisionPolygon2D, _aura_range)
 
 	_caster.level_up.connect(_on_caster_level_up)
 
@@ -35,57 +34,60 @@ func get_level() -> int:
 	return _level + _caster.get_level() * _level_add
 
 
-func _on_area_2d_body_entered(body: Node2D):
-	if !_check_body_match(body):
-		return
-
-	var unit: Unit = body as Unit
-
-	var buff: Buff = _aura_effect.apply_advanced(_caster, unit, get_level(), get_power(), -1)
-	buff._applied_by_aura_count += 1
-
-
-func _on_area_2d_body_exited(body: Node2D):
-	if !_check_body_match(body):
-		return
-
-	var unit: Unit = body as Unit
-
-	var buff: Buff = unit.get_buff_of_type(_aura_effect)
-
-	if buff != null:
-		buff._applied_by_aura_count -= 1
-
-#		NOTE: remove buff only if it's not being applied by
-#		other aura's on other casters. For example if a
-#		target moves out of range of this aura but is still
-#		in range of an aura on another caster, it should
-#		still have the aura effect.
-		if buff._applied_by_aura_count == 0:
-			buff.remove_buff()
-
-
-func _check_body_match(body: Node2D) -> bool:
-	if !body is Unit:
-		return false
-
-	var unit: Unit = body as Unit
-
-	if !_target_self && unit == self:
-		return false
-
-	var target_match: bool = _target_type.match(unit)
-
-	return target_match
-
-
 # NOTE: when caster levels up, re-apply aura effect. apply()
 # will upgrade the aura effect.
 func _on_caster_level_up(_event: Event):
-	var unit_list: Array = Utils.get_units_in_range(_target_type, position, _aura_range)
+	for target in _target_list:
+		if !is_instance_valid(target):
+			continue
 
-	if !_target_self:
-		unit_list.erase(self)
+		_aura_effect.apply_advanced(_caster, target, get_level(), get_power(), -1)
 
-	for unit in unit_list:
-		_aura_effect.apply_advanced(_caster, unit, get_level(), get_power(), -1)
+
+func _on_timer_timeout():
+# 	Remove buff from units that have went out of range or
+# 	became invisible
+	var removed_target_list: Array = []
+	
+	for target in _target_list:
+		if !is_instance_valid(target):
+			removed_target_list.append(target)
+
+			continue
+
+		var distance: float = Isometric.vector_distance_to(position, target.position)
+		var out_of_range: bool = distance > _aura_range
+
+		if out_of_range || target.is_invisible():
+			removed_target_list.append(target)
+
+	for target in removed_target_list:
+		_target_list.erase(target)
+
+		if !is_instance_valid(target):
+			continue
+
+		var buff: Buff = target.get_buff_of_type(_aura_effect)
+
+		if buff != null:
+			buff._applied_by_aura_count -= 1
+
+			var buff_not_applied_by_any_aura: bool = buff._applied_by_aura_count == 0
+
+			if buff_not_applied_by_any_aura:
+				buff.remove_buff()
+
+# 	Apply buff to units in range
+	var units_in_range: Array = Utils.get_units_in_range(_target_type, global_position, _aura_range)
+
+	for unit in _target_list:
+		units_in_range.erase(unit)
+
+	for unit in units_in_range:
+		if !_target_self && unit == self:
+			continue
+
+		_target_list.append(unit)
+		
+		var buff: Buff = _aura_effect.apply_advanced(_caster, unit, get_level(), get_power(), -1)
+		buff._applied_by_aura_count += 1
