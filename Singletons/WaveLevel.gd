154955517809df004defd41aa1d504bca6ba72a7
Kvel2D <kvelyognaut@gmail.com>
2023-05-02 10:53:02 +0200

    Fix WaveLevel.get_current()
    Increment wave level when wave is cleared, not when wave starts
    Set wave level to 1 at game start
    Fixes not being able to build any towers before wave 1 starts
    Closes #71

--- Singletons/WaveLevel.gd
+++ Singletons/WaveLevel.gd
@@ -2,22 +2,30 @@ extends Node
 
 
 # Stores the wave level of the most recent wave that has
-# been spawned. Wave level changes when a new wave starts
-# spawning.
+# been spawned. Wave level changes when a wave is cleared.
 
 
 signal changed()
 
 
-# NOTE: on startup, no waves have been spawned yet so
-# current wave level is 0 and corresponds to no wave.
-var _wave_level: int = 0
+var _last_cleared_wave_level: int = 0
 
 
-func increment():
-	_wave_level += 1
-	changed.emit()
+@onready var _wave_spawner: WaveSpawner = get_tree().get_root().get_node("GameScene/Map/WaveSpawner")
 
 
+func _ready():
+	_wave_spawner.wave_ended.connect(_on_wave_ended)
+
+
+# Current wave level is the level of the current wave if the wave is in progress or the level of the next wave if a in
+# progress or the level of the next wave if 
 func get_current():
-	return _wave_level
+	var current_level: int = _last_cleared_wave_level + 1
+
+	return current_level
+
+
+func _on_wave_ended(wave: Wave):
+	_last_cleared_wave_level = wave.get_wave_number()
+	changed.emit()
