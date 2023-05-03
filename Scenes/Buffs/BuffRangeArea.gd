70699de85c4fcaa5095597c498c86c6af9c39840
Kvel2D <kvelyognaut@gmail.com>
2023-05-03 21:16:05 +0200

    Redo BuffRangeArea without Area2D

--- Scenes/Buffs/BuffRangeArea.gd
+++ Scenes/Buffs/BuffRangeArea.gd
@@ -1,35 +1,40 @@
 class_name BuffRangeArea
-extends Area2D
+extends Node2D
 
-# BuffRangeArea wraps an Area2D  with circle shape. Used by
-# Buff to implement the "unit comes in range" event.
+# BuffRangeArea emits a signal when a unit that matches the
+# defined target type comes in range. Used by buffs to
+# implement the "unit comes in range" event.
 
 signal unit_came_in_range(callable: Callable, unit: Unit)
 
 var _target_type: TargetType
 var _callable: Callable
-
-
-func _init():
-	body_entered.connect(_on_body_entered)
+var _radius: float
+var _prev_units_in_range: Array = []
 
 
 func init(radius: float, target_type: TargetType, callable: Callable):
-	Utils.circle_polygon_set_radius($CollisionPolygon2D, radius)
+	_radius = radius
 	_target_type = target_type
 	_callable = callable
 
 
-func _on_body_entered(body: Node):
-	if !body is Unit:
-		return
-	
-	var unit: Unit = body as Unit
+func _on_timer_timeout():
+	var all_units_in_range: Array = Utils.get_units_in_range(_target_type, global_position, _radius)
 
-	if unit.is_invisible():
-		return
+	var matching_units: Array = []
 
-	var target_match: bool = _target_type.match(unit)
+	for unit in all_units_in_range:
+		var target_match: bool = _target_type.match(unit)
+		var is_invisible: bool = unit.is_invisible()
 
-	if target_match:
-		unit_came_in_range.emit(_callable, unit)
+		if target_match && !is_invisible:
+			matching_units.append(unit)
+
+	for unit in matching_units:
+		var unit_just_came_in_range: bool = !_prev_units_in_range.has(unit)
+
+		if unit_just_came_in_range:
+			unit_came_in_range.emit(_callable, unit)
+
+	_prev_units_in_range = matching_units
