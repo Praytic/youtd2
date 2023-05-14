36a4d297cbd2bf5fe34ae00f8f495ad3aa35878a
Kvel2D <kvelyognaut@gmail.com>
2023-05-14 10:01:49 +0200

    Add comments to config items

--- Singletons/Config.gd
+++ Singletons/Config.gd
@@ -7,6 +7,7 @@ extends Node
 # repo.
 
 
+# Set starting research level for all elements.
 func starting_research_level() -> int:
 	return ProjectSettings.get_setting("application/config/starting_research_level") as int
 
@@ -16,18 +17,24 @@ func starting_gold() -> int:
 func starting_tomes() -> int:
 	return ProjectSettings.get_setting("application/config/starting_tomes") as int
 
+# Removes time delay between waves 
 func fast_waves_enabled() -> bool:
 	return ProjectSettings.get_setting("application/config/fast_waves") as bool
 
+# Displays a godot icon texture on the location of a spell dummy.
 func visible_spell_dummys_enabled() -> bool:
 	return ProjectSettings.get_setting("application/config/visible_spell_dummys") as bool
 
 func dev_controls_enabled() -> bool:
 	return ProjectSettings.get_setting("application/config/dev_controls") as bool
 
+# Adds a list of test items to item bar. List is defined in
+# ItemBar script.
 func add_test_item() -> bool:
 	return ProjectSettings.get_setting("application/config/add_test_item") as bool
 
+# Load all tower scenes on startup. Otherwise tower scenes
+# will be loaded when needed.
 func preload_all_towers_on_startup() -> bool:
 	return ProjectSettings.get_setting("application/config/preload_all_towers_on_startup") as bool
 
@@ -37,11 +44,17 @@ func build_version() -> String:
 func minimap_enabled() -> bool:
 	return ProjectSettings.get_setting("application/config/minimap_enabled") as bool
 
+# Turns on visible damage numbers for all tower attacks.
 func damage_numbers() -> bool:
 	return ProjectSettings.get_setting("application/config/damage_numbers") as bool
 
+# Disables requirements for building and upgrading towers.
+# You will be able to perform all actions even if you don't
+# have enough gold, tomes or research levels.
 func ignore_requirements() -> bool:
 	return ProjectSettings.get_setting("application/config/ignore_requirements") as bool
 
+# Enables sound effects. Currently disabled because sfx are
+# a work in progress.
 func sfx_enabled() -> bool:
 	return ProjectSettings.get_setting("application/config/sfx_enabled") as bool
