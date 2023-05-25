52217b4e18f9258558bd4d07ceb2219311bbae79
Kvel2D <kvelyognaut@gmail.com>
2023-05-25 21:51:55 +0200

    Increase item drop index
    Temporary hack until real calculation of z index is implemented

--- Scenes/Items/ItemDrop.gd
+++ Scenes/Items/ItemDrop.gd
@@ -16,6 +16,10 @@ func _ready():
 
 	selected.connect(_on_selected)
 
+# 	TODO: calculate correct z index so that item drop is
+# 	drawn behind creeps/towers when it should.
+	z_index = 100
+
 
 # NOTE: this must be called once after the itemdrop is created
 # but before it's added to game scene.
