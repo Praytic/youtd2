9b0696d2c38f82565d1761e66a255acb7ef81279
Praytic <chernogorovvm@gmail.com>
2023-05-24 23:25:26 -0700

    Remove ItemManager and enhance DevControls

--- Scenes/HUD/DevControls.gd
+++ Scenes/HUD/DevControls.gd
@@ -3,6 +3,7 @@ extends Control
 
 @onready var dev_control_buttons = get_tree().get_nodes_in_group("dev_control_button")
 @onready var dev_controls = get_tree().get_nodes_in_group("dev_control")
+@onready var positional_control: ItemList = $PositionalControl
 
 
 func _ready():
@@ -11,12 +12,22 @@ func _ready():
 	
 	for dev_control_button in dev_control_buttons:
 		dev_control_button.button_up.connect(_on_DevControlButton_button_up.bind(dev_control_button))
+	
+	# TODO
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
+func _unhandled_input(event):
+	if event is InputEventMouse:
+		var right_click: bool = event.is_action_released("right_click")
+		if right_click:
+			positional_control.show()
+			positional_control.position = event.position
+		else:
+			positional_control.hide()
 
 func _on_DevControlButton_button_up(button: Button):
 	var control_name = button.get_name().replace("Button", "")
@@ -24,3 +35,9 @@ func _on_DevControlButton_button_up(button: Button):
 		if dev_control.get_name() == control_name:
 			dev_control.show()
 			break
+
+func _on_PositionalControl_id_focused(id):
+	match id:
+		0: print_verbose("TODO")
+		_: push_error("Invalid index for positional control: %s" % id)
+
