386f9d097c02f4fabe9c8ded4f7da8f69cf728dd
Praytic <chernogorovvm@gmail.com>
2023-06-25 07:59:47 -0700

    Change animation speed for generated texture

--- Resources/Sprites/Mobs/CreepSprite.gd
+++ Resources/Sprites/Mobs/CreepSprite.gd
@@ -8,9 +8,11 @@ const ACTIONS = ["floating", "slow_run", "run", "death", "stunned"]
 const DIRECTIONS = ["E", "W", "S", "N"]
 const CELL_WIDTH = 512
 const COLUMNS = 4 
+const ANIMATION_FPS = 15.0
 
 
 func _ready():
+	
 	for action in ACTIONS:
 		for direction in DIRECTIONS:
 			var animation_name = "%s_%s" % [action, direction]
@@ -22,12 +24,10 @@ func _ready():
 				sprite_frames.clear(animation_name)
 
 			sprite_frames.add_animation(animation_name)
-			
-			print_verbose("Create [%s] animation frames for [%s] action." % [rows * COLUMNS, animation_name])
+			sprite_frames.set_animation_speed(animation_name, ANIMATION_FPS)
 			
 			for row in range(0, rows):
 				for col in range(0, COLUMNS):
-					print_verbose("Create [%s:%s] frame." % [row, col])
 					_create_animation_frame(animation_name, row, col, sprite_sheet_atlas)
 
 
@@ -36,8 +36,6 @@ func _create_animation_frame(anim, row, col, sprite_sheet):
 	frame.blit_rect(sprite_sheet.get_image(), Rect2(col * CELL_WIDTH, row * CELL_WIDTH, CELL_WIDTH, CELL_WIDTH), Vector2(0, 0))
 
 	var texture = ImageTexture.create_from_image(frame)
-	
-	print_verbose("[%s:%s] - %s" % [row, col, texture.get_size()])
 	
 	var frame_index = row * COLUMNS + col
 	sprite_frames.add_frame(anim, texture)
