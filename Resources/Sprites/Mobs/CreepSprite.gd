97d91b1fa2c60f26b0591012827c3edc7d150d27
Kvel2D <kvelyognaut@gmail.com>
2023-06-26 19:23:47 +0200

    Speed up CreepSprite.gd
    Use AtlasTexture instead of blit_rect() and Image
    Still takes 0.7s

--- Resources/Sprites/Mobs/CreepSprite.gd
+++ Resources/Sprites/Mobs/CreepSprite.gd
@@ -20,26 +20,25 @@ func _ready():
 			var sprite_sheet_path = "%s/%s.png" % [sprite_sheets_dir, animation_name]
 			var sprite_sheet_atlas = load(sprite_sheet_path)
 			var rows = sprite_sheet_atlas.get_height() / CELL_WIDTH
-			
+
 			if sprite_frames.has_animation(animation_name):
 				sprite_frames.clear(animation_name)
-			
+
 			sprite_frames.add_animation(animation_name)
 			sprite_frames.set_animation_speed(animation_name, ANIMATION_FPS)
-			
+
 			for row in range(0, rows):
 				for col in range(0, COLUMNS):
 					_create_animation_frame(animation_name, row, col, sprite_sheet_atlas)
-	
+
 	var end_time = Time.get_ticks_msec()
 	print_verbose("Generated animation frames in [%s] seconds." % [(end_time - start_time) / 1000.0])
 
 
 func _create_animation_frame(anim, row, col, sprite_sheet):
-	var frame = Image.create(CELL_WIDTH, CELL_WIDTH, false, Image.FORMAT_RGBA8)
-	frame.blit_rect(sprite_sheet.get_image(), Rect2(col * CELL_WIDTH, row * CELL_WIDTH, CELL_WIDTH, CELL_WIDTH), Vector2(0, 0))
+	var texture = AtlasTexture.new()
+	texture.atlas = sprite_sheet
+	texture.region = Rect2(col * CELL_WIDTH, row * CELL_WIDTH, CELL_WIDTH, CELL_WIDTH)
 
-	var texture = ImageTexture.create_from_image(frame)
-	
 	var frame_index = row * COLUMNS + col
 	sprite_frames.add_frame(anim, texture)
