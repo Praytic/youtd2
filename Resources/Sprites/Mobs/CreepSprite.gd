15b5dc58ef7db052371527ae39308941244714ba
Praytic <chernogorovvm@gmail.com>
2023-06-29 18:12:36 -0700

    Add orc_mass sprites and remove empty frames from generated SpriteFrames

--- Resources/Sprites/Mobs/CreepSprite.gd
+++ Resources/Sprites/Mobs/CreepSprite.gd
@@ -19,6 +19,10 @@ func _ready():
 			var animation_name = "%s_%s" % [action, direction]
 			var sprite_sheet_path = "%s/%s.png" % [sprite_sheets_dir, animation_name]
 			var sprite_sheet_atlas = load(sprite_sheet_path)
+			
+			if not sprite_sheet_atlas:
+				continue
+			
 			var rows = sprite_sheet_atlas.get_height() / CELL_WIDTH
 
 			if sprite_frames.has_animation(animation_name):
@@ -39,6 +43,9 @@ func _create_animation_frame(anim, row, col, sprite_sheet):
 	var texture = AtlasTexture.new()
 	texture.atlas = sprite_sheet
 	texture.region = Rect2(col * CELL_WIDTH, row * CELL_WIDTH, CELL_WIDTH, CELL_WIDTH)
+	if _is_valid_frame(texture):
+		var frame_index = row * COLUMNS + col
+		sprite_frames.add_frame(anim, texture)
 
-	var frame_index = row * COLUMNS + col
-	sprite_frames.add_frame(anim, texture)
+func _is_valid_frame(texture_frame: AtlasTexture):
+	return texture_frame.get_image().get_used_rect().size != Vector2i(0, 0)
