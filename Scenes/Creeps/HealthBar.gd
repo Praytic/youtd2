4400ab25f310c973de7946c65dee293496b3c376
Praytic <chernogorovvm@gmail.com>
2023-06-30 17:08:36 -0700

    Fix healthbars

--- Scenes/Creeps/HealthBar.gd
+++ Scenes/Creeps/HealthBar.gd
@@ -1,27 +1,18 @@
 @tool
 extends ProgressBar
 
-
-@export var _health_style_box: StyleBoxFlat
 @export var color3: Color
 @export var color2: Color
 @export var color1: Color
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	remove_theme_stylebox_override("Fill")
-	add_theme_stylebox_override("Fill", _health_style_box)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _on_value_changed(value):
 	var health_left = value / max_value
 	if health_left > 0.75:
-		_health_style_box.bg_color = color3
+		get_theme_stylebox("fill").bg_color = color3
 	elif health_left > 0.5:
-		_health_style_box.bg_color = lerp(color2, color3, (value / max_value - 0.5) * 4)
+		get_theme_stylebox("fill").bg_color = lerp(color2, color3, (value / max_value - 0.5) * 4)
 	elif health_left > 0.25:
-		_health_style_box.bg_color = lerp(color1, color2, (value / max_value - 0.25) * 4)
+		get_theme_stylebox("fill").bg_color = lerp(color1, color2, (value / max_value - 0.25) * 4)
 	else:
-		_health_style_box.bg_color = color1
-
+		get_theme_stylebox("fill").bg_color = color1
