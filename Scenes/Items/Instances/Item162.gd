e61c5e0e9af3aa8365f6849316285e5034943da3
Kvel2D <kvelyognaut@gmail.com>
2023-07-23 13:47:09 +0200

    Implement add_modified_attack_crit()
    Finish script for Item162
    Remove Item162 from disabled list

--- Scenes/Items/Instances/Item162.gd
+++ Scenes/Items/Instances/Item162.gd
@@ -1,8 +1,14 @@
 # Glaive of Supreme Follow Up
 extends Item
 
-# TODO: implement add_modified_attack_crit(). Disabled until
-# then.
+
+# NOTE: original script uses buff.userInt as a flag so that
+# attack() f-n does nothing the first time it is called and
+# does something the second time it is called. This is to
+# solve some issue which exists in original script engine.
+# In current engine this is not needed and causes a bug
+# where follow up attack to happen twice. So it was removed.
+
 
 var BT: BuffType
 
@@ -12,6 +18,11 @@ func get_extra_tooltip_text() -> String:
 
 	text += "[color=GOLD]Follow Up[/color]\n"
 	text += "Whenever this tower attacks it has a 10% chance to gain 300% attackspeed until next attack. The next attack will crit for sure but deals 50% less crit damage.\n"
+	text += "\n"
+	text += "[color=GOLD]Level Bonus:[/color]\n"
+	text += "+0.4% chance\n"
+	text += "+4% attackspeed\n"
+	text += "+1% crit damage\n"
 
 	return text
 
@@ -24,11 +35,8 @@ func attack(event: Event):
 	var B: Buff = event.get_buff()
 	var t: Tower = B.get_buffed_unit()
 
-	if B.user_int == 0:
-		# t.add_modified_attack_crit(0.00, 0.5 + t.get_level() / 100.0)
-		B.remove_buff()
-	else:
-		B.user_int = 0
+	t.add_modified_attack_crit(0.00, 0.5 + t.get_level() / 100.0)
+	B.remove_buff()
 
 
 func item_init():
@@ -42,5 +50,11 @@ func item_init():
 	BT.set_buff_tooltip("Follow Up\nThis unit is affected by Follow Up; it's next attack will be faster and will always be critical.")
 
 
-func on_attack(event: Event):
+func on_attack(_event: Event):
 	var itm: Item = self
+	var tower: Tower = itm.get_carrier()
+
+	if !tower.calc_chance(0.1 + 0.004 * tower.get_level()):
+		return
+
+	BT.apply(itm.get_carrier(), itm.get_carrier(), itm.get_carrier().get_level())
