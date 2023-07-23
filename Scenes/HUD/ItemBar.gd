a84be158ed8b03253b088dcaf4b9939eb59ab091
Kvel2D <kvelyognaut@gmail.com>
2023-07-23 13:10:13 +0200

    Delete unneeded comment

--- Scenes/HUD/ItemBar.gd
+++ Scenes/HUD/ItemBar.gd
@@ -52,8 +52,6 @@ func on_item_move_from_itembar_done(move_success: bool):
 
 func _create_ItemButton(item: Item) -> ItemButton:
 	var item_button: ItemButton = ItemButton.make(item)
-#	NOTE: attach item to button while item is stored in item
-#	bar.
 	item_button.hide_cooldown_indicator()
 #	NOTE: While item is not parented to tower, parent it to button
 	item_button.add_child(item)
