5bbace3d13bb4645e16d756b8fa31736a8f1fb48
Kvel2D <kvelyognaut@gmail.com>
2023-07-26 20:14:10 +0200

    Add Consumable Chicken item

---
+++ Scenes/Items/Instances/Item2003.gd
@@ -0,0 +1,9 @@
+# Consumable Chicken
+extends Item
+
+
+func on_consume():
+	print_verbose("Consumable Chicken was used. Adding 2 food (not implemented.")
+
+# 	TODO: uncomment when FoodManager.add_food() is implemented.
+	# FoodManager.add_food(2)
