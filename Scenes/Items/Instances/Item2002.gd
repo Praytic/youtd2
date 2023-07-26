dca12d83bbb14d4ed3813c6417ae6bcc77da2eec
Kvel2D <kvelyognaut@gmail.com>
2023-07-26 18:51:13 +0200

    Add 2 consumable items

---
+++ Scenes/Items/Instances/Item2002.gd
@@ -0,0 +1,9 @@
+# Consumable Plant
+extends Item
+
+
+func on_consume():
+	print_verbose("Consumable Plant was used. Adding 1 food (not implemented.")
+
+# 	TODO: uncomment when FoodManager.add_food() is implemented.
+	# FoodManager.add_food(1)
--- Singletons/Managers/FoodManager.gd
+++ Singletons/Managers/FoodManager.gd
@@ -2,3 +2,6 @@ extends Node
 
 var cur_food: int = 0
 var max_food: int = Properties.globals["ini_food"]
+
+# TODO: implement FoodManager.add_food()
+# Needs to work same way as same f-n for tomes
