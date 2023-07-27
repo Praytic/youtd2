c2aa1a19c2ac31b57647cb08217a38edc03a9f46
Praytic <chernogorovvm@gmail.com>
2023-07-26 20:36:40 -0700

    Create UnitBtn.gd script

--- Scenes/HUD/Buttons/UnitBtn.gd
+++ Scenes/HUD/Buttons/UnitBtn.gd
@@ -1,67 +1,31 @@
 extends Button
 
 
-@onready var _unit_button: TextureButton = get_node("UnitButton")
-@onready var _disabled_lock: TextureRect = $UnitButton/LockTexture
-@onready var _counter: Container = $UnitButton/CounterContainer
-@onready var _counter_label: Label = $UnitButton/CounterContainer/CounterLabel
-@onready var _icon_rect: TextureRect = $UnitButton/IconContainer/Icon
-
-const _fallback_icon: Texture2D = preload("res://Assets/icon.png")
-const common_unit_button_texture = preload("res://Resources/Textures/UI/common_unit_button.tres")
-const uncommon_unit_button_texture = preload("res://Resources/Textures/UI/uncommon_unit_button.tres")
-const rare_unit_button_texture = preload("res://Resources/Textures/UI/rare_unit_button.tres")
-const unique_unit_button_texture = preload("res://Resources/Textures/UI/unique_unit_button.tres")
-const common_unit_button_hover_texture = preload("res://Resources/Textures/UI/common_unit_button_hover.tres")
-const uncommon_unit_button_hover_texture = preload("res://Resources/Textures/UI/uncommon_unit_button_hover.tres")
-const rare_unit_button_hover_texture = preload("res://Resources/Textures/UI/rare_unit_button_hover.tres")
-const unique_unit_button_hover_texture = preload("res://Resources/Textures/UI/unique_unit_button_hover.tres")
-
-var _unit_icon: Texture2D = _fallback_icon : set = set_unit_icon
-var _rarity: String: set = set_rarity
-var _count: int : set = set_count
-var _disabled: bool: set = set_disabled
+@onready var _disabled_lock: TextureRect = $%LockTexture
+@onready var _rarity_container: TextureRect = $%RarityContainer
 
 
-func set_disabled(value):
-	_disabled = value
-	_unit_button.set_disabled(value)
-	if value:
+var _rarity: String: get = get_rarity, set = set_rarity
+
+
+func _draw():
+	if disabled:
 		_disabled_lock.show()
 	else:
 		_disabled_lock.hide()
 
 
-func set_rarity(value):
+func get_rarity() -> String:
+	return _rarity
+
+func set_rarity(value: String):
 	_rarity = value
 	match Rarity.convert_from_string(value):
 		Rarity.enm.COMMON:
-			_unit_button.texture_normal = common_unit_button_texture
-			_unit_button.texture_hover = common_unit_button_hover_texture
+			_rarity_container.theme_type_variation = "CommonRarityPanelContainer"
 		Rarity.enm.UNCOMMON:
-			_unit_button.texture_normal = uncommon_unit_button_texture
-			_unit_button.texture_hover = uncommon_unit_button_hover_texture
+			_rarity_container.theme_type_variation = "UncommonRarityPanelContainer"
 		Rarity.enm.RARE:
-			_unit_button.texture_normal = rare_unit_button_texture
-			_unit_button.texture_hover = rare_unit_button_hover_texture
+			_rarity_container.theme_type_variation = "RareRarityPanelContainer"
 		Rarity.enm.UNIQUE:
-			_unit_button.texture_normal = unique_unit_button_texture
-			_unit_button.texture_hover = unique_unit_button_hover_texture
-
-
-func set_count(value: int):
-	_count = value
-	_counter_label.text = str(value)
-	if _count > 1:
-		_counter.show()
-	else:
-		_counter.hide()
-
-
-func set_unit_icon(value):
-	_icon_rect.texture = value
-	_unit_icon = value
-
-
-func get_button():
-	return _unit_button
+			_rarity_container.theme_type_variation = "UniqueRarityPanelContainer"
