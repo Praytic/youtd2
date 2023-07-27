95c9829dc9bf63f16a038b97bccffa4e298281a2
Kvel2D <kvelyognaut@gmail.com>
2023-07-27 19:45:34 +0200

    Remove unused function

--- Scenes/Unit.gd
+++ Scenes/Unit.gd
@@ -1374,20 +1374,6 @@ func get_experience_for_next_level():
 	return for_next_level
 
 
-func get_experience_for_prev_level():
-	if reached_max_level():
-		return 0
-	elif !Constants.EXP_FOR_LEVEL.has(_level):
-		push_error("No exp for level value for level: ", _level)
-
-		return 0
-
-	var next_level: int = _level + 1
-	var for_next_level: float = Constants.EXP_FOR_LEVEL[next_level]
-
-	return for_next_level
-
-
 # Changes experience of unit. Change can be positive or
 # negative. Level will also be changed accordingly. Note
 # that level downs are possible.
