e63b7c15498c9fb153de3ae9cb2b2f41f17e277d
Kvel2D <kvelyognaut@gmail.com>
2023-08-03 18:59:12 +0200

    Add HintsMenu

---
+++ Scenes/PauseHUD/HintsMenu.gd
@@ -0,0 +1,57 @@
+extends PanelContainer
+
+
+# Displays hints for the game. Loads the list of hints from
+# a CSV file. Left side of the menu contains a list of
+# titles, while the right side of the menu displays the text
+# of the selected hint.
+
+
+signal closed()
+
+
+const HINTS_CSV_PATH: String = "res://Data/hints.csv"
+
+var _text_list: Array[String] = []
+
+@onready var _tree: Tree = $VBoxContainer/HSplitContainer/Tree
+@onready var _hints_text_label: RichTextLabel = $VBoxContainer/HSplitContainer/MarginContainer/HintsTextLabel
+
+
+func _ready():
+	var csv: Array[PackedStringArray] = Utils.load_csv(HINTS_CSV_PATH)
+	
+	var root: TreeItem = _tree.create_item()
+	
+	for csv_line in csv:
+		var title: String = csv_line[0]
+		var text: String = csv_line[1]
+	
+		var child: TreeItem = _tree.create_item(root)
+		child.set_text(0, title)
+		
+		_text_list.append(text)
+	
+	var first_item: TreeItem = root.get_child(0)
+	_tree.set_selected(first_item, 0)
+
+
+func _on_close_button_pressed():
+	closed.emit()
+
+
+func _on_tree_item_selected():
+	var selected_item: TreeItem = _tree.get_selected()
+	var index: int = selected_item.get_index()
+	var text: String = _text_list[index]
+
+	_hints_text_label.clear()
+
+# 	NOTE: newlines don't work if you append the whole text.
+# 	Have to append newlines separately to make it work.
+	var lines: Array = text.split("\\n")
+
+	for line in lines:
+		_hints_text_label.append_text(line)
+		_hints_text_label.append_text("\n")
+	
--- Scenes/PauseHUD/PauseHUD.gd
+++ Scenes/PauseHUD/PauseHUD.gd
@@ -1,8 +1,29 @@
 extends Control
 
 
+enum PauseTab {
+	MAIN = 0,
+	HINTS,
+}
+
+
 signal resume_pressed()
+
+
+@onready var _tab_container: TabContainer = $TabContainer
 
 
 func _on_resume_button_pressed():
 	resume_pressed.emit()
+
+
+func _on_hints_button_pressed():
+	_tab_container.current_tab = PauseTab.HINTS
+
+
+func _on_hidden():
+	_tab_container.current_tab = PauseTab.MAIN
+
+
+func _on_hints_menu_closed():
+	_tab_container.current_tab = PauseTab.MAIN
