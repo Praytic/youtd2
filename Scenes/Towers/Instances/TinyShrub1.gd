420267534a203294a1c4e5ec5844ff939866f721
Kvel2D <kvelyognaut@gmail.com>
2023-08-04 17:49:24 +0200

    Fix specials for TinyShrub

--- Scenes/Towers/Instances/TinyShrub1.gd
+++ Scenes/Towers/Instances/TinyShrub1.gd
@@ -1,14 +1,22 @@
 extends Tower
 
 
+# NOTE: there are some discrepancies between values for
+# specials in tower descriptions vs actual values in JASS
+# code. In some places value in description is equal to
+# value in JASS code + base value for stat, in others it's
+# same as value in JASS code without adding base value for
+# stat. Used same values as in JASS code.
+
+
 func get_tier_stats() -> Dictionary:
 	return {
-		1: {crit_chance = 0.02, crit_chance_add = 0.0035, crit_damage = 0.0, crit_damage_add = 0.0},
-		2: {crit_chance = 0.04, crit_chance_add = 0.003, crit_damage = 1.4, crit_damage_add = 0.03},
-		3: {crit_chance = 0.05, crit_chance_add = 0.004, crit_damage = 1.5, crit_damage_add = 0.04},
-		4: {crit_chance = 0.07, crit_chance_add = 0.005, crit_damage = 1.7, crit_damage_add = 0.05},
-		5: {crit_chance = 0.08, crit_chance_add = 0.006, crit_damage = 1.8, crit_damage_add = 0.06},
-		6: {crit_chance = 0.10, crit_chance_add = 0.007, crit_damage = 2.0, crit_damage_add = 0.07},
+		1: {crit_chance = 0.0075, crit_chance_add = 0.002, crit_damage = 0.0, crit_damage_add = 0.0},
+		2: {crit_chance = 0.0275, crit_chance_add = 0.003, crit_damage = 0.15, crit_damage_add = 0.03},
+		3: {crit_chance = 0.0375, crit_chance_add = 0.004, crit_damage = 0.25, crit_damage_add = 0.04},
+		4: {crit_chance = 0.0575, crit_chance_add = 0.005, crit_damage = 0.45, crit_damage_add = 0.05},
+		5: {crit_chance = 0.0675, crit_chance_add = 0.006, crit_damage = 0.55, crit_damage_add = 0.06},
+		6: {crit_chance = 0.0875, crit_chance_add = 0.007, crit_damage = 0.75, crit_damage_add = 0.07},
 	}
 
 
