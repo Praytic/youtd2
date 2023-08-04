6e5492be14f6428427a770a0bcae4b9455ab2548
Kvel2D <kvelyognaut@gmail.com>
2023-08-04 17:08:56 +0200

    Fix typos in tower script

--- Scenes/Towers/Instances/BasicKnowledge1.gd
+++ Scenes/Towers/Instances/BasicKnowledge1.gd
@@ -12,12 +12,12 @@ func get_tier_stats() -> Dictionary:
 
 
 func get_extra_tooltip_text() -> String:
-	var exp: String = Utils.format_float(_stats.exp, 2)
+	var experience: String = Utils.format_float(_stats.exp, 2)
 
 	var text: String = ""
 
 	text += "[color=GOLD]High Knowledge[/color]\n"
-	text += "Grants 0.7 experience on attack.\n" % exp
+	text += "Grants %s experience on attack.\n" % experience
 
 	return text
 
@@ -26,6 +26,6 @@ func load_triggers(triggers: BuffType):
 	triggers.add_event_on_attack(on_attack)
 
 
-func on_attack(event: Event):
+func on_attack(_event: Event):
 	var tower: Tower = self
 	tower.add_exp(_stats.exp)
