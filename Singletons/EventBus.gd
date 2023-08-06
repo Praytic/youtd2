6925fa4cee78d54942d620d0c19caf4331715deb
Kvel2D <kvelyognaut@gmail.com>
2023-08-06 13:57:13 +0200

    Improve comment for EventBus

--- Singletons/EventBus.gd
+++ Singletons/EventBus.gd
@@ -1,8 +1,29 @@
 extends Node
 
-# Class for global events. Emitters should can use this to
-# emit global events. Subscribers can connect to global
-# signals.
+# Class for global events. Emitters can use this to emit
+# global events. Subscribers can connect to global signals.
+# EventBus is useful for cases where connecting signals
+# normally would require a long chain of connections.
+# 
+# For example:
+# 
+# ButtonTooltip needs to react to ItemButton's
+# mouse_entered() and mouse_exited() signals. Connecting
+# these two components normally would require the following
+# chain:
+# 
+# ItemButton->ItemBar->BottomMenuBar->HUD->ButtonTooltip
+# 
+# Connecting using Eventbus shortens the chain down to:
+# 
+# ItemButton->EventBus->ButtonTooltip
+# 
+# One way to think about EventBus is that it is like a
+# middle man available from anywhere in the code. If there
+# are two components which are "far away" from each other in
+# the tree, EventBus can act as a middle man to shorten the
+# distance between the components
+
 
 signal item_button_mouse_entered(item: Item)
 signal item_button_mouse_exited()
