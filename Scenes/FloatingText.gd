ba669d4d35c981591ec04f43f1cb14e1c1292a5b
Kvel2D <kvelyognaut@gmail.com>
2023-08-27 10:36:03 +0200

    Implement "go up" movement for floating text

--- Scenes/FloatingText.gd
+++ Scenes/FloatingText.gd
@@ -1,4 +1,10 @@
-extends Node2D
+class_name FloatingText extends Node2D
+
+
+enum Type {
+	BOUNCE_TO_RIGHT,
+	GO_UP,
+}
 
 
 @export var label: Label
@@ -6,6 +12,7 @@ var text: String = "placeholder"
 var duration: float = 1.0
 var color: Color = Color.WHITE
 var animated: bool = true
+var _type: Type = Type.BOUNCE_TO_RIGHT
 
 func _ready():
 	label.text = text
@@ -18,7 +25,19 @@ func _ready():
 	queue_free_tween.tween_callback(queue_free).set_delay(1.0)
 
 
+# NOTE: must be called before adding floating text
+# to the scene tree
+func set_type(type: FloatingText.Type):
+	_type = type
+
+
 func _setup_animation():
+	match _type:
+		Type.BOUNCE_TO_RIGHT: _setup_animation_bounce_to_right()
+		Type.GO_UP: _setup_animation_go_up()
+
+
+func _setup_animation_bounce_to_right():
 # 	Text bounces up and right
 	var pos_tween = create_tween()
 	pos_tween.tween_property(self, "position",
@@ -40,3 +59,17 @@ func _setup_animation():
 	modulate_tween.tween_property(self, "modulate",
 		Color(modulate.r, modulate.g, modulate.b, 0),
 		0.3 * duration).set_trans(Tween.TRANS_LINEAR).set_delay(0.7 * duration)
+
+
+func _setup_animation_go_up():
+# 	Text bounces up and right
+	var pos_tween = create_tween()
+	pos_tween.tween_property(self, "position",
+		Vector2(position.x, position.y - 100),
+		1.0 * duration).set_trans(Tween.TRANS_LINEAR)
+
+# 	Text fades away to nothing at the end
+	var modulate_tween = create_tween()
+	modulate_tween.tween_property(self, "modulate",
+		Color(modulate.r, modulate.g, modulate.b, 0),
+		0.3 * duration).set_trans(Tween.TRANS_LINEAR).set_delay(0.9 * duration)
