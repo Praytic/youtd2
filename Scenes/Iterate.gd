a1f7f76280d293b99f4e5138f773180dd1ea94fe
Kvel2D <kvelyognaut@gmail.com>
2023-08-30 20:17:08 +0200

    Simplify Iterate
    Do not need special logic for removing units which went out of range
    \ or adding units which went in range
    Only a few towers use Iterate together with await(), so this is not
    \ needed
    Leave removal of invalid units because it's still needed

--- Scenes/Iterate.gd
+++ Scenes/Iterate.gd
@@ -1,78 +1,68 @@
 class_name Iterate
 
 
-# NOTE: cannot use any typing for unit lists here, so no
-# "Array[Unit]" or "foo: Unit" is allowed. Using typing
-# causes runtime errors because stored unit references may
-# become invalid due to units dying, in which case their
-# references turn into "Object" type.
-
-enum NextOrder {
-	FRONT,
-	RANDOM,
-}
+# Iterate is a wrapper over get_units_in_range() f-n which
+# stores the resulting unit list and allows iterating over
+# units.
 
 
-var _caster: Unit
-var _center_unit: Unit
-var _center_unit_last_known_pos: Vector2
-var _center_pos: Vector2
-var _target_type: TargetType
-var _radius: float
-var _next_list: Array = []
-var _already_returned_list: Array = []
+var _next_list: Array[Unit] = []
+
+
+func _init(center_pos: Vector2, target_type: TargetType, radius: float):
+	_next_list = Utils.get_units_in_range(target_type, center_pos, radius)
 
 
 # NOTE: Iterate.overUnitsInRangeOf() in JASS
-static func over_units_in_range_of(caster: Unit, target_type: TargetType, x: float, y: float, radius: float) -> Iterate:
-	var it: Iterate = Iterate.new()
-	it._caster = caster
-	it._center_unit = null
-	it._center_pos = Vector2(x, y)
-	it._target_type = target_type
-	it._radius = radius
+static func over_units_in_range_of(_caster: Unit, target_type: TargetType, x: float, y: float, radius: float) -> Iterate:
+	var center_pos: Vector2 = Vector2(x, y)
+	var it: Iterate = Iterate.new(center_pos, target_type, radius)
 
 	return it
 
 
 # NOTE: Iterate.overUnitsInRangeOfCaster() in JASS
 static func over_units_in_range_of_caster(caster: Unit, target_type: TargetType, radius: float) -> Iterate:
-	var it: Iterate = Iterate.new()
-	it._caster = caster
-	it._center_unit = caster
-	it._center_unit_last_known_pos = it._center_unit.position
-	it._center_pos = Vector2.ZERO
-	it._target_type = target_type
-	it._radius = radius
+	var center_pos: Vector2 = caster.position
+	var it: Iterate = Iterate.new(center_pos, target_type, radius)
 
 	return it
 
 
 # NOTE: Iterate.overUnitsInRangeOfUnit() in JASS
-static func over_units_in_range_of_unit(caster: Unit, target_type: TargetType, center: Unit, radius: float) -> Iterate:
-	var it: Iterate = Iterate.new()
-	it._caster = caster
-	it._center_unit = center
-	it._center_unit_last_known_pos = it._center_unit.position
-	it._center_pos = Vector2.ZERO
-	it._target_type = target_type
-	it._radius = radius
+static func over_units_in_range_of_unit(_caster: Unit, target_type: TargetType, center: Unit, radius: float) -> Iterate:
+	var center_pos: Vector2 = center.position
+	var it: Iterate = Iterate.new(center_pos, target_type, radius)
 
 	return it
 
 
 # NOTE: iterate.next() in JASS
 func next() -> Unit:
-	var unit: Unit = _next_internal(NextOrder.FRONT)
+	_remove_invalid_units()
 
-	return unit
+	var next_unit: Unit
+
+	if !_next_list.is_empty():
+		next_unit = _next_list.pop_front()
+	else:
+		next_unit = null
+
+	return next_unit
 
 
 # NOTE: iterate.nextRandom() in JASS
 func next_random() -> Unit:
-	var unit: Unit = _next_internal(NextOrder.RANDOM)
+	_remove_invalid_units()
+	
+	var next_unit: Unit
 
-	return unit
+	if !_next_list.is_empty():
+		next_unit = _next_list.pick_random()
+	else:
+		next_unit = null
+
+	return next_unit
 
 
 # NOTE: Original API has this f-n but in Godot Iterate will
@@ -88,71 +78,13 @@ func count() -> int:
 	return _next_list.size()
 
 
-func _next_internal(next_order: NextOrder) -> Unit:
-#   NOTE: some tower scripts use Iterate together with
-#   sleeping so calls to next() may happen with a delay.
-#   Therefore we need consider that mobs may move between
-#   calls to next(). This is also why we need to check that
-#   units are still valid.
-
-#	Remove units that became invalid
+# NOTE: need to remove invalid units before each call to
+# next() because units may be killed or removed from the
+# game while Iterate is used.
+func _remove_invalid_units():
 	_next_list = _next_list.filter(
 		func(unit) -> bool:
 			var unit_is_valid: bool = Utils.unit_is_valid(unit)
 
-			if !unit_is_valid:
-				return false
-
-			return true
+			return unit_is_valid
 	)
-
-#	Remove units that went out of range
-	_next_list = _next_list.filter(
-		func(unit) -> bool:
-			var distance: float = Isometric.vector_distance_to(_get_center_pos(), unit.position)
-			var unit_is_in_range: bool = distance < _radius
-
-			if !unit_is_in_range:
-				return false
-
-			return true
-	)
-
-# 	If ran out units, add units that entered into range
-	if _next_list.is_empty():
-		var units_in_range: Array = Utils.get_units_in_range(_target_type, _get_center_pos(), _radius)
-
-		for unit in units_in_range:
-			var unit_already_returned: bool = _already_returned_list.has(unit)
-
-			if !unit_already_returned:
-				_next_list.append(unit)
-
-	if !_next_list.is_empty():
-		var next_unit: Unit
-
-		match next_order:
-			NextOrder.FRONT:
-				next_unit = _next_list.pop_front()
-			NextOrder.RANDOM:
-				next_unit = _next_list.pick_random()
-
-		_already_returned_list.append(next_unit)
-
-		return next_unit
-	else:
-		return null
-
-
-func _get_center_pos() -> Vector2:
-	if _center_unit != null:
-		var center_unit_is_valid: bool = Utils.unit_is_valid(_center_unit)
-
-		if center_unit_is_valid:
-			_center_unit_last_known_pos = _center_unit.position
-
-			return _center_unit.position
-		else:
-			return _center_unit_last_known_pos
-	else:
-		return _center_pos
