b900810bab3f27a9b4263fbf7525ba193951914e
Kvel2D <kvelyognaut@gmail.com>
2023-09-07 21:18:36 +0200

    Fix error in Item247 (Chameleons Soul)

--- Scenes/Items/Instances/Item247.gd
+++ Scenes/Items/Instances/Item247.gd
@@ -21,19 +21,19 @@ func on_drop():
 	var itm: Item = self
 	var tower: Tower = itm.get_carrier()
 
-	if tower.get_element() == Element.ASTRAL:
+	if tower.get_element() == Element.enm.ASTRAL:
 		tower.modify_property(Modification.Type.MOD_EXP_RECEIVED, -1.00)
-	elif tower.get_element() == Element.DARKNESS:
+	elif tower.get_element() == Element.enm.DARKNESS:
 		tower.modify_property(Modification.Type.MOD_SPELL_DAMAGE_DEALT, -0.45)
-	elif tower.get_element() == Element.NATURE:
+	elif tower.get_element() == Element.enm.NATURE:
 		tower.modify_property(Modification.Type.MOD_ATK_CRIT_CHANCE, -0.10)
-	elif tower.get_element() == Element.FIRE:
+	elif tower.get_element() == Element.enm.FIRE:
 		tower.modify_property(Modification.Type.MOD_DAMAGE_ADD_PERC, -0.40)
-	elif tower.get_element() == Element.ICE:
+	elif tower.get_element() == Element.enm.ICE:
 		tower.modify_property(Modification.Type.MOD_BUFF_DURATION, -0.50)
-	elif tower.get_element() == Element.STORM:
+	elif tower.get_element() == Element.enm.STORM:
 		tower.modify_property(Modification.Type.MOD_ATTACKSPEED, -0.25)
-	elif tower.get_element() == Element.IRON:
+	elif tower.get_element() == Element.enm.IRON:
 		tower.modify_property(Modification.Type.MOD_ITEM_CHANCE_ON_KILL, -0.30)
 
 
@@ -41,17 +41,17 @@ func on_pickup():
 	var itm: Item = self
 	var tower: Tower = itm.get_carrier()
 
-	if tower.get_element() == Element.ASTRAL:
+	if tower.get_element() == Element.enm.ASTRAL:
 		tower.modify_property(Modification.Type.MOD_EXP_RECEIVED, 1.00)
-	elif tower.get_element() == Element.DARKNESS:
+	elif tower.get_element() == Element.enm.DARKNESS:
 		tower.modify_property(Modification.Type.MOD_SPELL_DAMAGE_DEALT, 0.45)
-	elif tower.get_element() == Element.NATURE:
+	elif tower.get_element() == Element.enm.NATURE:
 		tower.modify_property(Modification.Type.MOD_ATK_CRIT_CHANCE, 0.10)
-	elif tower.get_element() == Element.FIRE:
+	elif tower.get_element() == Element.enm.FIRE:
 		tower.modify_property(Modification.Type.MOD_DAMAGE_ADD_PERC, 0.40)
-	elif tower.get_element() == Element.ICE:
+	elif tower.get_element() == Element.enm.ICE:
 		tower.modify_property(Modification.Type.MOD_BUFF_DURATION, 0.50)
-	elif tower.get_element() == Element.STORM:
+	elif tower.get_element() == Element.enm.STORM:
 		tower.modify_property(Modification.Type.MOD_ATTACKSPEED, 0.25)
-	elif tower.get_element() == Element.IRON:
+	elif tower.get_element() == Element.enm.IRON:
 		tower.modify_property(Modification.Type.MOD_ITEM_CHANCE_ON_KILL, 0.30)
