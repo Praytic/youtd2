e40923dea3f9e7e331910b1fd14153f274a3d66f
Kvel2D <kvelyognaut@gmail.com>
2023-09-07 07:14:39 +0200

    Implement immunity wave special
    Implemented the mechanic before but forgot to use it for the immunity
    \ wave special

--- Scenes/Buffs/CreepBuffs/CreepMagicImmunity.gd
+++ Scenes/Buffs/CreepBuffs/CreepMagicImmunity.gd
@@ -1,22 +1,14 @@
 class_name CreepMagicImmunity extends BuffType
 
-# TODO: this wave special is implemented but I don't
-# understand when creep's mana goes below 10. Creeps don't
-# spend mana on anything. So these creeps are always immune?
-# Doesn't make sense. Couldn't find towers that drain mana
-# from creeps.
-
 
 func _init(parent: Node):
 	super("creep_magic_immunity", 0, 0, true, parent)
-	add_event_on_damaged(on_damaged)
+	add_periodic_event(periodic, 0.1)
 
 
-func on_damaged(event: Event):
+func periodic(event: Event):
 	var buff: Buff = event.get_buff()
 	var creep: Unit = buff.get_buffed_unit()
 	var creep_mana: float = creep.get_mana()
-	var has_enough_mana: bool = creep_mana >= 10
-
-	if has_enough_mana && event.is_spell_damage():
-		event.damage = 0
+	var should_be_immune: bool = creep_mana >= 10
+	creep.set_immune(should_be_immune)
