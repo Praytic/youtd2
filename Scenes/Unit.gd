df74d3bca00a31fd9ec9071cfe2ce5cbbb88472c
Kvel2D <kvelyognaut@gmail.com>
2023-09-07 19:06:42 +0200

    Fix aoe damage calculation
    Was always using position of caster instead of position of aoe

--- Scenes/Unit.gd
+++ Scenes/Unit.gd
@@ -483,10 +483,11 @@ func _do_attack_damage_internal(target: Unit, damage_base: float, crit_ratio: fl
 # 
 # NOTE: unit.doAttackDamageAoEUnit() in JASS
 func do_attack_damage_aoe_unit(target: Unit, radius: float, damage: float, crit_ratio: float, sides_ratio: float):
-	var creep_list: Array = Utils.get_units_in_range(TargetType.new(TargetType.CREEPS), target.position, radius)
+	var aoe_center: Vector2 = Vector2(target.get_x(), target.get_y())
+	var creep_list: Array = Utils.get_units_in_range(TargetType.new(TargetType.CREEPS), aoe_center, radius)
 
 	for creep in creep_list:
-		var damage_for_creep: float = _get_aoe_damage(creep, radius, damage, sides_ratio)
+		var damage_for_creep: float = _get_aoe_damage(aoe_center, creep, radius, damage, sides_ratio)
 		do_attack_damage(creep, damage_for_creep, crit_ratio)
 
 
@@ -501,7 +502,7 @@ func do_spell_damage_aoe(x: float, y: float, radius: float, damage: float, crit_
 	var creep_list: Array = Utils.get_units_in_range(TargetType.new(TargetType.CREEPS), aoe_center, radius)
 
 	for creep in creep_list:
-		var damage_for_creep: float = _get_aoe_damage(creep, radius, damage, sides_ratio)
+		var damage_for_creep: float = _get_aoe_damage(aoe_center, creep, radius, damage, sides_ratio)
 		do_spell_damage(creep, damage_for_creep, crit_ratio)
 
 
@@ -710,8 +711,8 @@ func set_health_over_max(new_health: float):
 	health_changed.emit()
 
 
-func _get_aoe_damage(target: Unit, radius: float, damage: float, sides_ratio: float) -> float:
-	var distance: float = Isometric.vector_distance_to(position, target.position)
+func _get_aoe_damage(aoe_center: Vector2, target: Unit, radius: float, damage: float, sides_ratio: float) -> float:
+	var distance: float = Isometric.vector_distance_to(aoe_center, target.position)
 	var target_is_on_the_sides: bool = (distance / radius) > 0.5
 
 	if target_is_on_the_sides:
