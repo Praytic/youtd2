c77a6b51ddeae32faf9fe0cacba0d9246eef9633
Kvel2D <kvelyognaut@gmail.com>
2023-09-09 07:29:30 +0200

    Add "miss" floating text for evasion special

--- Scenes/Buffs/CreepBuffs/CreepEvasion.gd
+++ Scenes/Buffs/CreepBuffs/CreepEvasion.gd
@@ -13,7 +13,9 @@ func on_damaged(event: Event):
 static func evasion_effect(event: Event, chance: float):
 	var buff: Buff = event.get_buff()
 	var creep: Unit = buff.get_buffed_unit()
+	var tower: Unit = event.get_target()
 	var evade_success: bool = creep.calc_chance(chance)
 
 	if evade_success:
 		event.damage = 0
+		tower.get_player().display_floating_text_x("Miss", tower, 255, 0, 0, 255, 0.05, 0.0, 2.0)
