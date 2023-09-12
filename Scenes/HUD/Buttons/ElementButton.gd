ac5e9dc141fc6778031e4f162ce4fad4effd4182
Praytic <chernogorovvm@gmail.com>
2023-09-12 14:00:13 -0700

    Don't show 0 element level on hover

--- Scenes/HUD/Buttons/ElementButton.gd
+++ Scenes/HUD/Buttons/ElementButton.gd
@@ -24,8 +24,8 @@ func _ready():
 	mouse_exited.connect(_on_mouse_exited)
 	ElementLevel.changed.connect(_on_element_level_changed)
 	
-	texture_progress_bar.hide()
-	progress_label.hide()
+	_on_mouse_exited()
+	_on_element_level_changed()
 
 
 func _make_custom_tooltip(for_text: String) -> Object:
@@ -37,7 +37,10 @@ func _make_custom_tooltip(for_text: String) -> Object:
 func _on_element_level_changed():
 	var curent_element_level = ElementLevel.get_current(element)
 	texture_progress_bar.value = curent_element_level
-	progress_label.text = str(curent_element_level)
+	if curent_element_level == 0:
+		progress_label.text = ""
+	else:
+		progress_label.text = str(curent_element_level)
 
 
 func _on_mouse_entered():
