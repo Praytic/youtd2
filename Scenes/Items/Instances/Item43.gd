d9c0c748cd3da920613e9ad7cae41bad4abf1faf
Kvel2D <kvelyognaut@gmail.com>
2023-09-13 07:04:23 +0200

    Fix errors in items

--- Scenes/Items/Instances/Item43.gd
+++ Scenes/Items/Instances/Item43.gd
@@ -3,5 +3,5 @@ extends Item
 
 
 func load_modifier(modifier: Modifier):
-	modifier.add_modification(Modification.Type.MOD_ATTACKSPEED, 0.15, 0.0)
+	modifier.add_modification(Modification.Type.MOD_ATTACKSPEED, -0.15, 0.0)
 	modifier.add_modification(Modification.Type.MOD_DAMAGE_ADD_PERC, 0.50, 0.0)
