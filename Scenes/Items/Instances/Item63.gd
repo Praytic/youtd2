4c99818118e62c853112b9883528c5c76951f19d
Kvel2D <kvelyognaut@gmail.com>
2023-09-13 19:23:33 +0200

    Fix Item63 (Magic Vial)

--- Scenes/Items/Instances/Item63.gd
+++ Scenes/Items/Instances/Item63.gd
@@ -3,5 +3,5 @@ extends Item
 
 
 func load_modifier(modifier: Modifier):
-	modifier.add_modification(Modification.Type.MOD_MANA_REGEN, 0.10, 0.004)
+	modifier.add_modification(Modification.Type.MOD_MANA_REGEN_PERC, 0.10, 0.004)
 	modifier.add_modification(Modification.Type.MOD_BUFF_DURATION, 0.10, 0.004)
