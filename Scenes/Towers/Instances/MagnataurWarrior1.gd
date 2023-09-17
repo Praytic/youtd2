b61946e7be1ca9b6ec5e1f6675976bdc26986b04
Kvel2D <kvelyognaut@gmail.com>
2023-09-17 09:31:48 +0200

    Fix MagnataurWarrior
    Trigger type was incorrect

--- Scenes/Towers/Instances/MagnataurWarrior1.gd
+++ Scenes/Towers/Instances/MagnataurWarrior1.gd
@@ -30,7 +30,7 @@ func get_extra_tooltip_text() -> String:
 
 
 func load_triggers(triggers_buff_type: BuffType):
-	triggers_buff_type.add_event_on_attack(on_damage)
+	triggers_buff_type.add_event_on_damage(on_damage)
 
 
 func tower_init():
