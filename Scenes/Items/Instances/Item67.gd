cda6baf0def28aaea971b22ca613b7180fa0a438
Kvel2D <kvelyognaut@gmail.com>
2023-10-02 18:19:53 +0200

    Fix Floating Mark item
    Mod type was incorrect

--- Scenes/Items/Instances/Item67.gd
+++ Scenes/Items/Instances/Item67.gd
@@ -3,5 +3,5 @@ extends Item
 
 
 func load_modifier(modifier: Modifier):
-	modifier.add_modification(Modification.Type.MOD_DAMAGE_ADD, 0.02, 0.001)
+	modifier.add_modification(Modification.Type.MOD_DAMAGE_ADD_PERC, 0.02, 0.001)
 	modifier.add_modification(Modification.Type.MOD_ATK_CRIT_CHANCE, 0.02, 0.0)
