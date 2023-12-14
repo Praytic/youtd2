96ed6e9b8cd4dea0c9f5f8489ed460d2a95c582d
Kvel2D <kvelyognaut@gmail.com>
2023-12-14 17:30:32 +0100

    Fix Oil of Accuracy
    It had incorrect mod values

--- Scenes/Items/Instances/Item1007.gd
+++ Scenes/Items/Instances/Item1007.gd
@@ -3,5 +3,5 @@ extends Item
 
 
 func load_modifier(modifier: Modifier):
-	modifier.add_modification(Modification.Type.MOD_ATK_CRIT_CHANCE, 0.04, 0)
-	modifier.add_modification(Modification.Type.MOD_ATK_CRIT_DAMAGE, 0.25, 0)
+	modifier.add_modification(Modification.Type.MOD_ATK_CRIT_CHANCE, 0.008, 0)
+	modifier.add_modification(Modification.Type.MOD_ATK_CRIT_DAMAGE, 0.05, 0)
