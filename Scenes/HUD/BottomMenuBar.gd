3b969dba80e1f65082d536e931c4fb0b50114a2a
Praytic <chernogorovvm@gmail.com>
2023-12-16 14:25:48 -0800

    Enable part of BottomMenu to handle ItemMovement events

--- Scenes/HUD/BottomMenuBar.gd
+++ Scenes/HUD/BottomMenuBar.gd
@@ -9,3 +9,9 @@ func _ready():
 	HighlightUI.register_target("tomes_status", _tomes_status)
 	HighlightUI.register_target("gold_status", _gold_status)
 
+
+func _on_items_menu_gui_input(event):
+	var left_click: bool = event.is_action_released("left_click")
+
+	if left_click:
+		ItemMovement.item_stash_was_clicked()
