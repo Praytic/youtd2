6fd565ceb3b62d2397d8701f06f7a997ae985183
Praytic <chernogorovvm@gmail.com>
2023-12-21 15:24:51 -0800

    Finish designing ItemMenuCard

--- Scenes/HUD/ItemsMenuCard.gd
+++ Scenes/HUD/ItemsMenuCard.gd
@@ -8,11 +8,10 @@ extends PanelContainer
 @onready var _hidable_status_panels: Array = get_tree().get_nodes_in_group("hidable_status_panel")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_main_button.toggled.connect(_on_main_button_toggled)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_main_button_toggled(toggled):
+	for status_panel in _hidable_status_panels:
+		status_panel.visible = toggled
