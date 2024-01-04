48a0c6e108b1fc31aaa5b75ba9cf4f065334ac07
Praytic <chernogorovvm@gmail.com>
2024-01-04 15:09:31 -0800

    Remove intermediate script

--- Scenes/Towers/Visual2.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$SelectionOutline.position = $Sprite2D.position
-	$SelectionOutline.scale = $Sprite2D.scale
