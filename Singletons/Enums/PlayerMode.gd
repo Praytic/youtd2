422071d483ba477595a4dab1a666393e4bac2d31
Kvel2D <kvelyognaut@gmail.com>
2024-01-05 05:27:36 +0100

    Add missing PlayerMode.from_string()
    PlayerMode.from_string() was called in Config.gd but that
    \ function was not implemented

--- Singletons/Enums/PlayerMode.gd
+++ Singletons/Enums/PlayerMode.gd
@@ -8,3 +8,25 @@ enum enm {
 	# running in headless mode.
 	SERVER,
 }
+
+
+const _string_map: Dictionary = {
+	PlayerMode.enm.SINGLE: "single",
+	PlayerMode.enm.COOP: "coop",
+	PlayerMode.enm.SERVER: "server",
+}
+
+
+func convert_to_string(type: PlayerMode.enm):
+	return _string_map[type]
+
+
+func from_string(string: String) -> PlayerMode.enm:
+	var key = _string_map.find_key(string)
+	
+	if key != null:
+		return key
+	else:
+		push_error("Invalid string: \"%s\". Possible values: %s" % [string, _string_map.values()])
+
+		return PlayerMode.enm.SINGLE
