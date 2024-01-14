6bd399670751885535940904f56e32c6ea38c025
Praytic <chernogorovvm@gmail.com>
2024-01-14 02:38:30 -0800

    Update level_panel on unit.level_changed signal

--- Scenes/HUD/UnitStatusCard.gd
+++ Scenes/HUD/UnitStatusCard.gd
@@ -16,15 +16,28 @@ func _on_selected_unit_changed(prev_unit):
 	var selected_unit = SelectUnit.get_selected_unit()
 	
 	if selected_unit != null:
+		selected_unit.level_changed.connect(_update_level_panel)
 		if prev_unit == null:
 			get_main_button().set_pressed(true)
-		
-		_level_panel.set_count(selected_unit.get_level())
-		_level_panel.ack_count()
-		
+	
+	_update_level_panel()
+	_update_main_button_icon()
+
+
+func _update_main_button_icon():
+	var selected_unit = SelectUnit.get_selected_unit()
+	
+	if selected_unit != null:
 		var icon_texture
 		if selected_unit is Tower:
 			icon_texture = TowerProperties.get_icon_texture(selected_unit.get_id())
 		elif selected_unit is Creep:
 			icon_texture = CreepProperties.get_icon_texture(selected_unit.get_id())
 		get_main_button().set_button_icon(icon_texture)
+
+
+func _update_level_panel():
+	var selected_unit = SelectUnit.get_selected_unit()
+	if selected_unit != null:
+		_level_panel.set_count(selected_unit.get_level())
+		_level_panel.ack_count()
