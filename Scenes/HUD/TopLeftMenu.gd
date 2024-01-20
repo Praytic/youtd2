Praytic/youtd2

commit 4b9b2b0124bfee9b58326a4175af678b7a7c1c0b

Fix the bug when 'Next wave' button overlaps with other elements

Scenes/HUD/WaveStatus.tscn
@@ -1,7 +1,15 @@
-[gd_scene load_steps=3 format=3 uid="uid://4tcjyre0hoji"]
+[gd_scene load_steps=5 format=3 uid="uid://4tcjyre0hoji"]
 
 [ext_resource type="Theme" uid="uid://bql5sxaowafw3" path="res://Resources/Theme/wc3_theme.tres" id="1_3h6gw"]
 [ext_resource type="Script" path="res://Scenes/HUD/WaveStatus.gd" id="2_fsc15"]
+
+[sub_resource type="InputEventKey" id="InputEventKey_srlh2"]
+device = -1
+keycode = 78
+unicode = 110
+
+[sub_resource type="Shortcut" id="Shortcut_u7iub"]
+events = [SubResource("InputEventKey_srlh2")]
 
 [node name="WaveStatus" type="VBoxContainer" node_paths=PackedStringArray("_label", "_stats_label", "_timer_label")]
 custom_minimum_size = Vector2(650, 0)
@@ -10,11 +18,13 @@ offset_bottom = 134.0
 theme = ExtResource("1_3h6gw")
 script = ExtResource("2_fsc15")
 _label = NodePath("DetailsLabel")
-_stats_label = NodePath("StatsLabel")
+_stats_label = NodePath("HBoxContainer2/StatsLabel")
 _timer_label = NodePath("TimerLabel")
 
 [node name="TimerLabel" type="RichTextLabel" parent="."]
 layout_mode = 2
+size_flags_horizontal = 3
+size_flags_vertical = 4
 theme_override_constants/table_h_separation = 20
 bbcode_enabled = true
 text = "Wave [color=GOLD]2[/color]
@@ -31,7 +41,10 @@ scroll_active = false
 autowrap_mode = 0
 hint_underlined = false
 
-[node name="StatsLabel" type="RichTextLabel" parent="."]
+[node name="HBoxContainer2" type="HBoxContainer" parent="."]
+layout_mode = 2
+
+[node name="StatsLabel" type="RichTextLabel" parent="HBoxContainer2"]
 layout_mode = 2
 theme_override_constants/table_h_separation = 20
 bbcode_enabled = true
@@ -42,6 +55,16 @@ text = "
 "
 fit_content = true
 autowrap_mode = 0
+
+[node name="StartNextWaveButton" type="Button" parent="HBoxContainer2"]
+layout_mode = 2
+size_flags_horizontal = 0
+size_flags_vertical = 4
+tooltip_text = "Start next wave"
+focus_mode = 0
+theme_override_font_sizes/font_size = 25
+shortcut = SubResource("Shortcut_u7iub")
+text = "Next wave"
 
 [node name="UpdateStatsTimer" type="Timer" parent="."]
 autostart = true

Scenes/HUD/TopLeftMenu.tscn
@@ -1,4 +1,4 @@
-[gd_scene load_steps=8 format=3 uid="uid://b3grhp7bp5t54"]
+[gd_scene load_steps=6 format=3 uid="uid://b3grhp7bp5t54"]
 
 [ext_resource type="Script" path="res://Scenes/HUD/TopLeftMenu.gd" id="1_d4k6r"]
 [ext_resource type="Theme" uid="uid://bql5sxaowafw3" path="res://Resources/Theme/wc3_theme.tres" id="1_lhpbt"]
@@ -7,19 +7,11 @@
 
 [sub_resource type="StyleBoxEmpty" id="StyleBoxEmpty_hwtay"]
 
-[sub_resource type="InputEventKey" id="InputEventKey_srlh2"]
-device = -1
-keycode = 78
-unicode = 110
-
-[sub_resource type="Shortcut" id="Shortcut_gogd1"]
-events = [SubResource("InputEventKey_srlh2")]
-
 [node name="TopLeftMenu" type="PanelContainer" node_paths=PackedStringArray("_start_next_wave_button")]
 theme = ExtResource("1_lhpbt")
 theme_override_styles/panel = SubResource("StyleBoxEmpty_hwtay")
 script = ExtResource("1_d4k6r")
-_start_next_wave_button = NodePath("MarginContainer/StartNextWaveButton")
+_start_next_wave_button = NodePath("")
 
 [node name="TabContainer" type="TabContainer" parent="."]
 self_modulate = Color(1, 1, 1, 0.705882)
@@ -33,20 +25,6 @@ layout_mode = 2
 visible = false
 layout_mode = 2
 
-[node name="MarginContainer" type="MarginContainer" parent="."]
-layout_mode = 2
-mouse_filter = 2
-theme_override_constants/margin_right = 40
-theme_override_constants/margin_bottom = 40
+[connection signal="pressed" from="TabContainer/Wave Status/HBoxContainer2/StartNextWaveButton" to="." method="_on_start_next_wave_button_pressed"]
 
-[node name="StartNextWaveButton" type="Button" parent="MarginContainer"]
-layout_mode = 2
-size_flags_horizontal = 8
-size_flags_vertical = 8
-tooltip_text = "Start next wave"
-focus_mode = 0
-theme_override_font_sizes/font_size = 25
-shortcut = SubResource("Shortcut_gogd1")
-text = "Next wave"
-
-[connection signal="pressed" from="MarginContainer/StartNextWaveButton" to="." method="_on_start_next_wave_button_pressed"]
+[editable path="TabContainer/Wave Status"]

commit cb37eb51fd691e181bcdae011e6c015431853c2b

Fix crash on game over
Bug was introduced by 4b9b2b0124bfee9b58326a4175af678b7a7c1c0b because
\ "start next wave" button was moved to different scene but
\ the functions which use this button weren't moved

Scenes/HUD/TopLeftMenu.gd
@@ -5,11 +5,6 @@ extends PanelContainer
 @onready var _wave_spawner: WaveSpawner = get_tree().get_root().get_node("GameScene/Map/WaveSpawner")
 
 
-func _ready():
-	_wave_spawner.all_waves_started.connect(_on_all_waves_started)
-	EventBus.game_over.connect(_on_game_over)
-
-
 func _on_start_next_wave_button_pressed():
 	if !Globals.built_at_least_one_tower:
 		Messages.add_error("You have to build some towers before you can start a wave!")
@@ -19,11 +14,3 @@ func _on_start_next_wave_button_pressed():
 	var success: bool = _wave_spawner.force_start_next_wave()
 	if !success:
 		Messages.add_error("Can't start next wave, wave is still in progress.")
-
-
-func _on_all_waves_started():
-	_start_next_wave_button.disabled = true
-
-
-func _on_game_over():
-	_start_next_wave_button.disabled = true

Scenes/HUD/WaveStatus.gd
@@ -9,6 +9,7 @@ extends VBoxContainer
 @export var _stats_label: RichTextLabel
 @onready var _wave_spawner: WaveSpawner = get_tree().get_root().get_node("GameScene/Map/WaveSpawner")
 @export var _timer_label: RichTextLabel
+@export var _start_next_wave_button: Button
 
 var _armor_hint_map: Dictionary
 
@@ -18,6 +19,9 @@ var _armor_hint_map: Dictionary
 #########################
 
 func _ready():
+	_wave_spawner.all_waves_started.connect(_on_all_waves_started)
+	EventBus.game_over.connect(_on_game_over)
+
 	_armor_hint_map = _generate_armor_hints()
 
 	WaveLevel.changed.connect(_update_all_labels)
@@ -184,3 +188,11 @@ func _on_update_stats_timer_timeout():
 
 	_stats_label.clear()
 	_stats_label.append_text(text)
+
+
+func _on_all_waves_started():
+	_start_next_wave_button.disabled = true
+
+
+func _on_game_over():
+	_start_next_wave_button.disabled = true

Scenes/HUD/WaveStatus.tscn
@@ -11,7 +11,7 @@ unicode = 110
 [sub_resource type="Shortcut" id="Shortcut_u7iub"]
 events = [SubResource("InputEventKey_srlh2")]
 
-[node name="WaveStatus" type="VBoxContainer" node_paths=PackedStringArray("_label", "_stats_label", "_timer_label")]
+[node name="WaveStatus" type="VBoxContainer" node_paths=PackedStringArray("_label", "_stats_label", "_timer_label", "_start_next_wave_button")]
 custom_minimum_size = Vector2(650, 0)
 offset_right = 600.0
 offset_bottom = 134.0
@@ -20,6 +20,7 @@ script = ExtResource("2_fsc15")
 _label = NodePath("DetailsLabel")
 _stats_label = NodePath("HBoxContainer2/StatsLabel")
 _timer_label = NodePath("TimerLabel")
+_start_next_wave_button = NodePath("HBoxContainer2/StartNextWaveButton")
 
 [node name="TimerLabel" type="RichTextLabel" parent="."]
 layout_mode = 2