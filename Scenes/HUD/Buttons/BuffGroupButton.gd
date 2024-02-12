81229d7cc775481aaf13c4a6ad569b7c41fe09c8
Praytic <chernogorovvm@gmail.com>
2024-02-12 11:53:08 -0800

    Create visual feedback for BuffGroupButton modes

--- Scenes/HUD/Buttons/BuffGroupButton.gd
+++ Scenes/HUD/Buttons/BuffGroupButton.gd
@@ -11,12 +11,14 @@ enum Mode {
 const _modes_list = [Mode.NONE, Mode.OUTGOING, Mode.INCOMING, Mode.BOTH]
 
 var _current_mode_id: int = 0
+@export var _buff_group_number: int
 
 
 func _on_pressed():
 	_current_mode_id = (_current_mode_id + 1) % _modes_list.size()
+	text = "%s (%s)" % [_buff_group_number, get_current_mode()]
 
 
-func get_current_mode():
+func get_current_mode() -> Mode:
 	return _modes_list[_current_mode_id]
 
