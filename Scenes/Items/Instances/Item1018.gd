ab83a0ab74763ba174441d59c462b82bae1a87d0
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-02-28 07:59:37 +0100

    Fix rangeo of Seeker's Oil
    Original value of 250 was picked long ago
    Since then, there was an adjustment to Utils.get_units_in_range_PIXELS()
    \ which added (Constants.TILE_SIZE_PIXELS / 2) to radius for ranges
    \ between towers - to account of similar adjustment in original game
    Also, now it's possible to build towers in honey-comb pattern, so this
    \ new range will cover up to 4+3+4 honeycomb patterns
    Closes #393

--- Scenes/Items/Instances/Item1018.gd
+++ Scenes/Items/Instances/Item1018.gd
@@ -2,11 +2,10 @@
 class_name Item1018 extends Item
 
 
-# NOTE: this range covers all towers in 3x3 grid around
-# carrier. In original game this oil could hit 4+3+4 in
-# honeycomb pattern but currently in youtd2 you can't build
-# honeycomb patterns.
-static var SEEKER_OIL_RANGE: float = 250
+# NOTE: this range is a hand-picked value which hits 3x3
+# grid of towers around the main tower or 4+3+4 towers built
+# in honeycomb pattern.
+static var SEEKER_OIL_RANGE: float = 190
 const SEEKER_OIL_ID: int = 1018
 
 
