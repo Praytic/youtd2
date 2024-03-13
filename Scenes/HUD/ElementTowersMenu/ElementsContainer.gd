5921643ad851a88eaa24f9de8e1dfb6aca292bc0
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-03-13 18:25:54 +0100

    Use ButtonGroup for element container

--- Scenes/HUD/ElementTowersMenu/ElementsContainer.gd
+++ Scenes/HUD/ElementTowersMenu/ElementsContainer.gd
@@ -7,22 +7,27 @@ signal element_changed()
 var _current_element: Element.enm = Element.enm.ICE : get = get_element
 
 
+#########################
+###     Built-in      ###
+#########################
+
 func _ready():
 	HighlightUI.register_target("elements_container", self)
 	self.element_changed.connect(func(): HighlightUI.highlight_target_ack.emit("elements_container"))
 
 
+#########################
+###       Public      ###
+#########################
+
 func get_element() -> Element.enm:
 	return _current_element
 
 
+#########################
+###     Callbacks     ###
+#########################
+
 func _on_element_button_pressed(element: Element.enm):
 	_current_element = element
-	for button in get_children():
-		var is_current = button.element == _current_element
-		var is_researched = ElementLevel.get_current(button.element) > 0 
-		if is_current or is_researched:
-			button.set_pressed_no_signal(true)
-		else:
-			button.set_pressed_no_signal(false)
 	element_changed.emit()
