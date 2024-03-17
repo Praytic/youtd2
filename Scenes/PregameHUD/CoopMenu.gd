fe2d77bf1cb75035c81db1dc5db9c1db67c8dc8d
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-03-17 07:31:42 +0100

    Make it faster to join room
    Enter valid address as default text, skip placeholder text
    \ so user can press "Join" immediately
    Enable join button from the start and show error if address is not
    \ valid

--- Scenes/PregameHUD/CoopMenu.gd
+++ Scenes/PregameHUD/CoopMenu.gd
@@ -5,10 +5,18 @@ signal finished()
 
 
 @export var _host_address_field: TextEdit
-@export var _connect_button: Button
+@export var _address_error_label: Label
 
 
 func _on_join_room_button_pressed():
+#	TODO: check validity more thoroughly
+	var address_is_valid: bool = _host_address_field.text.split(":", false).size() == 2
+	
+	if !address_is_valid:
+		_address_error_label.show()
+		
+		return
+	
 	var address_details: Array = _host_address_field.text.split(":")
 	Network.connect_to_server(address_details[0], address_details[1] as int)
 	finished.emit()
@@ -21,7 +29,3 @@ func _on_create_room_button_pressed():
 
 func meets_condition() -> bool:
 	return Globals._player_mode == PlayerMode.enm.COOP
-
-
-func _on_type_room_id_text_edit_text_changed():
-	_connect_button.disabled = _host_address_field.text.split(":", false).size() != 2
