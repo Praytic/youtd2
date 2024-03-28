9a17b211415437bd450bcabfe5ee11c5ab278578
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-03-28 19:53:03 +0100

    Add quit button to main menu
    Add "ButtonLarge" variation to theme

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -1,5 +1,11 @@
 extends Node
 
+# Main menu for the game. Opens when the game starts.
+
 
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(Preloads.game_scene_scene)
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
