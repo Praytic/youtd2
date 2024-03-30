7fabcbfaea546260d9539a9230317b59518ca7f8
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-03-30 10:01:50 +0100

    Extract GameModeUI as separate scene
    So it can be used both for singleplayer and for multiplayer

--- Scenes/TitleScreen/ConfigureSingleplayerMenu.gd
+++ Scenes/TitleScreen/ConfigureSingleplayerMenu.gd
@@ -5,9 +5,7 @@ signal cancel_pressed()
 signal start_button_pressed()
 
 
-@export var _difficulty_combo: OptionButton
-@export var _game_length_combo: OptionButton
-@export var _game_mode_combo: OptionButton
+@export var _game_mode_ui: GameModeUI
 
 
 #########################
@@ -23,31 +21,14 @@ func _ready():
 #########################
 
 func get_difficulty() -> Difficulty.enm:
-	var selected_index: int = _difficulty_combo.get_selected()
-	var difficulty: Difficulty.enm = selected_index as Difficulty.enm
-	
-	return difficulty
+	return _game_mode_ui.get_difficulty()
 
 
 func get_game_length() -> int:
-	var selected_index: int = _game_length_combo.get_selected()
-	var game_length: int
-	match selected_index:
-		0: game_length = Constants.WAVE_COUNT_TRIAL
-		1: game_length = Constants.WAVE_COUNT_FULL
-		2: game_length = Constants.WAVE_COUNT_NEVERENDING
-		_:
-			push_error("Unknown game length combo index: ", selected_index)
-			game_length = Constants.WAVE_COUNT_TRIAL
-	
-	return game_length
-
+	return _game_mode_ui.get_game_length()
 
 func get_game_mode() -> GameMode.enm:
-	var selected_index: int = _game_mode_combo.get_selected()
-	var game_mode: GameMode.enm = selected_index as GameMode.enm
-	
-	return game_mode
+	return _game_mode_ui.get_game_mode()
 
 
 #########################
---
+++ Scenes/TitleScreen/GameModeUI.gd
@@ -0,0 +1,34 @@
+class_name GameModeUI extends GridContainer
+
+
+@export var _difficulty_combo: OptionButton
+@export var _game_length_combo: OptionButton
+@export var _game_mode_combo: OptionButton
+
+
+func get_difficulty() -> Difficulty.enm:
+	var selected_index: int = _difficulty_combo.get_selected()
+	var difficulty: Difficulty.enm = selected_index as Difficulty.enm
+	
+	return difficulty
+
+
+func get_game_length() -> int:
+	var selected_index: int = _game_length_combo.get_selected()
+	var game_length: int
+	match selected_index:
+		0: game_length = Constants.WAVE_COUNT_TRIAL
+		1: game_length = Constants.WAVE_COUNT_FULL
+		2: game_length = Constants.WAVE_COUNT_NEVERENDING
+		_:
+			push_error("Unknown game length combo index: ", selected_index)
+			game_length = Constants.WAVE_COUNT_TRIAL
+	
+	return game_length
+
+
+func get_game_mode() -> GameMode.enm:
+	var selected_index: int = _game_mode_combo.get_selected()
+	var game_mode: GameMode.enm = selected_index as GameMode.enm
+	
+	return game_mode
