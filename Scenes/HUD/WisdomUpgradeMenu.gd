3855c633f0f49d4b7bd649b78a7fd372a0bf79e9
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-04-10 21:08:10 +0200

    Validate wisdom upgrades cache
    Move loading of player stats to beginning so that they are available
    \ during cache validation

--- Scenes/HUD/WisdomUpgradeMenu.gd
+++ Scenes/HUD/WisdomUpgradeMenu.gd
@@ -25,8 +25,26 @@ var _upgrades_cached: Dictionary = {}
 #########################
 
 func _ready():
-	var upgrade_id_list: Array = WisdomUpgradeProperties.get_id_list()
+	var player_lvl: int = _get_player_level()
+	var orb_count: int = player_lvl * 1
+	_orbs_total = orb_count
+#	TODO: this value is currently incorrect if loaded cached
+#	upgrades.
+	_orbs_remaining = orb_count
+	_update_orbs_label()
+	var local_player: Player = PlayerManager.get_local_player()
+	_upgrade_max = local_player.get_wisdom_upgrade_max()
+
+# 	NOTE: upgrades cache may be invalid if player manually
+# 	modifies settings file or if player chooses a builder
+# 	which modifies max upgrades and old cached values don't
+# 	fit.
+	var upgrades_cached_is_valid: bool = _validate_upgrades_cache(_upgrades_cached, _upgrade_max, orb_count)
+	if !upgrades_cached_is_valid:
+		_upgrades_cached = {}
 	
+	var upgrade_id_list: Array = WisdomUpgradeProperties.get_id_list()
+
 	for upgrade_id in upgrade_id_list:
 		var bar: WisdomUpgradeBar = WisdomUpgradeBar.make(upgrade_id)
 		
@@ -44,18 +62,9 @@ func _ready():
 		
 		_bar_map[upgrade_id] = bar
 	
-	var player_lvl: int = _get_player_level()
-	var orb_count: int = player_lvl * 1
-	_orbs_total = orb_count
-	_orbs_remaining = orb_count
-	_update_orbs_label()
-	
 	var level_orbs_text: String = "You are level [color=GOLD]%d[/color] and you have [color=GOLD]%d[/color] orbs." % [player_lvl, orb_count]
 	_level_orbs_label.clear()
 	_level_orbs_label.append_text(level_orbs_text)
-
-	var local_player: Player = PlayerManager.get_local_player()
-	_upgrade_max = local_player.get_wisdom_upgrade_max()
 
 	for bar in _bar_map.values():
 		bar.set_max_value(_upgrade_max)
@@ -89,6 +98,33 @@ func get_wisdom_upgrades() -> Dictionary:
 #########################
 ###      Private      ###
 #########################
+
+func _validate_upgrades_cache(cache: Dictionary, upgrades_max: int, orb_count: int) -> bool:
+	var upgrade_id_list: Array = WisdomUpgradeProperties.get_id_list()
+
+	var orbs_used: int = 0
+
+	for upgrade_id in upgrade_id_list:
+		var value: int = cache.get(str(upgrade_id), 0)
+
+		var value_in_bounds: bool =  0 <= value && value <= upgrades_max
+
+		if !value_in_bounds:
+			push_error("Cached value for upgrade %d is out of bounds. Value: %d" % [upgrade_id, value])
+
+			return false
+
+		orbs_used += value
+
+	var used_more_orbs_than_got: bool = orbs_used > orb_count
+
+	if used_more_orbs_than_got:
+		push_error("Cached wisdom upgrades use too many orbs. Upgrades: %s" % cache)
+
+		return false
+
+	return true
+
 
 func _get_player_level() -> int:
 	var exp_password: String = Settings.get_setting(Settings.EXP_PASSWORD)
