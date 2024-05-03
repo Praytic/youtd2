929c14d9461f366db32d3a552d77a90750e8bed9
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-05-03 19:06:02 +0200

    Fix icons for challenge creeps

--- Singletons/UnitIcons.gd
+++ Singletons/UnitIcons.gd
@@ -72,13 +72,13 @@ func get_tower_tier_icon(tower_id: int) -> Texture2D:
 
 
 func get_creep_icon(creep: Creep) -> Texture2D:
-	var creep_size: CreepSize.enm = creep.get_size()
+	var creep_size: CreepSize.enm = creep.get_size_including_challenge_sizes()
 	var creep_category: CreepCategory.enm = creep.get_category() as CreepCategory.enm
 	var icon_path: String = _get_creep_icon_path(creep_category, creep_size)
 	var icon_path_exists: bool = ResourceLoader.exists(icon_path)
 
 	if !icon_path_exists:
-		push_error("Icon path doesn't exist")
+		push_error("Icon path doesn't exist: %s", icon_path)
 
 		return Texture2D.new()
 
