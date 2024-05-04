e5f7fd4dbfd74555f9a0fda8b75e7a0f306ae390
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-05-04 21:37:26 +0200

    Add coloring to portal damage messages

--- Scenes/Creeps/Creep.gd
+++ Scenes/Creeps/Creep.gd
@@ -330,12 +330,12 @@ func _deal_damage_to_portal():
 	var player: Player = get_player()
 	
 	if creep_size == CreepSize.enm.BOSS:
-		Messages.add_normal(player, "Dealt %s damage to BOSS" % damage_done_string)
+		Messages.add_normal(player, "[color=ORANGE_RED]Dealt %s damage to BOSS[/color]" % damage_done_string)
 	else:
-		Messages.add_normal(player, "Failed to kill a %s" % creep_size_string.to_upper())		
+		Messages.add_normal(player, "[color=ORANGE_RED]Failed to kill a %s[/color]" % creep_size_string.to_upper())		
 
 	if damage_to_portal > 0:
-		Messages.add_normal(player, "You lose %s of your lives!" % damage_to_portal_string)
+		Messages.add_normal(player, "[color=ORANGE_RED]You lose %s of your lives![/color]" % damage_to_portal_string)
 
 	if creep_score > 0:
 		player.add_score(creep_score)
