21c623daee455692e410e74d4f9c637c04faa450
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-05-11 11:22:21 +0200

    Add wordwrap to recipe button tooltips
    Also add coloring for numbers

--- Scenes/HUD/Buttons/RecipeButton.gd
+++ Scenes/HUD/Buttons/RecipeButton.gd
@@ -6,3 +6,9 @@ class_name RecipeButton extends Button
 func _ready():
 	var recipe_description: String = RecipeProperties.get_description(recipe)
 	set_tooltip_text(recipe_description)
+
+
+func _make_custom_tooltip(for_text: String) -> Object:
+	var label: RichTextLabel = Utils.make_rich_text_tooltip(for_text)
+
+	return label
