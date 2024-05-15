0b74e669477503c475dccc07567b45ceeb1d1b5a
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-05-15 21:27:44 +0200

    Fix Meteor Totem tower Attraction ability
    The meteor damage was being incorrectly dealt by the Meteor Totem tower.
    \ It should be dealt by the buffed tower. Note that projectile caster
    \ is the buffed tower.

--- Scenes/Towers/TowerBehaviors/MeteorTotem.gd
+++ Scenes/Towers/TowerBehaviors/MeteorTotem.gd
@@ -107,7 +107,8 @@ func on_autocast(_event: Event):
 
 
 func missile_pt_on_hit(projectile: Projectile, _target: Unit):
-	tower.do_spell_damage_aoe(Vector2(projectile.get_x(), projectile.get_y()), 220, projectile.user_int, tower.calc_spell_crit_no_bonus(), 0)
+	var buffed_tower: Tower = projectile.get_caster()
+	buffed_tower.do_spell_damage_aoe(Vector2(projectile.get_x(), projectile.get_y()), 220, projectile.user_int, buffed_tower.calc_spell_crit_no_bonus(), 0)
 	SFX.sfx_at_pos("DoomDeath.mdl", projectile.get_position_canvas())
 
 
