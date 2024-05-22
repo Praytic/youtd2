3113219871b898b375775ddcf78afc2e59dd250f
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-05-22 06:30:14 +0200

    Change speed buttons to toggle mode
    Add speed button controls to help menu

--- src/hud/hud.gd
+++ src/hud/hud.gd
@@ -322,13 +322,16 @@ func _on_unit_menu_details_pressed():
 		_creep_details.visible = false
 
 
-func _on_speed_normal_pressed():
-	Globals.set_update_ticks_per_physics_tick(Constants.GAME_SPEED_NORMAL)
+func _on_speed_normal_toggled(button_pressed: bool):
+	if button_pressed:
+		Globals.set_update_ticks_per_physics_tick(Constants.GAME_SPEED_NORMAL)
 
 
-func _on_speed_fast_pressed():
-	Globals.set_update_ticks_per_physics_tick(Constants.GAME_SPEED_FAST)
+func _on_speed_fast_toggled(button_pressed: bool):
+	if button_pressed:
+		Globals.set_update_ticks_per_physics_tick(Constants.GAME_SPEED_FAST)
 
 
-func _on_speed_fastest_pressed():
-	Globals.set_update_ticks_per_physics_tick(Constants.GAME_SPEED_FASTEST)
+func _on_speed_fastest_toggled(button_pressed: bool):
+	if button_pressed:
+		Globals.set_update_ticks_per_physics_tick(Constants.GAME_SPEED_FASTEST)
