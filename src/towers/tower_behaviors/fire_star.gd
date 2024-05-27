f16470661050baf6b5d4ca7a926a7bde33aa9c56
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-05-27 06:26:10 +0200

    Fix bug in Fire Star
    Ignite stacks were not affecting damage

--- src/towers/tower_behaviors/fire_star.gd
+++ src/towers/tower_behaviors/fire_star.gd
@@ -1,6 +1,15 @@
 extends TowerBehavior
 
 
+# NOTE: fixed bug in original script. Ignite stacks did not
+# get increased damage because that was implemented in
+# REFRESH callback, which never got called. REFRESH callback
+# gets called if buff is applied with same level and less or
+# lower power. Everytime Ignite is reapplied, it gets
+# increased power, so therefore it always triggers UPGRADE
+# instead of REFRESH.
+
+
 var ignite_bt: BuffType
 var firestar_pt: ProjectileType
 
@@ -47,7 +56,6 @@ func tower_init():
 	ignite_bt.set_buff_modifier(ignite_bt_mod)
 	ignite_bt.set_buff_icon("res://resources/icons/generic_icons/azul_flake.tres")
 	ignite_bt.set_buff_tooltip("Ignite\nDeals damage over time and reduces movement speed.")
-	ignite_bt.add_event_on_refresh(ignite_bt_on_refresh)
 	ignite_bt.add_periodic_event(ignite_bt_periodic, 2.0)
 
 	firestar_pt = ProjectileType.create("LordofFlameMissile.mdl", 7.0, 1000, self)
@@ -95,22 +103,15 @@ func firestar_pt_on_hit(_p: Projectile, target: Unit):
 	if buff != null:
 		buff_power = buff.get_power() + 1
 		buff = ignite_bt.apply_custom_power(tower, target, 1, buff_power)
+		var added_damage_multiplier: float = 0.05 + 0.002 * level
+		buff.user_real += added_damage_multiplier
 	else:
 		buff_power = 0
 		buff = ignite_bt.apply_custom_power(tower, target, 1, buff_power)
-		var damage_multiplier: float = 1.0 + 0.04 * level
-		buff.user_real = damage_multiplier
+		var initial_damage_multiplier: float = 1.0 + 0.04 * level
+		buff.user_real = initial_damage_multiplier
 
 	tower.do_attack_damage(target, tower.get_current_attack_damage_with_bonus(), tower.calc_attack_multicrit_no_bonus())
-
-
-func ignite_bt_on_refresh(event: Event):
-	var buff: Buff = event.get_buff()
-	var caster: Unit = buff.get_caster()
-	var level: int = caster.get_level()
-	var damage_multiplier_bonus: float = 0.05 + 0.002 * level
-
-	buff.user_real += damage_multiplier_bonus
 
 
 func ignite_bt_periodic(event: Event):
