47c90170867c2d3c5d96fed7dc1620d6e92cb796
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-06-04 18:01:40 +0200

    Adjust bonus wave autospawn
    It will now reach 1s after ~80 waves, instead of 40

--- src/player/team.gd
+++ src/player/team.gd
@@ -251,7 +251,7 @@ func _get_extreme_autospawn_time(level: int) -> float:
 func _get_bonus_wave_autospawn_time(level: int) -> float:
 	const START_TIME: float = 20.0
 	const MIN_TIME: float = 1.0
-	const REDUCTION_PER_LEVEL: float = 0.5
+	const REDUCTION_PER_LEVEL: float = 0.25
 
 	var autospawn_time: float = START_TIME - max(0, level - Constants.WAVE_COUNT_NEVERENDING) * REDUCTION_PER_LEVEL
 	autospawn_time = max(autospawn_time, MIN_TIME)
