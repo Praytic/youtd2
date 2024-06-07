51fd30d587e3bb4f074df037b0bd1f6910de93a6
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-06-07 17:27:30 +0200

    Fix Oils of Lore not giving exp

--- src/items/item_behaviors/arcane_oil_of_lore.gd
+++ src/items/item_behaviors/arcane_oil_of_lore.gd
@@ -1,5 +1,14 @@
 extends ItemBehavior
 
 
+func get_ability_description() -> String:
+	return "Adds 120 flat exp to tower."
+
+
 func load_modifier(modifier: Modifier):
 	modifier.add_modification(Modification.Type.MOD_EXP_RECEIVED, 0.7, 0)
+
+
+func on_pickup():
+	var carrier: Tower = item.get_carrier()
+	carrier.add_exp_flat(120)
--- src/items/item_behaviors/oil_of_lore.gd
+++ src/items/item_behaviors/oil_of_lore.gd
@@ -1,5 +1,14 @@
 extends ItemBehavior
 
 
+func get_ability_description() -> String:
+	return "Adds 60 flat exp to tower."
+
+
 func load_modifier(modifier: Modifier):
 	modifier.add_modification(Modification.Type.MOD_EXP_RECEIVED, 0.28, 0)
+
+
+func on_pickup():
+	var carrier: Tower = item.get_carrier()
+	carrier.add_exp_flat(60)
