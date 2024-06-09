696c2327f1e275b237b3b06c7d914243ff3d7db0
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-06-09 10:01:39 +0200

    Fix Cruel Torch not having level bonus

--- src/items/item_behaviors/cruel_torch.gd
+++ src/items/item_behaviors/cruel_torch.gd
@@ -21,7 +21,7 @@ func item_init():
 	flames_bt.set_buff_icon("res://resources/icons/generic_icons/mighty_force.tres")
 	flames_bt.set_buff_tooltip("Flames of Fury\nIncreases critical chance.")
 	var mod: Modifier = Modifier.new()
-	mod.add_modification(Modification.Type.MOD_ATK_CRIT_CHANCE, 0.035, 0.0)
+	mod.add_modification(Modification.Type.MOD_ATK_CRIT_CHANCE, 0.035, 0.0008)
 	flames_bt.set_buff_modifier(mod)
 
 	var aura: AuraType = AuraType.new()
