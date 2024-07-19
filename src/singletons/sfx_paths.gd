bba84da4e304822872765df047e85c0611d51f7e
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-07-19 14:54:24 +0700

    Organize sound files

--- src/singletons/sfx_paths.gd
+++ src/singletons/sfx_paths.gd
@@ -6,14 +6,14 @@ const PICKUP_ITEM: String = "res://assets/sfx/411177__silverillusionist__pick-up
 const PICKUP_GOLD: String = "res://assets/sfx/734247__noisyredfox__coins3.ogg"
 const DAMAGE_PORTAL: String = "res://assets/sfx/442903__qubodup__slash-remix.ogg"
 const START_WAVE: String = "res://assets/sfx/161654__qubodup__war-game-battle-level-up-remix.ogg"
-const LEVEL_UP: String = "res://assets/sfx/bfxr_level_up.ogg"
+const LEVEL_UP: String = "res://assets/sfx/kevl_level_up.ogg"
 
 const TOWER_ATTACK_MAP: Dictionary = {
-	Element.enm.NATURE: "res://assets/sfx/swosh-08.mp3",
-	Element.enm.STORM: "res://assets/sfx/foom_02.mp3",
-	Element.enm.FIRE: "res://assets/sfx/fire_attack1.mp3",
-	Element.enm.ICE: "res://assets/sfx/iceball.mp3",
-	Element.enm.ASTRAL: "res://assets/sfx/attack_sound1.mp3",
-	Element.enm.DARKNESS: "res://assets/sfx/swosh-11.mp3",
-	Element.enm.IRON: "res://assets/sfx/iron_attack1.mp3",
+	Element.enm.NATURE: "res://assets/sfx/unknown_swosh-08.mp3",
+	Element.enm.STORM: "res://assets/sfx/opengameart/bart/foom_0.ogg",
+	Element.enm.FIRE: "res://assets/sfx/unknown_fire_attack1.mp3",
+	Element.enm.ICE: "res://assets/sfx/unknown_iceball.mp3",
+	Element.enm.ASTRAL: "res://assets/sfx/unknown_attack_sound1.mp3",
+	Element.enm.DARKNESS: "res://assets/sfx/unknown_swosh-11.mp3",
+	Element.enm.IRON: "res://assets/sfx/unknown_iron_attack1.mp3",
 }
