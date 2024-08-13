7724339ed02bcbbbb13da63cbf5d71ec86319cf0
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-08-13 20:18:45 +0200

    Remove hole punch icon

--- addons/Holepunch/holepunch.gd
+++ addons/Holepunch/holepunch.gd
@@ -3,7 +3,8 @@ extends EditorPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here
-	add_custom_type("HolePunch", "Node", preload("holepunch_node.gd"), preload("icon.png"))
+	# NOTE: removed icon to avoid needing to commit png to repo
+	add_custom_type("HolePunch", "Node", preload("holepunch_node.gd"), Texture2D.new())
 
 func _exit_tree():
 	# Clean-up of the plugin goes here
