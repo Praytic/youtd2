5976f8270fe2b1dd3cf62df3957320fdfaaaba9c
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-08-27 07:35:48 +0200

    Fix desync bug in distorted idol
    Native Timer must not be used for gameplay code - caues desyncs
    Use ManualTimer instead

--- src/items/item_behaviors/distorted_idol.gd
+++ src/items/item_behaviors/distorted_idol.gd
@@ -43,8 +43,8 @@ func on_pickup():
 	if !carrier_is_on_corner:
 		player.display_floating_text("Distorted Idol carrier must be on corner!", carrier, Color.PURPLE)
 
-		await get_tree().create_timer(0.1).timeout
-
+		await Utils.create_timer(0.1, self).timeout
+		
 		item.drop()
 		item.fly_to_stash(0.0)
 
