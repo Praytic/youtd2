97d1a380c7ed38964a48ff8ea47ed42299f5cbc2
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-08-31 20:29:49 +0200

    Fix Ruined Wind Tower
    It was incorrectly dropping Common items, ignoring upgrade tier of
    \ tower. This ability will now correctly switch to higher rarity
    \ after each upgrade.

--- src/towers/tower_behaviors/ruined_wind_tower.gd
+++ src/towers/tower_behaviors/ruined_wind_tower.gd
@@ -1,14 +1,28 @@
 extends TowerBehavior
 
 
+func get_tier_stats() -> Dictionary:
+	return {
+		1: {item_rarity = Rarity.enm.COMMON},
+		2: {item_rarity = Rarity.enm.UNCOMMON},
+		3: {item_rarity = Rarity.enm.RARE},
+		4: {item_rarity = Rarity.enm.UNIQUE},
+	}
+
+
 func get_ability_info_list() -> Array[AbilityInfo]:
+	var color: Color = Rarity.get_color(_stats.item_rarity)
+	var item_rarity: String = Rarity.convert_to_string(_stats.item_rarity)
+	item_rarity = item_rarity.capitalize()
+	item_rarity = Utils.get_colored_string(item_rarity, color)
+
 	var list: Array[AbilityInfo] = []
 	
 	var ability: AbilityInfo = AbilityInfo.new()
 	ability.name = "Rejection"
 	ability.icon = "res://resources/icons/clubs/club_01.tres"
-	ability.description_short = "This tower drops all except Common items on attack.\n"
-	ability.description_full = "This tower drops all except Common items on attack.\n"
+	ability.description_short = "This tower drops all except %s items on attack.\n" % item_rarity
+	ability.description_full = "This tower drops all except %s items on attack.\n" % item_rarity
 	list.append(ability)
 
 	return list
@@ -22,13 +36,15 @@ func on_attack(_event: Event):
 	var itm: Item
 	var i: int = 1
 
+	var item_rarity: Rarity.enm = _stats.item_rarity
+
 	while true:
 		if i > 6:
 			break
 
 		itm = tower.get_held_item(i)
 
-		if itm != null && itm.get_rarity() != Rarity.enm.COMMON:
+		if itm != null && itm.get_rarity() != item_rarity:
 			itm.drop()
 			itm.fly_to_stash(0.0)
 
