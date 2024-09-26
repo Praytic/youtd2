e33cf8c392db17ec87bfe3eb7f054c5cad7d211c
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-09-26 19:12:33 +0200

    Change variable names for Felweed

--- src/towers/tower_behaviors/felweed.gd
+++ src/towers/tower_behaviors/felweed.gd
@@ -3,25 +3,25 @@ extends TowerBehavior
 
 func get_tier_stats() -> Dictionary:
 	return {
-		1: {bonus_7 = 1.10, bonus_8 = 1.20, bonus_9 = 1.30, bonus_10 = 1.40, bonus_7_add = 0.002, bonus_8_add = 0.004, bonus_9_add = 0.006, bonus_10_add = 0.008},
-		2: {bonus_7 = 1.10, bonus_8 = 1.20, bonus_9 = 1.30, bonus_10 = 1.40, bonus_7_add = 0.002, bonus_8_add = 0.004, bonus_9_add = 0.006, bonus_10_add = 0.008},
-		3: {bonus_7 = 1.125, bonus_8 = 1.25, bonus_9 = 1.375, bonus_10 = 1.50, bonus_7_add = 0.0025, bonus_8_add = 0.005, bonus_9_add = 0.0075, bonus_10_add = 0.01},
-		4: {bonus_7 = 1.125, bonus_8 = 1.25, bonus_9 = 1.375, bonus_10 = 1.50, bonus_7_add = 0.0025, bonus_8_add = 0.005, bonus_9_add = 0.0075, bonus_10_add = 0.01},
-		5: {bonus_7 = 1.15, bonus_8 = 1.30, bonus_9 = 1.45, bonus_10 = 1.60, bonus_7_add = 0.003, bonus_8_add = 0.006, bonus_9_add = 0.009, bonus_10_add = 0.012},
-		6: {bonus_7 = 1.15, bonus_8 = 1.30, bonus_9 = 1.45, bonus_10 = 1.60, bonus_7_add = 0.003, bonus_8_add = 0.006, bonus_9_add = 0.009, bonus_10_add = 0.012},
+		1: {bonus_1 = 1.10, bonus_2 = 1.20, bonus_3 = 1.30, bonus_4 = 1.40, bonus_1_add = 0.002, bonus_2_add = 0.004, bonus_3_add = 0.006, bonus_4_add = 0.008},
+		2: {bonus_1 = 1.10, bonus_2 = 1.20, bonus_3 = 1.30, bonus_4 = 1.40, bonus_1_add = 0.002, bonus_2_add = 0.004, bonus_3_add = 0.006, bonus_4_add = 0.008},
+		3: {bonus_1 = 1.125, bonus_2 = 1.25, bonus_3 = 1.375, bonus_4 = 1.50, bonus_1_add = 0.0025, bonus_2_add = 0.005, bonus_3_add = 0.0075, bonus_4_add = 0.01},
+		4: {bonus_1 = 1.125, bonus_2 = 1.25, bonus_3 = 1.375, bonus_4 = 1.50, bonus_1_add = 0.0025, bonus_2_add = 0.005, bonus_3_add = 0.0075, bonus_4_add = 0.01},
+		5: {bonus_1 = 1.15, bonus_2 = 1.30, bonus_3 = 1.45, bonus_4 = 1.60, bonus_1_add = 0.003, bonus_2_add = 0.006, bonus_3_add = 0.009, bonus_4_add = 0.012},
+		6: {bonus_1 = 1.15, bonus_2 = 1.30, bonus_3 = 1.45, bonus_4 = 1.60, bonus_1_add = 0.003, bonus_2_add = 0.006, bonus_3_add = 0.009, bonus_4_add = 0.012},
 	}
 
 
 func get_ability_info_list() -> Array[AbilityInfo]:
-	var bonus_7: String = Utils.format_percent(_stats.bonus_7 - 1.0, 2)
-	var bonus_8: String = Utils.format_percent(_stats.bonus_8 - 1.0, 2)
-	var bonus_9: String = Utils.format_percent(_stats.bonus_9 - 1.0, 2)
-	var bonus_10: String = Utils.format_percent(_stats.bonus_10 - 1.0, 2)
+	var bonus_1: String = Utils.format_percent(_stats.bonus_1 - 1.0, 2)
+	var bonus_2: String = Utils.format_percent(_stats.bonus_2 - 1.0, 2)
+	var bonus_3: String = Utils.format_percent(_stats.bonus_3 - 1.0, 2)
+	var bonus_4: String = Utils.format_percent(_stats.bonus_4 - 1.0, 2)
 
-	var bonus_7_add: String = Utils.format_percent(_stats.bonus_7_add, 2)
-	var bonus_8_add: String = Utils.format_percent(_stats.bonus_8_add, 2)
-	var bonus_9_add: String = Utils.format_percent(_stats.bonus_9_add, 2)
-	var bonus_10_add: String = Utils.format_percent(_stats.bonus_10_add, 2)
+	var bonus_1_add: String = Utils.format_percent(_stats.bonus_1_add, 2)
+	var bonus_2_add: String = Utils.format_percent(_stats.bonus_2_add, 2)
+	var bonus_3_add: String = Utils.format_percent(_stats.bonus_3_add, 2)
+	var bonus_4_add: String = Utils.format_percent(_stats.bonus_4_add, 2)
 
 	var list: Array[AbilityInfo] = []
 	
@@ -29,16 +29,16 @@ func get_ability_info_list() -> Array[AbilityInfo]:
 	ability.name = "Fireblossom"
 	ability.icon = "res://resources/icons/plants/flower_02.tres"
 	ability.description_short = "Every few attacks this tower deals some bonus damage.\n"
-	ability.description_full = "Every 7th attack deals %s bonus damage.\n" % bonus_7 \
-	+ "Every 8th attack deals %s bonus damage.\n" % bonus_8 \
-	+ "Every 9th attack deals %s bonus damage.\n" % bonus_9 \
-	+ "Every 10th attack deals %s bonus damage.\n" % bonus_10 \
+	ability.description_full = "Every 7th attack deals %s bonus damage.\n" % bonus_1 \
+	+ "Every 8th attack deals %s bonus damage.\n" % bonus_2 \
+	+ "Every 9th attack deals %s bonus damage.\n" % bonus_3 \
+	+ "Every 10th attack deals %s bonus damage.\n" % bonus_4 \
 	+ " \n" \
 	+ "[color=ORANGE]Level Bonus:[/color]\n" \
-	+ "+%s bonus damage every 7th attack.\n" % bonus_7_add \
-	+ "+%s bonus damage every 8th attack.\n" % bonus_8_add \
-	+ "+%s bonus damage every 9th attack.\n" % bonus_9_add \
-	+ "+%s bonus damage every 10th attack.\n" % bonus_10_add
+	+ "+%s bonus damage every 7th attack.\n" % bonus_1_add \
+	+ "+%s bonus damage every 8th attack.\n" % bonus_2_add \
+	+ "+%s bonus damage every 9th attack.\n" % bonus_3_add \
+	+ "+%s bonus damage every 10th attack.\n" % bonus_4_add
 	list.append(ability)
 
 	return list
@@ -65,19 +65,19 @@ func on_damage(event: Event):
 	tower.user_real = tower.user_real + 1
 
 	if tower.user_int >= 7:
-		event.damage = event.damage * (_stats.bonus_7 + level * _stats.bonus_7_add)
+		event.damage = event.damage * (_stats.bonus_1 + level * _stats.bonus_1_add)
 		tower.user_int = 0
 
 	if tower.user_int2 >= 8:
-		event.damage = event.damage * (_stats.bonus_8 + level * _stats.bonus_8_add)
+		event.damage = event.damage * (_stats.bonus_2 + level * _stats.bonus_2_add)
 		tower.user_int2 = 0
 
 	if tower.user_int3 >= 9:
-		event.damage = event.damage * (_stats.bonus_9 + level * _stats.bonus_9_add)
+		event.damage = event.damage * (_stats.bonus_3 + level * _stats.bonus_3_add)
 		tower.user_int3 = 0
 
 	if tower.user_real >= 10:
-		event.damage = event.damage * (_stats.bonus_10 + level * _stats.bonus_10_add)
+		event.damage = event.damage * (_stats.bonus_4 + level * _stats.bonus_4_add)
 		tower.user_real = 0
 
 	if event.damage > damage:
