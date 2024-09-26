b879e2edeea954bfb94a82cf9b4c4fa8ed59482f
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-09-26 19:27:19 +0200

    Fix Felweed ability not scaling with tower tier
    The required attack count for each bonus is supposed to go up with
    \ tower tiers
    Also change the color of floating text to be more readable
    Closes #448

--- src/towers/tower_behaviors/felweed.gd
+++ src/towers/tower_behaviors/felweed.gd
@@ -3,16 +3,21 @@ extends TowerBehavior
 
 func get_tier_stats() -> Dictionary:
 	return {
-		1: {bonus_1 = 1.10, bonus_2 = 1.20, bonus_3 = 1.30, bonus_4 = 1.40, bonus_1_add = 0.002, bonus_2_add = 0.004, bonus_3_add = 0.006, bonus_4_add = 0.008},
-		2: {bonus_1 = 1.10, bonus_2 = 1.20, bonus_3 = 1.30, bonus_4 = 1.40, bonus_1_add = 0.002, bonus_2_add = 0.004, bonus_3_add = 0.006, bonus_4_add = 0.008},
-		3: {bonus_1 = 1.125, bonus_2 = 1.25, bonus_3 = 1.375, bonus_4 = 1.50, bonus_1_add = 0.0025, bonus_2_add = 0.005, bonus_3_add = 0.0075, bonus_4_add = 0.01},
-		4: {bonus_1 = 1.125, bonus_2 = 1.25, bonus_3 = 1.375, bonus_4 = 1.50, bonus_1_add = 0.0025, bonus_2_add = 0.005, bonus_3_add = 0.0075, bonus_4_add = 0.01},
-		5: {bonus_1 = 1.15, bonus_2 = 1.30, bonus_3 = 1.45, bonus_4 = 1.60, bonus_1_add = 0.003, bonus_2_add = 0.006, bonus_3_add = 0.009, bonus_4_add = 0.012},
-		6: {bonus_1 = 1.15, bonus_2 = 1.30, bonus_3 = 1.45, bonus_4 = 1.60, bonus_1_add = 0.003, bonus_2_add = 0.006, bonus_3_add = 0.009, bonus_4_add = 0.012},
+		1: {bonus_1_attack_count = 7, bonus_1 = 1.10, bonus_2 = 1.20, bonus_3 = 1.30, bonus_4 = 1.40, bonus_1_add = 0.002, bonus_2_add = 0.004, bonus_3_add = 0.006, bonus_4_add = 0.008},
+		2: {bonus_1_attack_count = 7, bonus_1 = 1.10, bonus_2 = 1.20, bonus_3 = 1.30, bonus_4 = 1.40, bonus_1_add = 0.002, bonus_2_add = 0.004, bonus_3_add = 0.006, bonus_4_add = 0.008},
+		3: {bonus_1_attack_count = 6, bonus_1 = 1.125, bonus_2 = 1.25, bonus_3 = 1.375, bonus_4 = 1.50, bonus_1_add = 0.0025, bonus_2_add = 0.005, bonus_3_add = 0.0075, bonus_4_add = 0.01},
+		4: {bonus_1_attack_count = 6, bonus_1 = 1.125, bonus_2 = 1.25, bonus_3 = 1.375, bonus_4 = 1.50, bonus_1_add = 0.0025, bonus_2_add = 0.005, bonus_3_add = 0.0075, bonus_4_add = 0.01},
+		5: {bonus_1_attack_count = 5, bonus_1 = 1.15, bonus_2 = 1.30, bonus_3 = 1.45, bonus_4 = 1.60, bonus_1_add = 0.003, bonus_2_add = 0.006, bonus_3_add = 0.009, bonus_4_add = 0.012},
+		6: {bonus_1_attack_count = 5, bonus_1 = 1.15, bonus_2 = 1.30, bonus_3 = 1.45, bonus_4 = 1.60, bonus_1_add = 0.003, bonus_2_add = 0.006, bonus_3_add = 0.009, bonus_4_add = 0.012},
 	}
 
 
 func get_ability_info_list() -> Array[AbilityInfo]:
+	var bonus_1_attack_count: String = Utils.format_float(_stats.bonus_1_attack_count, 0)
+	var bonus_2_attack_count: String = Utils.format_float(_stats.bonus_1_attack_count + 1, 0)
+	var bonus_3_attack_count: String = Utils.format_float(_stats.bonus_1_attack_count + 2, 0)
+	var bonus_4_attack_count: String = Utils.format_float(_stats.bonus_1_attack_count + 3, 0)
+
 	var bonus_1: String = Utils.format_percent(_stats.bonus_1 - 1.0, 2)
 	var bonus_2: String = Utils.format_percent(_stats.bonus_2 - 1.0, 2)
 	var bonus_3: String = Utils.format_percent(_stats.bonus_3 - 1.0, 2)
@@ -29,16 +34,16 @@ func get_ability_info_list() -> Array[AbilityInfo]:
 	ability.name = "Fireblossom"
 	ability.icon = "res://resources/icons/plants/flower_02.tres"
 	ability.description_short = "Every few attacks this tower deals some bonus damage.\n"
-	ability.description_full = "Every 7th attack deals %s bonus damage.\n" % bonus_1 \
-	+ "Every 8th attack deals %s bonus damage.\n" % bonus_2 \
-	+ "Every 9th attack deals %s bonus damage.\n" % bonus_3 \
-	+ "Every 10th attack deals %s bonus damage.\n" % bonus_4 \
+	ability.description_full = "Every %sth attack deals %s bonus damage.\n" % [bonus_1_attack_count, bonus_1] \
+	+ "Every %sth attack deals %s bonus damage.\n" % [bonus_2_attack_count, bonus_2] \
+	+ "Every %sth attack deals %s bonus damage.\n" % [bonus_3_attack_count, bonus_3] \
+	+ "Every %sth attack deals %s bonus damage.\n" % [bonus_4_attack_count, bonus_4] \
 	+ " \n" \
 	+ "[color=ORANGE]Level Bonus:[/color]\n" \
-	+ "+%s bonus damage every 7th attack.\n" % bonus_1_add \
-	+ "+%s bonus damage every 8th attack.\n" % bonus_2_add \
-	+ "+%s bonus damage every 9th attack.\n" % bonus_3_add \
-	+ "+%s bonus damage every 10th attack.\n" % bonus_4_add
+	+ "+%s bonus damage every %sth attack.\n" % [bonus_1_add, bonus_1_attack_count] \
+	+ "+%s bonus damage every %sth attack.\n" % [bonus_2_add, bonus_2_attack_count] \
+	+ "+%s bonus damage every %sth attack.\n" % [bonus_3_add, bonus_3_attack_count] \
+	+ "+%s bonus damage every %sth attack.\n" % [bonus_4_add, bonus_4_attack_count]
 	list.append(ability)
 
 	return list
@@ -64,22 +69,27 @@ func on_damage(event: Event):
 	tower.user_int3 = tower.user_int3 + 1
 	tower.user_real = tower.user_real + 1
 
-	if tower.user_int >= 7:
+	var bonus_1_attack_count: int = _stats.bonus_1_attack_count
+	var bonus_2_attack_count: int = _stats.bonus_1_attack_count + 1
+	var bonus_3_attack_count: int = _stats.bonus_1_attack_count + 2
+	var bonus_4_attack_count: int = _stats.bonus_1_attack_count + 3
+
+	if tower.user_int >= bonus_1_attack_count:
 		event.damage = event.damage * (_stats.bonus_1 + level * _stats.bonus_1_add)
 		tower.user_int = 0
 
-	if tower.user_int2 >= 8:
+	if tower.user_int2 >= bonus_2_attack_count:
 		event.damage = event.damage * (_stats.bonus_2 + level * _stats.bonus_2_add)
 		tower.user_int2 = 0
 
-	if tower.user_int3 >= 9:
+	if tower.user_int3 >= bonus_3_attack_count:
 		event.damage = event.damage * (_stats.bonus_3 + level * _stats.bonus_3_add)
 		tower.user_int3 = 0
 
-	if tower.user_real >= 10:
+	if tower.user_real >= bonus_4_attack_count:
 		event.damage = event.damage * (_stats.bonus_4 + level * _stats.bonus_4_add)
 		tower.user_real = 0
 
 	if event.damage > damage:
 		var bonus_damage_text: String = Utils.format_float(event.damage, 0)
-		tower.get_player().display_small_floating_text(bonus_damage_text, tower, Color8(255, 150, 150), 0)
+		tower.get_player().display_small_floating_text(bonus_damage_text, tower, Color8(190, 50, 50), 0)
