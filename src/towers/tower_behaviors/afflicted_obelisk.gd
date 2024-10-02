905a46ba73ab4343e9ddb52bcf48031c947caa69
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-10-02 17:09:20 +0200

    Undo deviations in Afflicted Obelisk
    It's fine for this buff to be friendly even though it's applied on
    \ creeps - but it doesn't matter because this buff is purely visual.
    Better to have less deviations

--- src/towers/tower_behaviors/afflicted_obelisk.gd
+++ src/towers/tower_behaviors/afflicted_obelisk.gd
@@ -1,12 +1,10 @@
 extends TowerBehavior
 
 
-# NOTE: fixed multiple errors in original script. The
-# parasite buff should be a debuff, so friendly argument for
-# buff constructor should be false. When debuff is applied,
-# the duration needs to be divided by target's property not
-# tower's. Also changed the property to debuff duration
-# because I changed the buff to debuff.
+# NOTE: it may look wrong that the parasite buff has
+# "friendly" property set to true but it doesn't matter
+# because this buff is only for visual purposes. Leave it as
+# is and do not change this.
 
 
 var parasite_bt: BuffType
@@ -49,7 +47,7 @@ func load_triggers(triggers: BuffType):
 
 
 func tower_init():
-	parasite_bt = BuffType.new("parasite_bt", 0, 0, false, self)
+	parasite_bt = BuffType.new("parasite_bt", 0, 0, true, self)
 	parasite_bt.set_buff_icon("res://resources/icons/generic_icons/amber_mosquito.tres")
 	parasite_bt.set_buff_tooltip("Slumbering Parasite\nIncreases damage taken from Nature towers and deals Decay damage when the parasite surfaces.")
 
@@ -58,8 +56,11 @@ func on_damage(event: Event):
 	var target: Unit = event.get_target()
 	var level: int = tower.get_level()
 
-#	applying the dummy buff to show the effect on the unot
-	parasite_bt.apply_custom_timed(tower, target, 0, 3.0 / target.get_prop_debuff_duration())
+#	NOTE: need to divide by tower's buff duration because
+#	duration will later be multiplied by tower's buff
+#	duration, when buff is applied. Final result will be
+#	that duration is 3 seconds.
+	parasite_bt.apply_custom_timed(tower, target, 0, 3.0 / tower.get_prop_buff_duration())
 	target.modify_property(Modification.Type.MOD_DMG_FROM_NATURE, _stats.vuln_value + level * _stats.vuln_value_add)
 
 	await Utils.create_timer(3.0, self).timeout
