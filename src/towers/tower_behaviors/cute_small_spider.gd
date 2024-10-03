d9c4651f1b0d68a05dd45744ceb953c5b0689011
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-10-03 19:27:26 +0200

    Remove deviation from Cute Small Spider
    Deviation was that the comparison of spell_damage_dealt was missing
    Fix ORIGINAL_GAME_BUG related to spell_damage_dealt

--- src/towers/tower_behaviors/cute_small_spider.gd
+++ src/towers/tower_behaviors/cute_small_spider.gd
@@ -5,12 +5,21 @@ extends TowerBehavior
 # "on_damage" event handler. Changed script to add handler
 # directly to tower.
 
-# NOTE: [ORIGINAL_GAME_DEVIATION] removed mechanic of Poison
-# getting replaced when a tower with higher "spell damage
-# dealt" applies it. It's not good because a lower tier
-# Spider could randomly get a big boost to "spell damage
-# dealt" from some ability or item, which would incorrectly
-# replace the more powerful Poison.
+# NOTE: [ORIGINAL_GAME_BUG] fixed bug in poison buff logic.
+# Original script compares the spell_damage_dealt stat of
+# active buff's caster with current tower. If current tower
+# has higher spell_damage_dealt stat, the buff will be
+# replaced. The problem is that original script didn't
+# consider the case where there is one Cute Small Spider
+# tower and it's spell_damage_dealt stat changes frequently
+# because of buffs from another tower or items. The buff
+# will get replaced everytime spell_damage_dealt stat
+# changes. If tower's spell_damage_dealt stat went up and
+# down frequently, then buff would get reset frequently.
+# This would cause the buff's periodic timer for damage to
+# get constantly reset. If spell_damage_dealt stat changed
+# every second, then it's possible for poison damage ticks
+# to never happen at all!
 
 
 var poison_bt: BuffType
@@ -76,24 +85,40 @@ func on_damage(event: Event):
 	var active_stacks: int = 0
 	var active_damage: float = 0
 	var active_damage_cap: float = 0
+	var active_spell_damage_dealt: float = -1
 	if active_buff != null:
 		active_stacks = active_buff.user_int
 		active_damage = active_buff.user_real
 		active_damage_cap = active_buff.user_real2
+		active_spell_damage_dealt = active_buff.user_real3
 
 	var new_stacks: int = min(active_stacks + 1, 5)
 	var added_damage: float = _stats.damage + _stats.damage_add * level
 	var this_damage_cap: float = _stats.max_damage + _stats.max_damage_add * level
 	var new_damage_cap: float = max(active_damage_cap, this_damage_cap)
 	var new_damage: float = min(active_damage + added_damage, new_damage_cap)
+	var new_spell_damage_dealt: float = tower.get_prop_spell_damage_dealt()
 
-#	NOTE: weaker tier tower increases damage without
-#	refreshing duration
+#	NOTE: [ORIGINAL_GAME_DEVIATION] in original game, the
+#	spell power comparison and buff replacement did not
+#	consider the tier of the tower. Add it so that higher
+#	tier towers always have prio.
+	if active_buff != null && active_buff.get_caster() != tower:
+		var active_tier: int = active_buff.get_tower_tier()
+		var new_tier: int = tower.get_tier()
+
+		var this_tower_is_more_powerful: bool = new_tier >= active_tier && new_spell_damage_dealt > active_spell_damage_dealt
+
+		if this_tower_is_more_powerful:
+			active_buff.remove_buff()
+
 	active_buff = poison_bt.apply(tower, target, 1)
 	active_buff.user_int = new_stacks
 	active_buff.set_displayed_stacks(new_stacks)
 	active_buff.user_real = new_damage
 	active_buff.user_real2 = new_damage_cap
+	if new_spell_damage_dealt > active_spell_damage_dealt:
+		active_buff.user_real3 = new_spell_damage_dealt
 
 
 func tower_init():
