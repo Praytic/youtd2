f53fbc756c6cf9c5cf425526d39c01923761048b
Dmitry Degtyarev <kvelyognaut@gmail.com>
2024-10-04 17:36:25 +0200

    Fix aura.target_self for Steam Engine
    It's false in original game
    Closes #456

--- src/towers/tower_behaviors/the_steam_engine.gd
+++ src/towers/tower_behaviors/the_steam_engine.gd
@@ -69,7 +69,7 @@ func create_autocasts() -> Array[Autocast]:
 	autocast_speed_up.auto_range = 0
 	autocast_speed_up.cooldown = 0.5
 	autocast_speed_up.mana_cost = 0
-	autocast_speed_up.target_self = true
+	autocast_speed_up.target_self = false
 	autocast_speed_up.is_extended = false
 	autocast_speed_up.buff_type = null
 	autocast_speed_up.buff_target_type = null
